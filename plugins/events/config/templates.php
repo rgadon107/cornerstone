e73228b2bc2a7be23dad7d31f903a1c0af366b3e
hellofromtonya <hellofromtonya@knowthecode.io>
2018-04-11 14:10:27 -0500

    Renamed "templates" to "template" in Events plugin.
    Standardized on the singular form of template.
    
    Also, added DocBlocks to each of the template files.

--- plugins/events/config/templates.php
+++ plugins/events/config/templates.php
@@ -3,20 +3,20 @@
  * Templates configuration, providing the absolute path to each template
  * in this plugin which the Template Loader will load.
  *
- * @package     spiralWebDb\Events\Templates
+ * @package     spiralWebDb\Events\Template
  * @since       1.0.0
  * @author      Robert A. Gadon
  * @link        http://spiralwebdb.com
  * @license     GNU-2.0+
  */
 
-namespace spiralWebDb\Events\Templates;
+namespace spiralWebDb\Events\Template;
 
 return array(
 	'single'            => array(
-		'events' => EVENTS_DIR . '/src/templates/single-events.php',
+		'events' => EVENTS_DIR . '/src/template/single-events.php',
 	),
 	'post_type_archive' => array(
-		'events' => EVENTS_DIR . '/src/templates/archive-events.php',
+		'events' => EVENTS_DIR . '/src/template/archive-events.php',
 	),
 );
