4c52f225ebb09b9d578e37cc1410f9b33c0f0b10
hellofromtonya <hellofromtonya@knowthecode.io>
2018-05-15 11:53:22 -0500

    Wrapped the review's content in a blockquote.

--- plugins/reviews/src/template/single-reviews.php
+++ plugins/reviews/src/template/single-reviews.php
@@ -13,4 +13,28 @@ namespace spiralWebDb\Reviews\Template;
 
 remove_all_actions( 'genesis_entry_header' );
 
+add_action( 'genesis_before_entry_content', __NAMESPACE__ . '\add_opening_blockquote', 999999 );
+/**
+ * Change the markup attributes for the review.
+ *
+ * @since 1.0.0
+ *
+ * @return string
+ */
+function add_opening_blockquote() {
+	printf ('<blockquote class="review review-%s" itemprop="text">', (int) get_the_ID() );
+}
+
+add_action( 'genesis_after_entry_content', __NAMESPACE__ . '\add_closing_blockquote', 0 );
+/**
+ * Change the markup attributes for the review.
+ *
+ * @since 1.0.0
+ *
+ * @return string
+ */
+function add_closing_blockquote() {
+	echo '</blockquote>';
+}
+
 genesis();
