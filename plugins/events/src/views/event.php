7d45084ab9b7974151b3c90991b442b1c91dff29
Robert Gadon <rgadon107@gmail.com>
2018-05-21 22:39:25 -0500

    Add comments to Event view file define tasks for building out the file.

--- plugins/events/src/views/event.php
+++ plugins/events/src/views/event.php
@@ -13,9 +13,14 @@ echo '<p class="entry-meta" itemprop="performance-date">' . 'Performance Date: '
 <article class="post-(ID) events type-events status-publish entry events-301" itemscope
          itemtype="https://schema.org/MusicEvent">
     <header class="entry-header">
-        <h1 class="entry-title" itemprop="headline"><?php $post_title; ?></h1>
-        <p class="entry-meta"><!--Replace Genesis' time & post-edit-link with
-	the performance day and date--></p>
+        <!--Modified 'entry-title' wrap & atts in 'single-events.php' file.-->
+        <!--Filter the entry-title and prepend either the venue image file or
+        <!--use the Font Awesome 'music' icon'-->
+        <h2 class="entry-title" itemprop="location" itemtype="https://schema.org/MusicVenue"><?php $post_title; ?></h2>
+        <!--Build the 'events/src/meta.php' file to call the post metadata-->
+        <!--Build the 'post-meta-before-entry' view file.-->
+        <!--Call the 'post-meta-before-entry' view file to render the entry meta.-->
+        <p class="entry-meta"></p>
     </header>
     <div class="entry-content" itemprop="text"><!--Probably will be empty as
     the $post_content textfield is unused.-->
