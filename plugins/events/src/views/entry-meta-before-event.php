9ca002f68577e277346f08aac32dca361a6ebf02
Robert Gadon <rgadon107@gmail.com>
2018-05-23 00:14:41 -0500

    Add $event_id as class attribute.

--- plugins/events/src/views/entry-meta-before-event.php
+++ plugins/events/src/views/entry-meta-before-event.php
@@ -6,7 +6,7 @@ namespace spiralWebDb\Events;
 
 ?>
 <div class="entry-meta-before-content" itemprop="text" itemtype="performance-date-">
-    <p class="event event-date" itemprop="startDate">
+    <p class="event event-date event-<?php echo $event_id; ?>" itemprop="startDate">
 		<?php render_the_event_meta( $event_id ); ?></p>
 <!--        <p></p>--><!--Performance City & State-->
 <!--        <p></p>--><!--Link to Google Map to venue. Use FA fa-map-marker icon with link.-->
