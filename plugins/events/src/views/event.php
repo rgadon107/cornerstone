b9ffd4e2c9b28c4f588a88b8319131c1444ff815
Robert Gadon <rgadon107@gmail.com>
2018-05-23 00:22:08 -0500

    Reminder note to prepend font awesome icon to post title.

--- plugins/events/src/views/event.php
+++ plugins/events/src/views/event.php
@@ -16,8 +16,10 @@ echo '<p class="entry-meta" itemprop="performance-date">' . 'Performance Date: '
         <!--Modified 'entry-title' wrap & atts in 'single-events.php' file.-->
         <!--Filter the entry-title and prepend either the venue image file or
         <!--use the Font Awesome 'music' icon'-->
+        <!--TODO: Figure out how to prepend FA in a post title.-->
         <h2 class="entry-title" itemprop="location" itemtype="https://schema.org/MusicVenue"><?php $post_title; ?></h2>
         <!--Build the 'events/src/meta.php' file to call the post metadata-->
+        <!--Build the performance day, date, and time.-->
         <!--Build the 'post-meta-before-entry' view file.-->
         <!--Call the 'post-meta-before-entry' view file to render the entry meta.-->
         <p class="entry-meta"></p>
