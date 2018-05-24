2b221cd3ef7e8d3377247da179def147b2b5f4f9
Robert Gadon <rgadon107@gmail.com>
2018-05-24 09:44:18 -0500

    Pass in Font Awesome icon to Events plugin template.
    - Display FA before scheduled performance date and time.

--- plugins/events/src/views/entry-meta-before-event.php
+++ plugins/events/src/views/entry-meta-before-event.php
@@ -7,7 +7,7 @@ namespace spiralWebDb\Events;
 ?>
 <div class="entry-meta-before-content" itemprop="text" itemtype="performance-date-">
     <p class="event event-date event-<?php echo $event_id; ?>" itemprop="startDate">
-		<?php render_the_event_meta( $event_id ); ?></p>
+		<span class="far fa-calendar"></span><?php render_the_event_meta( $event_id ); ?></p>
 <!--        <p></p>--><!--Performance City & State-->
 <!--        <p></p>--><!--Link to Google Map to venue. Use FA fa-map-marker icon with link.-->
 <!--    </p>-->
