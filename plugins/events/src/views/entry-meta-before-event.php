38f9f9fe2bd9901f85ba42bf63ee40664997bf93
Robert Gadon <rgadon107@gmail.com>
2018-05-24 11:28:50 -0500

    Update 'entry-meta-before-event.php' file.
    - Add functions from 'meta.php' file to render the event date,
    time,address, and maplink.
    
    - Add or update view file microdata.

--- plugins/events/src/views/entry-meta-before-event.php
+++ plugins/events/src/views/entry-meta-before-event.php
@@ -5,9 +5,9 @@
 namespace spiralWebDb\Events;
 
 ?>
-<div class="entry-meta-before-content" itemprop="text" itemtype="performance-date-">
+<div class="entry-meta-before-content" itemprop="text" itemtype="performance-information">
     <p class="event event-date event-<?php echo $event_id; ?>" itemprop="startDate">
-		<span class="far fa-calendar"></span><?php render_the_event_meta( $event_id ); ?></p>
-<!--        <p></p>--><!--Performance City & State-->
-<!--        <p></p>--><!--Link to Google Map to venue. Use FA fa-map-marker icon with link.-->
-<!--    </p>-->
+		<span class="far fa-calendar"></span><?php render_performance_date_and_time( $event_id ); ?></p>
+    <p class="event-address" itemprop="address" itemscope itemtype="https://schema.org/PostalAddress"><?php render_performance_address( $event_id ); ?></p>
+    <p class="event-map" itemprop="hasMap"><span class="fas fa-map-marker"></span><a itemprop="map" itemtype="https://schema.org/Map" href="<?php render_event_map( $event_id ); ?>" target="_blank">Google Map</a></p>
+</div>
