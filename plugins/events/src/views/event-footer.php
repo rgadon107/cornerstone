cfc3217a3a4049dfe2faad5aa0f9edfdc7e8d18c
Robert Gadon <rgadon107@gmail.com>
2018-05-25 17:38:17 -0500

    Update event-footer.php file with separate view files.

--- plugins/events/src/views/event-footer.php
+++ plugins/events/src/views/event-footer.php
@@ -6,22 +6,11 @@ namespace spiralWebDb\Events;
 
 ?>
 <footer class="entry-footer">
-    <div class="sponsor_tel-number">
-        <span class="fas fa-mobile-alt" itemprop="telephone"
-              itemtype="http://schema.org/ContactPoint"></span><?php render_event_tel_number( $event_id ); ?>
-    </div>
+    <div class="wrap--sponsor-tel-number"><?php render_event_tel_number( $event_id ); ?></div>
     <hr>
+    <div class="web-site sponsor-web-site"><?php render_event_url( $event_id ); ?></div>
     <div class="social-links sponsor-social-links">
-        <div><span class="fas fa-globe" itemprop="url" itemtype="http://schema.org/WebSite"></span>
-            <a href="<?php render_event_website( $event_id ); ?>" target="_blank">Web</a>
-        </div>
-        <div><span class="fab fa-facebook-square" itemprop="sharedContent"
-                   itemtype="http://schema.org/SocialMediaPosting"></span>
-            <a href="<?php render_event_facebook_link( $event_id ); ?>" target="_blank">Facebook</a>
-        </div>
-        <div><span class="fab fa-twitter-square" itemprop="sharedContent"
-                   itemtype="http://schema.org/SocialMediaPosting"></span>
-            <a href="<?php render_event_twitter_link( $event_id ); ?>" target="_blank">Twitter</a>
-        </div>
+        <div><?php render_event_facebook_link( $event_id ); ?></div>
+        <div><?php render_event_twitter_link( $event_id ); ?></div>
     </div>
 </footer>
