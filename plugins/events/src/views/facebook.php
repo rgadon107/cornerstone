6c20d54d3ae1868d475889ad9593b75f8d006bc8
Robert Gadon <rgadon107@gmail.com>
2018-05-25 17:38:33 -0500

    Build facebook.php view file.

---
+++ plugins/events/src/views/facebook.php
@@ -0,0 +1,1 @@
+<a href="<?php echo esc_url( 'https://facebook.com/' . $facebook ); ?>" target="_blank" itemprop="sharedContent" itemtype="http://schema.org/SocialMediaPosting"><span class="fab fa-facebook-square" aria-label="Facebook" role="img"></span> Facebook</a>
