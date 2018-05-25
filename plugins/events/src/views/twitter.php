1ee38e33a44f557d7e68a4a878aa19c677f60741
Robert Gadon <rgadon107@gmail.com>
2018-05-25 17:39:17 -0500

    Build twitter.php view file.

---
+++ plugins/events/src/views/twitter.php
@@ -0,0 +1,3 @@
+<a href="<?php echo esc_url( 'https://twitter.com/' . $twitter . '?lang=en' ) ?>" target="_blank" itemprop="sharedContent"
+   itemtype="http://schema.org/SocialMediaPosting">
+    <span class="fab fa-twitter-square" aria-label="Twitter" role="img"></span> Twitter</a>
