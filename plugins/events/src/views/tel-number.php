15ecaf6a0387cc56c41e9c322bd1830bd836d444
Robert Gadon <rgadon107@gmail.com>
2018-05-26 16:46:51 -0500

    Change markup in `tel_number` view file.
    - Output now matches web and social-links as `<li>`.

--- plugins/events/src/views/tel-number.php
+++ plugins/events/src/views/tel-number.php
@@ -1,3 +1,3 @@
-<p class="sponsor_tel-number" itemprop="telephone" itemtype="http://schema.org/ContactPoint">
+<li class="sponsor_tel-number" itemprop="telephone" itemtype="http://schema.org/ContactPoint">
     <span class="fas fa-mobile-alt" </span> Phone: <?php echo esc_html( $tel_number ); ?>
-</p>
+</li>
