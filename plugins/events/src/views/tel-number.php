e897b0b590bdbff551421dfc7af40340b522a64e
Robert Gadon <rgadon107@gmail.com>
2018-05-26 16:07:26 -0500

    Format and escape $tel_number metadata passed to view.

--- plugins/events/src/views/tel-number.php
+++ plugins/events/src/views/tel-number.php
@@ -1,3 +1,3 @@
 <p class="sponsor_tel-number" itemprop="telephone" itemtype="http://schema.org/ContactPoint">
-        <span class="fas fa-mobile-alt" </span> Phone: <?php echo $tel_number ?>
+    <span class="fas fa-mobile-alt" </span> Phone: <?php echo esc_html( $tel_number ); ?>
 </p>
