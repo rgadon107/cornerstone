ee454c7e37cc0c56a7e8c8c2f49966f0ee03b953
Robert Gadon <rgadon107@gmail.com>
2018-05-26 16:45:46 -0500

    Move markup from the `event-footer` file to the view file.

--- plugins/events/src/views/event-footer.php
+++ plugins/events/src/views/event-footer.php
@@ -5,11 +5,9 @@
 namespace spiralWebDb\Events;
 
 ?>
-<ul>
-    <li class="tel-number sponsor-tel-number"><?php render_event_tel_number( $event_id ); ?></li>
-    <hr>
+<ul class="event-footer">
+    <?php render_event_tel_number( $event_id ); ?>
     <li class="web-site sponsor-web-site"><?php render_event_url( $event_id ); ?></li>
-
     <ul class="social-links sponsor-social-links">
         <li><?php render_event_facebook_link( $event_id ); ?></li>
         <li><?php render_event_twitter_link( $event_id ); ?></li>
