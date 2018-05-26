4b35de4f6dada4e42a940a6dc02c2b5ffd8f46df
Robert Gadon <rgadon107@gmail.com>
2018-05-26 16:45:06 -0500

    Escape class attribute for $event_id.

--- plugins/events/src/views/entry-meta-before-event.php
+++ plugins/events/src/views/entry-meta-before-event.php
@@ -6,8 +6,10 @@ namespace spiralWebDb\Events;
 
 ?>
 <div class="entry-meta-before-content" itemprop="text" itemtype="performance-information">
-    <p class="event event-date event-<?php echo $event_id; ?>" itemprop="startDate">
-		<span class="far fa-calendar"></span><?php render_performance_date_and_time( $event_id ); ?></p>
-    <p class="event-address" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress"><?php render_performance_address( $event_id ); ?></p>
-    <p class="event-map" itemprop="hasMap"><span class="fas fa-map-marker"></span><a itemprop="map" itemtype="http://schema.org/Map" href="<?php render_event_map( $event_id ); ?>" target="_blank">Google Map</a></p>
+    <p class="event event-date event-<?php echo esc_attr( $event_id ); ?>" itemprop="startDate startTime">
+        <span class="far fa-calendar"></span><?php render_performance_date_and_time( $event_id ); ?></p>
+    <p class="event-address" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+        <?php render_performance_address( $event_id ); ?></p>
+    <p class="event-map" itemprop="hasMap"><span class="fas fa-map-marker"></span>
+        <a itemprop="map" itemtype="http://schema.org/Map" href="<?php render_event_map( $event_id ); ?>" target="_blank"> Google Map</a></p>
 </div>
