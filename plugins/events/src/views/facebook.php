95f4b7ee8dae157db0a6ec69e485ab387fdacc99
Robert Gadon <rgadon107@gmail.com>
2018-05-26 16:06:56 -0500

    Format and escape $facebook metadata passed to view.

--- plugins/events/src/views/facebook.php
+++ plugins/events/src/views/facebook.php
@@ -1,1 +1,3 @@
-<a href="<?php echo esc_url( 'https://facebook.com/' . $facebook ); ?>" target="_blank" itemprop="sharedContent" itemtype="http://schema.org/SocialMediaPosting"><span class="fab fa-facebook-square" aria-label="Facebook" role="img"></span> Facebook</a>
+<a href="<?php echo esc_url( $facebook ); ?>" target="_blank" itemprop="sharedContent"
+   itemtype="http://schema.org/SocialMediaPosting"><span class="fab fa-facebook-square" aria-label="Facebook"
+                                                         role="img"></span> Facebook</a>
