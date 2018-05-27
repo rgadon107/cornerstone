84f57006f0c9e71fee23cbf21da7fc5ed4b9b496
Robert Gadon <rgadon107@gmail.com>
2018-05-26 23:37:28 -0500

    Build the 'before-entry-header' view file.

---
+++ plugins/events/src/views/before-entry-header.php
@@ -0,0 +1,3 @@
+<img class="first has-post-thumbnail event-thumbnail" src="<?php spiralWebDb\Events\render_event_venue_image( $event_id ); ?>"
+     itemprop="thumbnail" itemscope
+     itemtype="http://schema.org/ImageObject">
