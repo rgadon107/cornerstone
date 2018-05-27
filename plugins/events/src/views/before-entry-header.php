414cd0e65a2b6d4b13ad93e8f04b12c66c1b8243
Robert Gadon <rgadon107@gmail.com>
2018-05-26 23:47:35 -0500

    Declare namespace and add description to header of view file.

--- plugins/events/src/views/before-entry-header.php
+++ plugins/events/src/views/before-entry-header.php
@@ -1,3 +1,9 @@
-<img class="first has-post-thumbnail event-thumbnail" src="<?php spiralWebDb\Events\render_event_venue_image( $event_id ); ?>"
+<?php
+/*
+ * Before entry header view file. 
+ */
+namespace spiralWebDb\Events;
+?>
+<img class="first has-post-thumbnail event-thumbnail" src="<?php render_event_venue_image( $event_id ); ?>"
      itemprop="thumbnail" itemscope
      itemtype="http://schema.org/ImageObject">
