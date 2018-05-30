c49b256797c55ab1907fa08f8625bb447607f114
Robert Gadon <rgadon107@gmail.com>
2018-05-29 22:57:39 -0500

    Relocate FA markup _before_ anchor tag.

--- plugins/events/src/views/facebook.php
+++ plugins/events/src/views/facebook.php
@@ -1,3 +1,3 @@
-<a href="<?php echo esc_url( $facebook ); ?>" target="_blank" itemprop="sharedContent"
-   itemtype="http://schema.org/SocialMediaPosting"><span class="fab fa-facebook-square" aria-label="Facebook"
-                                                         role="img"></span> Facebook</a>
+<span class="fab fa-facebook-square" aria-label="Facebook"
+      role="img"></span><a href="<?php echo esc_url( $facebook ); ?>" target="_blank" itemprop="sharedContent"
+                           itemtype="http://schema.org/SocialMediaPosting"> Facebook</a>
