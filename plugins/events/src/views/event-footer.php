2ee400b737973a40dcf0449636fac06bb003f32c
Robert Gadon <rgadon107@gmail.com>
2018-05-29 22:56:39 -0500

    Change file HTML
    - Add `<div>` for 'entry-footer-wrap'.
    - Add `<h3>` for 'after-entry-header'. Announce 'Sponsor Contact
    Information'.

--- plugins/events/src/views/event-footer.php
+++ plugins/events/src/views/event-footer.php
@@ -5,11 +5,14 @@
 namespace spiralWebDb\Events;
 
 ?>
-<ul class="event-footer">
-    <?php render_event_tel_number( $event_id ); ?>
-    <li class="web-site sponsor-web-site"><?php render_event_url( $event_id ); ?></li>
-    <ul class="social-links sponsor-social-links">
-        <li><?php render_event_facebook_link( $event_id ); ?></li>
-        <li><?php render_event_twitter_link( $event_id ); ?></li>
+<div class="entry-footer-wrap">
+    <h3 class="after-entry-header" itemprop="text">Sponsor Contact Information</h3>
+    <ul class="event-footer">
+		<?php render_event_tel_number( $event_id ); ?>
+        <li class="web-site sponsor-web-site"><?php render_event_url( $event_id ); ?></li>
+        <ul class="social-links sponsor-social-links">
+            <li><?php render_event_facebook_link( $event_id ); ?></li>
+            <li><?php render_event_twitter_link( $event_id ); ?></li>
+        </ul>
     </ul>
-</ul>
+</div>
