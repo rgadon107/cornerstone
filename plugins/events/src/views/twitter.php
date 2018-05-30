9b3f1c1eec81200b738bb6a1cb6730bc8d2813a9
Robert Gadon <rgadon107@gmail.com>
2018-05-29 23:01:31 -0500

    Relocate FA markup _before_ anchor tag.

--- plugins/events/src/views/twitter.php
+++ plugins/events/src/views/twitter.php
@@ -1,3 +1,4 @@
-<a href="<?php echo esc_url( $twitter ) ?>" target="_blank" itemprop="sharedContent"
-   itemtype="http://schema.org/SocialMediaPosting">
-    <span class="fab fa-twitter-square" aria-label="Twitter" role="img"></span> Twitter</a>
+<span class="fab fa-twitter-square" aria-label="Twitter" role="img"></span><a href="<?php echo esc_url( $twitter ) ?>"
+                                                                              target="_blank" itemprop="sharedContent"
+                                                                              itemtype="http://schema.org/SocialMediaPosting">
+    Twitter</a>
