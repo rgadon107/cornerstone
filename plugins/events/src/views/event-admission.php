742e1ef0b5608364365bad66e59110dd187164e5
Robert Gadon <rgadon107@gmail.com>
2018-06-03 03:26:44 -0500

    New single performance admission view file.

---
+++ plugins/events/src/views/event-admission.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ *  The single performance admission view.
+ */
+namespace spiralWebDb\Events;
+?>
+ <p class="event-admission" itemprop="price" itemtype="http://schema.org/Ticket"><span class="fas fa-ticket-alt"></span><?php render_addmission_information( $event_id ); ?></p>
