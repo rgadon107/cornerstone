0e8b902edef9f9c584eb5fc5bfe4ca0f3b722bc2
Robert Gadon <rgadon107@gmail.com>
2018-06-03 03:24:26 -0500

    Build the archive-events.php template file.

--- plugins/events/src/template/archive-events.php
+++ plugins/events/src/template/archive-events.php
@@ -11,47 +11,26 @@
 
 namespace spiralWebDb\Events\Template;
 
-ddd( 'Loaded the archive-events template' );
+remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
 
-remove_action( 'genesis_loop', 'genesis_do_loop' );
-add_action( 'genesis_loop', __NAMESPACE__ . '\loop_and_render_events' );
-
-/**
- *  Loop through and render out the Events
+add_action( 'genesis_entry_header', __NAMESPACE__ . '\render_event_archive_performance_data' );
+/*
+ * Render event archive performance meta data
  *
  * @since 1.0.0
  *
- * @param array $events
- *
- * @return void
+ * @return voic
  */
-function loop_and_render_events( array $events ) {
+function render_event_archive_performance_data() {
+	$event_id = (int) get_the_ID();
+	echo sprintf( '<div %s>', genesis_attr( 'before-entry-content-meta' ) );
 
-	foreach ( $events as $event ) {
-		$post_title = $faq['post_title'];
-		$content    = do_shortcode( $faq['post_content'] );
-		$show_icon  = esc_attr( $attributes['show_icon'] );
-		$hide_icon  = esc_attr( $attributes['hide_icon'] );
-
-		include _get_plugin_directory() . '/src/views/faq.php';
-	}
+	require dirname( __DIR__ ) . '/views/performance-date-time.php';
+	require dirname( __DIR__ ) . '/views/event-map.php';
 }
 
-// 1. Remove the default genesis_loop and replace with a custom events loop
-// to display all the single events. Review `wp-content/plugins/faq/src/template/archive-faq.php`
-// and walk the code. Determine how the faqs get rendered. What will apply in this
-// plugin?
+remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
 
-// 2. Get all the records for the 'events' post type.
-// Question: If I don't have a custom taxonomy term to add to my db query,
-// would I use the global $wp_query object instead to get the post_id and post_title?
-
-// 3. If there are no records to display, render a message that there are no records.
-
-// 4. Call the container view file for archive-events.
-
-// 5. Loop through and render the single-events view file to build the archive view.
-// Question: Can I customize the loop to render less data than what's presented in the
-// single-events.php template?
+require __DIR__ . '/single-events.php';
 
 genesis();
