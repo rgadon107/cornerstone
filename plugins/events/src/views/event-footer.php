32a45b2477daac8fd9da73e88091ded522fe732f
Robert Gadon <rgadon107@gmail.com>
2018-06-03 22:32:59 -0500

    Modify 'event-footer' view file to hide/show hidden content.

--- plugins/events/src/views/event-footer.php
+++ plugins/events/src/views/event-footer.php
@@ -5,13 +5,23 @@
 namespace spiralWebDb\Events;
 
 ?>
-<div class="entry-footer-wrap">
-    <h3 class="after-entry-header" itemprop="text">Sponsor Contact Information</h3>
-    <ul class="event-footer">
-        <li class="first sponsor_tel-number" itemprop="telephone"
-            itemtype="http://schema.org/ContactPoint"><?php render_event_tel_number( $event_id ); ?></li>
-        <li class="web-site sponsor-web-site"><?php render_event_url( $event_id ); ?></li>
-        <li class="sponsor-facebook"><?php render_event_facebook_link( $event_id ); ?></li>
-        <li class="sponsor-twitter"><?php render_event_twitter_link( $event_id ); ?></li>
-    </ul>
+<div class="event event-<?php echo esc_attr( $event_id ); ?>" itemprop="organizer" itemscope
+     itemtype="http://schema.org/MusicEvent">
+    <div class="revealer--visible">
+        <span class="revealer--icon <?php echo $show_icon; ?>" aria-hidden="true"
+              data-show-icon="<?php echo $show_icon; ?>"
+              data-hide-icon="<?php echo $hide_icon; ?>">
+             <span class="screen-reader-text">Click to reveal the event sponsor contact information.</span>
+            <h3 class="event-footer-header" itemprop="text">Sponsor Contact Information</h3>
+        </span>
+    </div>
+    <div class="revealer--hidden" itemprop="description" style="display: none;">
+        <ul class="event-footer-content">
+            <li class="first sponsor-tel-number" itemprop="telephone"
+                itemtype="http://schema.org/ContactPoint"><?php render_event_tel_number( $event_id ); ?></li>
+            <li class="web-site sponsor-web-site"><?php render_event_url( $event_id ); ?></li>
+            <li class="sponsor-facebook"><?php render_event_facebook_link( $event_id ); ?></li>
+            <li class="sponsor-twitter"><?php render_event_twitter_link( $event_id ); ?></li>
+        </ul>
+    </div>
 </div>
