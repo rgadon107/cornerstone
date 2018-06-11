36ede053bb41e15101c0f03c27412d47cdd3a8b3
Robert Gadon <rgadon107@gmail.com>
2018-06-11 13:56:50 -0500

    Add plugin.php file to src directory. Modify main query.

---
+++ plugins/reviews/src/plugin.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin specific tasks.
+ *
+ * @package     spiralWebDb\Reviews
+ * @since       1.0.0
+ * @author      Robert A. Gadon
+ * @link        http://spiralwebdb.com
+ * @license     GNU-2.0+
+ */
+
+namespace spiralWebDb\Reviews;
+
+add_action( 'pre_get_posts', __NAMESPACE__ . '\set_reviews_archive_order' );
+/**
+ * Modify the query for the reviews' archive page, i.e. to set the events order.
+ *
+ * @since 1.0.0
+ *
+ * @param WP_Query $query Instance of the query.
+ */
+function set_reviews_archive_order( $query ) {
+
+	if ( is_admin() || ! $query->is_main_query() ) {
+		return;
+	}
+
+	if ( ! is_post_type_archive( 'reviews' ) ) {
+		return;
+	}
+
+	$query->set( 'order', 'ASC' );
+	$query->set( 'orderby', 'menu_order' );
+}
