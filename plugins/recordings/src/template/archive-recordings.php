03c5cef29c9d81574eb4841baf006b7a3e26b4b6
Robert Gadon <rgadon107@gmail.com>
2018-06-16 16:46:12 -0500

    Build the archive-recordings template.
    - remove_action( 'genesis_post_content') doesn't work.
    - adding post_classes filter doesn't work.

--- plugins/recordings/src/template/archive-recordings.php
+++ plugins/recordings/src/template/archive-recordings.php
@@ -11,6 +11,74 @@
 
 namespace spiralWebDb\Recordings\Template;
 
-ddd( 'Loaded the archive-recordings template.' );
+require __DIR__ . '/single-recordings.php';
+//ddd( 'Yup, you called me!' );
+function unregister_genesis_content() {
+	remove_action( 'genesis_entry_content', __NAMESPACE__ . '\reveal_recording_song_titles', 12 );
+}
 
-genesis();
+add_filter( 'post_classes', __NAMESPACE__ . '\add_to_post_classes_for_grid_pattern' );
+/**
+ * Add to the post classes.
+ *
+ * @since 1.0.0.
+ *
+ * @param array $classes Post classes.
+ *
+ * @return array
+ */
+function add_to_post_classes_for_grid_pattern( array $classes ) {
+	ddd( 'Yup, you called me. ');
+	if ( is_admin() || is_single() ) {
+		return $classes;
+	}
+
+	if ( ! is_post_type_archive( 'recordings' ) ) {
+		return $classes;
+	}
+
+	return get_classes_for_grid_pattern( $classes, 3 );
+}
+
+/*
+ * Based on the number of columns requested, get the styling classes for the grid
+ * pattern.
+ *
+ * @since 1.0.0
+ *
+ * @param array $classes            Post classes.
+ * @param int   $number_of_classes  Number of columns to set for this grid pattern.
+ *
+ * @return array
+ */
+function get_classes_for_grid_pattern( array $classes, $number_of_columns = 2 ) {
+
+	if ( $number_of_columns < 2 || $number_of_columns > 6 ) {
+		return $classes;
+	}
+
+	global $wp_query;
+
+	/**
+	 * The element position within the array lines up with the $number_of_columns.
+	 * If $number_of_columns = 3, then the 4th array element is equal to 'one-third'.
+	 */
+	$column_classes = array(
+		'',             // index 0
+		'',             // index 1
+		'one-half',     // index 2
+		'one-third',    // index 3
+		'one-fourth',   // index 4
+		'one-fifth',    // index 5
+		'one-sixth',    // index 6
+	);
+
+	// Copy the column class out of the array and add it to the end of the $classes array.
+	$classes[] = $column_classes[ $number_of_columns ];
+
+	if ( $wp_query->$current_post % $number_of_columns == 0 ) {
+		$classes[] = 'first';
+	}
+
+	return $classes;
+}
