19e13f6666c023c72702316ddc8a0d9386f817c2
hellofromtonya <hellofromtonya@knowthecode.io>
2018-06-18 12:30:13 -0500

    Simplified adding grid classes to recordings.

--- plugins/recordings/src/template/archive-recordings.php
+++ plugins/recordings/src/template/archive-recordings.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template for the events post type archive.
+ * Template for the recordings post type archive.
  *
  * @package     spiralWebDb\Recordings\Template
  * @since       1.0.0
@@ -34,81 +34,21 @@ add_filter( 'post_class', __NAMESPACE__ . '\add_to_post_classes_for_grid_pattern
  * @return array
  */
 function add_to_post_classes_for_grid_pattern( array $classes ) {
-
-	if ( is_admin() || is_single() ) {
-		return $classes;
-	}
-
-	if ( ! is_post_type_archive( 'recordings' ) ) {
-		return $classes;
-	}
-
-	return get_classes_for_grid_pattern( $classes, 3 );
-}
-
-/*
- * Based on the number of columns requested, get the styling classes for the grid
- * pattern.
- *
- * @since 1.0.0
- *
- * @param array $classes            Post classes.
- * @param int   $number_of_columns  Number of columns to set for this grid pattern.
- *
- * @return array
- */
-function get_classes_for_grid_pattern( array $classes, $number_of_columns = 2 ) {
-
-	if ( $number_of_columns < 2 || $number_of_columns > 6 ) {
-		return $classes;
-	}
-
 	static $recording_count = 0;
 
-	/**
-	 * The element position within the array lines up with the $number_of_columns.
-	 * If $number_of_columns = 3, then the 4th array element is equal to 'one-third'.
-	 */
-	$column_classes = array(
-		'',             // index 0
-		'',             // index 1
-		'one-half',     // index 2
-		'one-third',    // index 3
-		'one-fourth',   // index 4
-		'one-fifth',    // index 5
-		'one-sixth',    // index 6
-	);
-
-	// Copy the column class out of the array above, and add it to the end of the $classes array.
-	// Limit the addition of column classes to 'recordings' post types only.
-	if ( is_post_of_post_type() ) {
-		$classes[] = $column_classes[ $number_of_columns ];
+	if ( is_admin() ) {
+		return $classes;
 	}
 
-	if ( $recording_count % $number_of_columns == 0 ) {
+	$classes[] = 'one-third';
+
+	if ( $recording_count % 3 === 0 ) {
 		$classes[] = 'first';
 	}
 
-	$recording_count ++;
+	$recording_count++;
 
 	return $classes;
-}
-
-/**
- * Checks if the current (or specified) post is of the specified post type.
- *
- * @since 1.0.0
- *
- * @param string           $post_type
- * @param int|WP_Post|null $post_or_post_id     Post ID or post object. When `null`,
- *                                              WordPress uses global $post.
- *
- * @uses  get_post_type()   Retrieve the post type of the current post or of a given post.
- *
- * @return bool
- */
-function is_post_of_post_type( $post_type = 'recordings', $post_or_post_id = null ) {
-	return get_post_type( $post_or_post_id ) == $post_type;
 }
 
 require __DIR__ . '/single-recordings.php';
