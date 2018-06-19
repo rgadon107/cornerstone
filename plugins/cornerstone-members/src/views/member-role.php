2abb5b5b8913709e8d7d52449f2717818bc6aca2
Robert Gadon <rgadon107@gmail.com>
2018-06-19 18:31:36 -0500

    Add Cornerstone Members view files.

---
+++ plugins/cornerstone-members/src/views/member-role.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * The single Cornerstone member role view.
+ */
+
+namespace spiralWebDb\Members;
+?>
+<h4 class="member-role two-thirds" itemprop="memberOf" itemtype="http://schema.org/MusicGroup" ><em><?php render_the_member_role( $member_id ); ?></em></h4>
---
+++ plugins/cornerstone-members/src/views/number-of-tours.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * The single Cornerstone member tour number view.
+ */
+
+namespace spiralWebDb\Members;
+?>
+<h4 class="member-tour-number" itemprop="memberOf" itemtype="http://schema.org/MusicGroup" ><?php render_number_of_cornerstone_tours( $member_id ); ?></h4>
