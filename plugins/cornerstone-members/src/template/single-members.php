744710b9b5424914ab6945f50de241fab0d50967
Robert Gadon <rgadon107@gmail.com>
2018-06-19 16:33:30 -0500

    Itemizing the tasks to accomplish in the single-members.php file.

--- plugins/cornerstone-members/src/template/single-members.php
+++ plugins/cornerstone-members/src/template/single-members.php
@@ -11,6 +11,18 @@
 
 namespace spiralWebDb\Members\Template;
 
-ddd( 'Loading the single-members template' );
+d( 'Loading the single-members template' );
+
+//Render the featured image for each Cornerstone Member
+
+//Render each member's role
+
+//Render each member's name (post_title)
+//If that's the case, do we really need the name in the post_meta?
+//No, we don't b/c we can order 'members' posts by menu order.
+
+//Render each member's bio (the_content)
+
+//Render the number of tours this member has participated in (include the current tour)
 
 genesis();
