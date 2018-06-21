e2a48c481ce1fb66789d13a4507d6ad2ae3e98a7
Robert Gadon <rgadon107@gmail.com>
2018-06-21 18:54:59 -0500

    Added new view file to render the performance community.

---
+++ plugins/events/src/views/event-community.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *  The single performance community (City, State) view.
+ */
+namespace spiralWebDb\Events;
+
+?>
+<h4 class="event-location" itemprop="location" itemtype="http://schema.org/MusicEvent">
+	<?php render_the_performance_community( $event_id ); ?></h4>
