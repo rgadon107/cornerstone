9de9e21086fbbe53111e86fd9c7a4c81e1698edc
Robert Gadon <rgadon107@gmail.com>
2018-07-16 16:53:23 -0400

    Build front-page.php file
    - Markup widget areas.
    - Use 'load_archive_template()' to render Events archive on front page.

--- themes/developers/front-page.php
+++ themes/developers/front-page.php
@@ -4,12 +4,46 @@
  *
  * @package     spiralWebDB\FrontPage
  * @since       1.0.0
- * @author     Robert A. Gadon
- * @link       http://spiralwebdb.com
- * @license    GNU-2.0+
+ * @author      Robert A. Gadon
+ * @link        http://spiralwebdb.com
+ * @license     GNU-2.0+
  */
+
 namespace spiralWebDB\FrontPage;
 
-remove_action( 'genesis_loop', 'genesis_do_loop' );
+use function spiralWebDB\Events\load_archive_template;
 
-genesis();
+//remove_action( 'genesis_loop', 'genesis_do_loop' );
+
+add_action( 'genesis_before_content_sidebar_wrap', __NAMESPACE__ . '\add_widget_areas' );
+/**
+ *
+ */
+function add_widget_areas() {
+
+	if ( ! is_front_page() ) {
+		return;
+	}
+
+	genesis_widget_area( 'welcome_widget', array(
+		'before' => '<div class="welcome"><div class="wrap">',
+		'after'  => '</div></div>',
+	) );
+
+	genesis_widget_area( 'reviews_bar_1', array(
+		'before' => '<div class="reviews_1"><div class="wrap">',
+		'after'  => '</div></div>',
+	) );
+
+	genesis_widget_area( 'events', array(
+		'before' => '<div class="events_widget"><div class="wrap">',
+		'after'  => '</div></div>',
+	) );
+
+	genesis_widget_area( 'reviews_bar_2', array(
+		'before' => '<div class="reviews_2"><div class="wrap">',
+		'after'  => '</div></div>',
+	) );
+}
+
+load_archive_template();
