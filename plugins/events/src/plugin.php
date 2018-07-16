55d10f441bd53aa118d6d7e92ac5a11ea386fa82
Robert Gadon <rgadon107@gmail.com>
2018-07-16 16:51:35 -0400

    Update Events plugin.php file.
    - Set $query_vars to render Events archive on front-page.
    - Declare function to call archive-events file on front-page.

--- plugins/events/src/plugin.php
+++ plugins/events/src/plugin.php
@@ -25,11 +25,35 @@ function set_events_archive_order( $query ) {
 		return;
 	}
 
-	if ( ! is_post_type_archive( 'events' ) ) {
+	// Whoops, It's the posts page! Bail out.
+	if ( $query->is_home && $query->is_posts_page ) {
 		return;
 	}
 
-	$query->set( 'posts_per_archive_page', 20 );
+	// Checks to see if it's either the front or archive page. If no, bail out.
+	if ( ! $query->is_home && ! is_post_type_archive( 'events' ) ) {
+		return;
+	}
+
+	// Set the query vars for the front page.
+	if ( $query->is_home && ! $query->is_posts_page ) {
+		$query->set( 'post_type', array( 'events' ) );
+		$query->set( 'posts_per_page', 20 );
+		// Set the query vars for archive page.
+	} else {
+		$query->set( 'posts_per_archive_page', 20 );
+	}
 	$query->set( 'order', 'ASC' );
 	$query->set( 'orderby', 'menu_order' );
+}
+
+/*
+ *  Loads the archive template.
+ *
+ *  @since 1.0.0.
+ *
+ *  @return void
+ */
+function load_archive_template() {
+	require __DIR__ . '/template/archive-events.php';
 }
