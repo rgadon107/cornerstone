c8cd7544e209922e418164be755ca5b4fb1efa62
Robert Gadon <rgadon107@gmail.com>
2018-07-30 17:39:32 -0500

    Modify archive-events template file to render on front-page.

--- plugins/events/src/template/archive-events.php
+++ plugins/events/src/template/archive-events.php
@@ -12,6 +12,18 @@
 namespace spiralWebDb\Events\Template;
 use function spiralWebDb\Events\render_event_map;
 
+add_action( 'genesis_before_loop', __NAMESPACE__ . '\render_events_header');
+/*
+ * Render archive Events header.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function render_events_header() {
+	require dirname( __DIR__ ) . '/views/events-header.php';
+}
+
 remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
 
 add_action( 'genesis_entry_header', __NAMESPACE__ . '\render_event_archive_performance_data' );
@@ -27,8 +39,10 @@ function render_event_archive_performance_data() {
 	echo sprintf( '<div %s>', genesis_attr( 'before-entry-content-meta' ) );
 
 	require dirname( __DIR__ ) . '/views/performance-date-time.php';
-	render_event_map( $event_id );
+
 }
+
+remove_all_filters( 'genesis_entry_content' );
 
 remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
 
