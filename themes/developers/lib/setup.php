7c1aa24de13422ab0e501cbeb59ce07d1df1d0a9
Robert Gadon <rgadon107@gmail.com>
2018-08-07 16:35:08 -0500

    Disable theme support for a custom-header area.

--- themes/developers/lib/setup.php
+++ themes/developers/lib/setup.php
@@ -65,13 +65,7 @@ function adds_theme_supports() {
 			'skip-links'
 		),
 		'genesis-responsive-viewport'     => null,
-		'custom-header'                   => array(
-			'width'           => 600,
-			'height'          => 160,
-			'header-selector' => '.site-title a',
-			'header-text'     => false,
-			'flex-height'     => true,
-		),
+		'custom-header'                   => null,
 		'custom-background'               => null,
 		'genesis-after-entry-widget-area' => null,
 //		'genesis-footer-widgets'          => 3,
