0f66cb6a653962e32dfa638cac8eb0b6098318d6
Tonya Mork <tonya.mork@xwp.co>
2018-09-03 11:59:26 -0500

    Completed the members shortcode.

--- plugins/cornerstone-members/src/shortcode/members.php
+++ plugins/cornerstone-members/src/shortcode/members.php
@@ -11,6 +11,8 @@
 
 namespace spiralWebDb\Members\Shortcode;
 
+use function spiralWebDb\Members\Template\get_members_post_classes;
+
 /**
  * Process the Members [members] shortcode.
  *
@@ -26,8 +28,31 @@ function process_members_shortcode( array $config, array $attributes ) {
 	$doing_single = ! empty( $attributes['member_id'] );
 	$query        = get_members_query( $config['query_args'], $attributes );
 
+	// None found. Return the appropriate message.
+	if ( ! $query->have_posts() ) {
+		$message = $doing_single
+			? $config['defaults']['none_found_single']
+			: $config['defaults']['none_found'];
+
+		return sprintf( '<p>%s</p>', esc_html( $message ) );
+	}
+
+	$members_count = 0;
+
 	// Call the view file, capture it into the output buffer, and then return it.
 	ob_start();
+
+	while ( $query->have_posts() ) {
+		$query->the_post();
+		$member_id       = (int) get_the_ID();
+		$article_classes = $doing_single
+			? "post-{$member_id} type-member"
+			: get_members_post_classes( $members_count, $member_id, array( 'grid-list' ) );
+
+		require __DIR__ . '/views/members.php';
+
+		$members_count++;
+	}
 
 	$html = ob_get_clean();
 
