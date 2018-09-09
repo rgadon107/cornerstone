14c9aa723b3eb7961cf682517c81f52db7cb4b77
Robert Gadon <rgadon107@gmail.com>
2018-09-08 23:34:37 -0500

    Events shortcode view file -- initial commit.

---
+++ plugins/events/src/shortcode/views/events.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Events shortcode view.
+ *
+ * @package    spiralWebDb\Events\Shortcode
+ * @since      1.0.0
+ * @author     Robert A. Gadon
+ * @link       http://spiralwebdb.com
+ * @license    GPL-2.0+
+ */
+
+namespace spiralWebDb\Events\Shortcode;
+
+use function spiralWebDb\Events\{name_of_function};
+
+?>
+
+<!--Begin HTML-->
+<!--See '/cornerstone-members/src/shortcode/views/members.php' for guidance-->
+<!--In the markup, add the variable $article_classes to the class="" attribute.-->
