aa8d5f44215b12f8e6e4eeedb4e3469dc2ffa910
Robert Gadon <rgadon107@gmail.com>
2018-09-08 23:35:00 -0500

    Events shortcode template helpers file -- initial commit.

---
+++ plugins/events/src/template/helpers.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Helper functions for the templates and views.
+ *
+ * @package     spiralWebDb\Events\Template
+ * @since       1.0.0
+ * @author      Robert A. Gadon
+ * @link        http://spiralwebdb.com
+ * @license     GNU-2.0+
+ */
+
+namespace spiralWebDb\Events\Template;
+
+// Call functions from plugin meta.php file if needed.
+// use function spiralWebDb\Events\{name_of_function};
+
+// If we're displaying the events in a grid, call
+// function get_members_post_classes. See'/cornerstone-members/src/template/helpers.php'
+// for guidance.
+
+// Call function from meta.php file to render the post_thumbnail_before_title
+
+// Call function from meta.php file to render the performance city & state.
+
+// Call functions from meta.php file to render the entry post-meta and entry footer.
+
