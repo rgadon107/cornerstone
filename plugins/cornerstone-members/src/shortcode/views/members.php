4a8e3a2a0c8ab2385c345eb5fe8b63ec6445846c
Robert Gadon <rgadon107@gmail.com>
2018-09-11 16:01:51 -0500

    Update [members] shortcode view file.
    - Add helper function to render the member post_title.

--- plugins/cornerstone-members/src/shortcode/views/members.php
+++ plugins/cornerstone-members/src/shortcode/views/members.php
@@ -15,20 +15,23 @@ use function spiralWebDb\Members\Template\render_post_thumbnail_before_title;
 use function spiralWebDb\Members\Template\render_members_content;
 use function spiralWebDb\Members\Template\render_member_role;
 use function spiralWebDb\Members\Template\render_member_meta;
+use function spiralWebDb\Members\Template\render_member_title;
 
 ?>
 
 <article class="<?php echo esc_attr( $article_classes ); ?>" itemscope itemtype="https://schema.org/CreativeWork">
-	<header class="entry-header">
+    <header class="entry-header">
 		<?php
 		render_post_thumbnail_before_title( $member_id );
 		render_member_role( $member_id );
+		render_member_title( $member_id );
 		?>
-	</header>
-	<div class="entry-content two-thirds" itemprop="text">
+    </header>
+    <div class="entry-content two-thirds" itemprop="text">
 		<?php
+
 		render_members_content();
 		render_member_meta( $member_id );
 		?>
-	</div>
+    </div>
 </article>
