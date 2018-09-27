3762cb2aa993aae75e8c7834da7b2808b4b095ee
Robert Gadon <rgadon107@gmail.com>
2018-09-27 00:40:05 -0500

    Update Members plugin admin file.
    - Type-cast $columns param as an array.
    - Change the conditional check based on true conditions flagged by Query 
    Monitor. 
    - Call 'set_custom_columns( $columns) & assign to $config. 
    - Use apply_filters to call the $member_id from $post object.
    - Remove helper function. No longer needed. 
    - Properly rename the filter event to sort custom columns. They are now 
    sortable!

--- plugins/cornerstone-members/src/admin/wp-list-table.php
+++ plugins/cornerstone-members/src/admin/wp-list-table.php
@@ -40,44 +40,41 @@ add_action( 'manage_members_posts_custom_column', __NAMESPACE__ . '\render_custo
  *
  * @param string $column_name The name of the column to display.
  */
-function render_custom_column_content( $column_name ) {
+function render_custom_column_content( $column_name, $columns = array() ) {
 
-	if ( ! is_admin() && ! post_type_exists( 'members' ) ) {
+	if ( ! is_admin() && ! is_post_type_archive( 'members' ) ) {
 		return;
 	}
+
+	$config = set_custom_columns( $columns );
 
 	$post = get_the_post();
 
 	$member_id  = sanitize_field( $post->ID );
 	$menu_order = sanitize_field( $post->menu_order );
 
-	if ( 'member_id' == $column_name ) {
-		echo get_the_member_ID( $member_id );
+	if ( $config['member_id'] == $column_name ) {
+		echo apply_filters( 'ID', $member_id );
 	}
 
-	if ( 'menu_order' == $column_name ) {
+	if ( $config['menu_order'] == $column_name ) {
 		echo apply_filters( 'menu_order', $menu_order );
 	}
 }
 
-function get_the_member_ID( $member_id ) {
-	return get_the_ID();
+add_filter( 'manage_edit-members_sortable_columns', __NAMESPACE__ . '\set_sortable_columns' );
+/**
+ * Set sortable columns on Members Admin page.
+ *
+ * @since 1.0.0
+ *
+ * @param string $column_name The name of the column to display.
+ * @param int    $member_id   The current post ID.
+ */
+function set_sortable_columns( array $column_name ) {
+	return array(
+		'title'      => 'Member',
+		'member_id'  => 'Member ID',
+		'menu_order' => 'Order Number',
+	);
 }
-
-
-//add_filter( 'manage_members sortable_columns', __NAMESPACE__ . '\set_sortable_columns' );
-///**
-// * Set sortable columns on Members Admin page.
-// *
-// * @since 1.0.0
-// *
-// * @param string $column_name The name of the column to display.
-// * @param int    $member_id   The current post ID.
-// */
-//function set_sortable_columns( array $column_name ) {
-//	return array(
-//		'title'      => 'Member',
-//		'member_id'  => 'Member ID',
-//		'menu_order' => 'Order Number',
-//	);
-//}
