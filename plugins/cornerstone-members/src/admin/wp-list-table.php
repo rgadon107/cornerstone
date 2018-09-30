92cb586a5d6aca1e8dd909e19ad59b21ea972132
Tonya Mork <tonya.mork@xwp.co>
2018-09-30 17:19:17 -0500

    Fixed member list tables to display values.

--- plugins/cornerstone-members/src/admin/wp-list-table.php
+++ plugins/cornerstone-members/src/admin/wp-list-table.php
@@ -21,12 +21,10 @@ add_filter( 'manage_members_posts_columns', __NAMESPACE__ . '\set_custom_columns
  *
  * @since 1.0.0
  *
- * @param array $posts_columns An array of column names.
- *
  * @return array $columns
  */
-function set_custom_columns( $posts_columns = array() ) {
-	return $posts_columns = array(
+function set_custom_columns() {
+	return array(
 		'cb'         => '<input type="checkbox"/>',
 		'title'      => 'Member',
 		'member_id'  => 'Member ID',
@@ -34,36 +32,24 @@ function set_custom_columns( $posts_columns = array() ) {
 	);
 }
 
-add_action( 'manage_members_posts_custom_column', __NAMESPACE__ . '\_render_custom_column_content' );
+add_action( 'manage_members_posts_custom_column', __NAMESPACE__ . '\_render_custom_column_content', 10, 2 );
 /**
  *  Display the content for each custom column.
  *
+ * @since 1.0.0
+ *
  * @param string $column_name The name of the column to display.
  * @param int    $post_id     The current post ID.
- * @param array  $config      Optional. Array of custom columns.
  */
-function _render_custom_column_content( $column_name, $post_id, $config = array() ) {
-
-	if ( ! is_admin() && ! is_post_type_archive( 'members' ) ) {
-		return;
+function _render_custom_column_content( $column_name, $post_id ) {
+	switch ( $column_name ) {
+		case 'member_id':
+			echo (int) $post_id;
+			break;
+		case 'menu_order':
+			echo (int) get_post_field( 'menu_order', $post_id );
+			break;
 	}
-
-	if ( get_post_type( $post_id ) == 'members' ) {
-		$post = get_post();
-	}
-
-	$config = set_custom_columns( $posts_columns );
-
-	$member_id  = sanitize_post( $post->ID );
-	$menu_order = sanitize_post( $post->menu_order );
-
-	foreach ( $config as $custom_columns => $column_name ) {
-
-		echo $config['member_id']  = $member_id;
-		echo $config['menu_order'] = $menu_order;
-	}
-
-	return $column_name;
 }
 
 add_filter( 'manage_edit-members_sortable_columns', __NAMESPACE__ . '\_set_sortable_columns' );
@@ -72,10 +58,9 @@ add_filter( 'manage_edit-members_sortable_columns', __NAMESPACE__ . '\_set_sorta
  *
  * @since 1.0.0
  *
- * @param string $column_name The name of the column to display.
- * @param int    $member_id   The current post ID.
+ * @return array New sortable columns.
  */
-function _set_sortable_columns( array $column_name ) {
+function _set_sortable_columns() {
 	return array(
 		'title'      => 'Member',
 		'member_id'  => 'Member ID',
