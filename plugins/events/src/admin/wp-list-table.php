df69c6bbf4e1bf9b46a46467f18f5db27dccd726
Robert Gadon <rgadon107@gmail.com>
2018-10-01 16:05:43 -0500

    Build the Events custom columns callback. - Render DB to admin for Event ID, Performance Date, and Menu Order.

--- plugins/events/src/admin/wp-list-table.php
+++ plugins/events/src/admin/wp-list-table.php
@@ -32,3 +32,29 @@ function set_custom_columns() {
 		'menu_order'       => 'Order Number',
 	);
 }
+
+add_action( 'manage_events_posts_custom_column', __NAMESPACE__ . '\_render_custom_column_content', 10, 2 );
+/**
+ *  Display the content for each custom column.
+ *
+ * @since 1.0.0
+ *
+ * @param string $column_name The name of the column to display.
+ * @param int    $event_id    The current member (post) ID.
+ */
+function _render_custom_column_content( $column_name, $event_id ) {
+	switch ( $column_name ) {
+		case 'event_id':
+			echo (int) $event_id;
+			break;
+		case 'performance_date':
+			$event_date     = (string) get_post_meta( $event_id, 'event-date', true );
+			$event_date     = strtotime( $event_date );
+			$formatted_date = date( 'M d, Y', $event_date );
+			echo esc_html( $formatted_date );
+			break;
+		case 'menu_order':
+			echo (int) get_post_field( 'menu_order', $event_id );
+			break;
+	}
+}
