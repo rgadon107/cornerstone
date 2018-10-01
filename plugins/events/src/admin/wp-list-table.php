f654ebc5bf050e5989139081cd1a72e539502a03
Robert Gadon <rgadon107@gmail.com>
2018-10-01 15:24:55 -0500

    Add filter 'manage_events_posts_columns'.
    - Added custom column headers to Events admin page.

--- plugins/events/src/admin/wp-list-table.php
+++ plugins/events/src/admin/wp-list-table.php
@@ -12,4 +12,23 @@
  *
  * @license    GNU General Public License 2.0+
  */
+
 namespace spiralWebDb\Events;
+
+add_filter( 'manage_events_posts_columns', __NAMESPACE__ . '\set_custom_columns' );
+/**
+ * Add custom columns to Events Admin.
+ *
+ * @since 1.0.0
+ *
+ * @return array Array of columns.
+ */
+function set_custom_columns() {
+	return array(
+		'cb'               => '<input type="checkbox"/>',
+		'title'            => 'Name of Venue',
+		'event_id'         => 'Event ID',
+		'performance_date' => 'Performance Date',
+		'menu_order'       => 'Order Number',
+	);
+}
