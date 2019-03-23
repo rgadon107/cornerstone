fce96359ea6c667e997b9fe0cea3c8d5f116258d
Tonya Mork <hellofromtonya@knowthecode.io>
2019-03-23 13:33:01 -0500

    Fixed Events first integration test.

--- plugins/events/tests/phpunit/integration/admin/changeTitlePlaceholderText.php
+++ plugins/events/tests/phpunit/integration/admin/changeTitlePlaceholderText.php
@@ -2,30 +2,31 @@
 /**
  * Tests for change_title_placeholder_text().
  *
- * @package     spiralWebDb\Events\Tests\Integration\ConfigStore
+ * @package     spiralWebDb\Events\Tests\Integration
  * @since       1.3.0
  * @author      Robert Gadon <rgadon107>
  * @link        https://github.com/rgadon107/cornerstone
  * @license     GNU-2.0+
  */
 
-namespace spiralWebDb\Events\Tests\Integration\ConfigStore;
+namespace spiralWebDb\Events\Tests\Integration;
 
 use spiralWebDb\Cornerstone\Tests\Integration\Test_Case;
 
 /**
  * Class Tests_ChangeTitlePlaceholderText
  *
- * @package spiralWebDb\Events\Tests\Integration\ConfigStore
+ * @package spiralWebDb\Events\Tests\Integration
  * @group   events
  * @group   admin
  */
 class Tests_ChangeTitlePlaceholderText extends Test_Case {
 
 	/**
-	 * Test change_title_placeholder_text() should ....
+	 * Test change_title_placeholder_text() should return the given text when the post type is not 'events'.
 	 */
-	public function test_should_() {
-		$this->assertTrue( true );
+	public function test_should_return_given_text_when_post_type_not_events() {
+		$text = 'original text';
+		$this->assertSame( $text, change_title_placeholder_text( $text ) );
 	}
 }
