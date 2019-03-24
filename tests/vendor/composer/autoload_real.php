084ebe89b81f81acf78cab6861cfc9de4595587f
Tonya Mork <hellofromtonya@knowthecode.io>
2019-03-23 19:00:18 -0500

    Added register_custom_config() unit tests for FAQ.

--- tests/vendor/composer/autoload_real.php
+++ tests/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInitf1a387622c5c5e07c1a6020d18a651ac
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitf1a387622c5c5e07c1a6020d18a651ac', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
