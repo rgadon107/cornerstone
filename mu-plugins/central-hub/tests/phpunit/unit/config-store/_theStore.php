c919a175d37d971872842d6e05264c596754b087
Robert Gadon <rgadon107@gmail.com>
2019-03-31 23:47:33 -0500

    Write unit tests for `_the_store` function in the config-store source module.

--- mu-plugins/central-hub/tests/phpunit/unit/config-store/_theStore.php
+++ mu-plugins/central-hub/tests/phpunit/unit/config-store/_theStore.php
@@ -32,14 +32,54 @@ class Tests_TheStore extends Test_Case {
 	}
 
 	/**
-	 * Test _the_store() should return an empty array when no storey key is passed in and there are no configurations stored.
+	 * Test _the_store() should return an empty array when no store key is passed in and there are no configurations stored.
 	 */
 	public function test_should_return_empty_array_when_no_key_or_configs() {
 		$expected = [];
+
 		$this->assertSame( $expected, _the_store() );
 		$this->assertSame( $expected, _the_store( null ) );
 		$this->assertSame( $expected, _the_store( false ) );
 		$this->assertSame( $expected, _the_store( '' ) );
+		$this->assertSame( $expected, _the_store( 0 ) );
+		$this->assertSame( $expected, _the_store( 0.0 ));
+		$this->assertSame( $expected, _the_store( '0'));
+	}
+
+	/*
+	 * Test _the_store() should return true when a configuration is stored.
+	 */
+	public function test_should_return_true_when_a_config_is_stored() {
+
+		$store_key       = 'custom-meta-box';
+		$config_to_store = [ $store_key ];
+
+		$this->assertTrue( _the_store( $store_key, $config_to_store ) );
+	}
+
+	/*
+	 * Test that stored configuration has array key $store_key.
+	 */
+	public function test_should_return_config_store_array_key() {
+
+		$store_key                   = 'custom-meta-box';
+		$config_to_store             = [ $store_key ];
+		$config_store[ $store_key ]  = $config_to_store;
+
+		$this->assertSame( _the_store( 'custom-meta-box' ), $config_store[ 'custom-meta-box' ] );
+		$this->assertArrayHasKey( 'custom-meta-box', $config_store );
+	}
+
+	/*
+	 * Test should assert $store_key is not in $config_store array. If key does
+	 *   not exist in the array, a class \Exception with message will be thrown.
+	 */
+	public function test_store_key_is_not_in_config_store() {
+
+		$store_key = '';
+        $config_store = [];
+
+		$this->assertArrayNotHasKey( $store_key, $config_store );
 	}
 
 }
