b3f5d029f9270a8d664aa0144b6befadaa72e856
Robert Gadon <rgadon107@gmail.com>
2019-05-07 17:01:15 -0500

    Added _loadConfigFromFilesystem.php file to config-store test suite.

---
+++ mu-plugins/central-hub/tests/phpunit/unit/config-store/_loadConfigFromFilesystem.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ *  Tests for _load_config_from_filesystem
+ *
+ * @package    spiralWebDb\centralHub\Tests\ConfigStore
+ * @since      1.3.0
+ * @author     Robert A. Gadon
+ * @link       https://github.com/rgadon107/cornerstone
+ * @license    GNU General Public License 2.0+
+ */
+
+namespace spiralWebDb\centralHub\Tests\ConfigStore;
+
+use function KnowTheCode\ConfigStore\_load_config_from_filesystem;
+use spiralWebDb\Cornerstone\Tests\Unit\Test_Case;
+
+/**
+ * Class Tests_LoadConfigFromFilesystem
+ *
+ * @package spiralWebDb\centralHub\Tests\Unit\ConfigStore
+ * @group   config-store
+ */
+class Tests_LoadConfigFromFilesystem extends Test_Case {
+
+	/**
+	 * Prepares the test environment before each test.
+	 */
+	protected function setUp() {
+		parent::setUp();
+
+		require_once CENTRAL_HUB_ROOT_DIR . '/src/config-store/internals.php';
+	}
+
+	/**
+	 *  Test _load_config_from_filesystem() should return an array when a configuration array is given.
+	 */
+	public function test_should_return_array_when_config_array_is_given() {
+
+		$config = [
+			'foo' => [
+				'aaa' => 'bbb',
+				'ccc' => 'ddd'
+			]
+		];
+
+		$this->assertIsArray( _load_config_from_filesystem( __METHOD__ ) );
+	}
+}
