103ad83cf7792a22ff826bd594792e595f256776
Robert Gadon <rgadon107@gmail.com>
2019-05-18 11:12:19 -0500

    Build tests for getConfig($store_key):
    - Change store_key to __METHOD__.
    - Clean up _the_store for next test.
    - Test that _the_store is empty when no key is given.
    - Test that an error is thrown when no store exists.

--- mu-plugins/central-hub/tests/phpunit/unit/config-store/getConfig.php
+++ mu-plugins/central-hub/tests/phpunit/unit/config-store/getConfig.php
@@ -38,10 +38,29 @@ class Tests_getConfig extends Test_Case {
 	 */
 	public function test_that_the_store_returns_true_when_given_a_store_key_and_config() {
 		$config_to_store = (array) require CENTRAL_HUB_ROOT_DIR . '/tests/phpunit/fixtures/test-cpt-config.php';
-		$actual          = _the_store( 'key', $config_to_store );
+		$actual          = _the_store( __METHOD__ , $config_to_store );
 
 		$this->assertTrue( $actual );
 		$this->assertSame( true, $actual );
+
+		// Clean up _the_store.
+		_the_store( __METHOD__, null, true );
+	}
+
+	/*
+	 *  Test that _the_store is empty when no key is given.
+	 */
+	public function test_that_store_config_is_empty() {
+		$expected = [];
+		$actual = getConfig( '' );
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	public function test_should_throw_error_when_store_key_does_not_exist_in_store() {
+		$this->expectException( \Exception::class );
+		$this->expectExceptionMessage( 'Configuration for [foo] does not exist in the ConfigStore' );
+		getConfig('foo');
 	}
 }
 
