fab5f8e365f54d710bcea8b4b41718e686bde7aa
Robert Gadon <rgadon107@gmail.com>
2019-05-30 19:05:51 -0500

    Revised unit tests for getConfig() following feedback from code review.

--- mu-plugins/central-hub/tests/phpunit/unit/config-store/getConfig.php
+++ mu-plugins/central-hub/tests/phpunit/unit/config-store/getConfig.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *  Tests for getConfig
+ *  Tests for getConfig()
  *
  * @package    spiralWebDb\centralHub\Tests\Unit\ConfigStore
  * @since      1.3.0
@@ -16,7 +16,7 @@ use function KnowTheCode\ConfigStore\_the_store;
 use spiralWebDb\Cornerstone\Tests\Unit\Test_Case;
 
 /**
- * Class Tests_getConfig
+ * Class Tests_GetConfig
  *
  * @package spiralWebDb\centralHub\Tests\Unit\ConfigStore
  * @group   config-store
@@ -33,37 +33,49 @@ class Tests_GetConfig extends Test_Case {
 		require_once CENTRAL_HUB_ROOT_DIR . '/src/config-store/api.php';
 	}
 
-	/*
-	 * Test that _the_store returns true when given a store key and config to store.
+	/**
+	 * Test should return configuration from _the_store() when store key is given.
 	 */
-	public function test_that_the_store_returns_true_when_given_a_store_key_and_config() {
-		$config_to_store = (array) require CENTRAL_HUB_ROOT_DIR . '/tests/phpunit/fixtures/test-cpt-config.php';
-		$actual          = _the_store( __METHOD__, $config_to_store );
+	public function test_should_return_configuration_when_store_key_is_given() {
+		$config = (array) require CENTRAL_HUB_ROOT_DIR . '/tests/phpunit/fixtures/test-cpt-config.php';
+		_the_store( 'foo', $config );
+		$expected = getConfig( 'foo' );
 
-		$this->assertTrue( $actual );
-		$this->assertSame( true, $actual );
+		$this->assertArrayHasKey( 'foo', $expected );
+		$this->assertSame( $expected, $config );
 
 		// Clean up _the_store.
-		_the_store( __METHOD__, null, true );
+		_the_store( 'foo', null, true );
 	}
 
-	/*
-	 *  Test that _the_store is empty when no key is given.
+	/**
+	 * Test should throw Exception when configuration does not exist in store.
 	 */
-	public function test_that_store_config_is_empty() {
-		$expected = [];
-		$actual   = getConfig( '' );
-
-		$this->assertSame( $expected, $actual );
-	}
-
-	/*
-	 *  Test that error message is thrown when store key does not exist in configuration.
-	 */
-	public function test_should_throw_error_when_store_key_does_not_exist_in_store() {
+	public function test_should_throw_exception_when_config_does_not_exist() {
 		$this->expectException( \Exception::class );
 		$this->expectExceptionMessage( 'Configuration for [foo] does not exist in the ConfigStore' );
+
 		getConfig( 'foo' );
+	}
+
+	/**
+	 * Test should return empty array when store key does not exist.
+	 */
+	public function test_should_return_empty_array_when_key_does_not_exist() {
+		$config = (array) require CENTRAL_HUB_ROOT_DIR . '/tests/phpunit/fixtures/test-cpt-config.php';
+		_the_store( '', $config );
+
+		$this->assertSame( [], getConfig( '' ) );
+
+		// Clean up _the_store.
+		_the_store( '', null, true );
+	}
+
+	/**
+	 * Test should return an empty array when store key and config are not stored.
+	 */
+	public function test_should_return_empty_array_when_key_and_config_not_stored() {
+		$this->assertSame( [], getConfig( '' ) );
 	}
 }
 
