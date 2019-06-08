98353ddcd54c405d48b6a940048b269fd4b04da2
Tonya Mork <hellofromtonya@knowthecode.io>
2019-06-08 12:19:48 -0500

    Refactored first getConfigParameter unit test.

--- mu-plugins/central-hub/tests/phpunit/unit/config-store/getConfigParameter.php
+++ mu-plugins/central-hub/tests/phpunit/unit/config-store/getConfigParameter.php
@@ -11,10 +11,8 @@
 
 namespace spiralWebDb\centralHub\Tests\Unit\ConfigStore;
 
-use function KnowTheCode\ConfigStore\_load_config_from_filesystem;
-use function KnowTheCode\ConfigStore\getConfig;
+use Brain\Monkey;
 use function KnowTheCode\ConfigStore\getConfigParameter;
-use function KnowTheCode\ConfigStore\_the_store;
 use spiralWebDb\Cornerstone\Tests\Unit\Test_Case;
 
 /**
@@ -34,27 +32,45 @@ class Tests_GetConfigParameter extends Test_Case {
 		require_once CENTRAL_HUB_ROOT_DIR . '/src/config-store/api.php';
 	}
 
-	// Test 1: $store_key given from stored config. $paramater within config exists.
-	// Expect array on return.
-	public function test_should_return_array_when_store_and_parameter_keys_given() {
-		// Load configuration array into memory with test fixture.
-		$path_to_file = CENTRAL_HUB_ROOT_DIR . '/tests/phpunit/fixtures/test-get-config-parameter.php';
-		$config       = _load_config_from_filesystem( $path_to_file );
+	/**
+	 * Test getConfigParameter() should return the expected value when a valid store and parameter keys are given.
+	 */
+	public function test_should_return_value_when_valid_store_and_parameter_keys_given() {
+		$config = [
+			'aaa' => [
+				'ddd' => 'Know the Code',
+				'eee' => 'Config Store',
+			],
+			'bbb' => 'WordPress rocks!',
+			'ccc' => 37,
+		];
 
-		// Add $config to _the_store().
-		_the_store( 'foo', $config ); // returns true; stores key & config.
-		getConfig( 'foo' ); // check entire config can be retrieved. Runs inside getConfigParamater.
+		Monkey\Functions\expect( '\KnowTheCode\ConfigStore\getConfig' )
+			// For the 1st assertion.
+			->once()
+			->with( __METHOD__ )
+			->ordered()
+			->andReturn( $config )
+			// For the 2nd assertion.
+			->andAlsoExpectIt()
+			->once()
+			->with( __METHOD__ )
+			->ordered()
+			->andReturn( $config )
+			// For the 3rd assertion.
+			->andAlsoExpectIt()
+			->once()
+			->with( __METHOD__ )
+			->ordered()
+			->andReturn( $config );
 
-		// Challenge: How do I isolate the $parameter_key of 'aaa' inside 'foo' as a string value?
-		// getConfigParameter() returns error msg; array key exists. Must be either string or integer.
-		// I'm returning an array value.
-
-		// Error Message: Exception: The configuration parameter [aaa] within [foo] does not exist
-		// in this configuration (even though it's clearly there).
-		$this->assertArrayHasKey( 'aaa', getConfigParameter( 'foo', 'aaa' ) );
+		$this->assertSame(
+			[
+				'ddd' => 'Know the Code',
+				'eee' => 'Config Store',
+			],
+			getConfigParameter( __METHOD__, 'aaa' ) );
+		$this->assertSame( 'WordPress rocks!', getConfigParameter( __METHOD__, 'bbb' ) );
+		$this->assertSame( 37, getConfigParameter( __METHOD__, 'ccc' ) );
 	}
-
-	// Test 2: $store_key not found in config. Exception will be thrown.
-
-	// Test 3: $parameter not found in config. Exception will be thrown.
 }
