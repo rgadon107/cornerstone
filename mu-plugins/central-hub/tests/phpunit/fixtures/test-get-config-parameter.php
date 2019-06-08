344cca895274a112a69e84c763177fdfb2e49a29
Robert Gadon <rgadon107@gmail.com>
2019-06-08 00:52:22 -0500

    Add new test fixture to test getConfigParamater().

---
+++ mu-plugins/central-hub/tests/phpunit/fixtures/test-get-config-parameter.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  Test data for the ConfigStore API.
+ *
+ * @package    spiralWebDb\centralHub\Tests\Fixtures
+ * @since      1.0.0
+ * @author     Robert A. Gadon
+ * @link       https://github.com/rgadon107/cornerstone
+ * @license    GNU General Public License 2.0+
+ */
+
+namespace spiralWebDb\centralHub\Tests\Fixtures;
+
+return [
+	'foo' => [
+		'aaa' => [ 'bbb', 37, 'Know The Code' ]
+	]
+];
+
