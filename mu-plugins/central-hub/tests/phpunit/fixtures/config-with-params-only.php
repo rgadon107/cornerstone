a0bbe2ebfdeca8a5f4997786e29bb019e44620ed
Robert Gadon <rgadon107@gmail.com>
2019-06-14 18:11:30 -0500

    Added 2 text fixture files to test 2 edge cases.
    - Config with store key and empty parameters.
    - Config with empty store key and paramaters.

---
+++ mu-plugins/central-hub/tests/phpunit/fixtures/config-with-params-only.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  Test data for the ConfigStore API.
+ *  Store key is empty and configuration parameters exist.
+ *
+ * @package    spiralWebDb\centralHub\Tests\Fixtures
+ * @since      1.0.0
+ * @author     Robert A. Gadon
+ * @link       https://github.com/rgadon107/cornerstone
+ * @license    GNU General Public License 2.0+
+ */
+
+namespace spiralWebDb\centralHub\Tests\Fixtures;
+
+return  [
+		'aaa' => 'bbb',
+		'ccc' => 'ddd'
+];
+
