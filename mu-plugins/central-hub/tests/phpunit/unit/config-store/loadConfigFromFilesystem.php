b0fd87abf8b4879305ac0b7183a5d18ebdabbb17
Robert Gadon <rgadon107@gmail.com>
2019-07-02 23:36:07 -0500

    Remove unit test method; edge case will never occur.

--- mu-plugins/central-hub/tests/phpunit/unit/config-store/loadConfigFromFilesystem.php
+++ mu-plugins/central-hub/tests/phpunit/unit/config-store/loadConfigFromFilesystem.php
@@ -87,28 +87,5 @@ class Tests_LoadConfigFromFilesystem extends Test_Case {
 
 		$this->assertSame( 'foo', loadConfigFromFilesystem( $path_to_file ) );
 	}
-
-	/**
-	 * Test loadConfigFromFilesystem() should overwrite stored configuration and return store key from
-	 * file configuration.
-	 */
-	public function test_should_overwrite_store_and_return_store_key_from_file_config() {
-		$path_to_file = CENTRAL_HUB_ROOT_DIR . '/tests/phpunit/fixtures/test-cpt-config.php';
-		$config       = [
-			'aaa' => 'bbb',
-			'ccc' => 'ddd',
-		];
-		Monkey\Functions\expect( '\KnowTheCode\ConfigStore\_load_config_from_filesystem' )
-			->twice()
-			->with( $path_to_file )
-			->andReturn( [ 'foo', $config ] );
-		Monkey\Functions\expect( '\KnowTheCode\ConfigStore\_merge_with_defaults' )->never();
-		Monkey\Functions\expect( '\KnowTheCode\ConfigStore\_the_store' )
-			->twice()
-			->with( 'foo', $config )
-			->andReturn( true );
-
-		$this->assertSame( 'foo', loadConfigFromFilesystem( $path_to_file ) );
-		$this->assertSame( 'foo', loadConfigFromFilesystem( $path_to_file ) );
-	}
 }
+
