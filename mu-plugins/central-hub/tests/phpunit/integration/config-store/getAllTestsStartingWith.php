813e3d16fb27a1eff27b9f02920ae34f3d4f7673
Robert Gadon <rgadon107@gmail.com>
2019-07-12 16:33:48 -0500

    Setup integration test file for API function `getAllKeysStartingWith()`.

---
+++ mu-plugins/central-hub/tests/phpunit/integration/config-store/getAllTestsStartingWith.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *  Tests for external API function getAllKeysStartingWith()
+ *
+ * @package    spiralWebDb\centralHub\Tests\Integration\ConfigStore
+ * @since      1.3.0
+ * @author     Robert A. Gadon
+ * @link       https://github.com/rgadon107/cornerstone
+ * @license    GNU General Public License 2.0+
+ */
+
+namespace spiralWebDb\centralHub\Tests\Integration\ConfigStore;
+
+use function KnowTheCode\ConfigStore\getAllKeysStartingWith;
+use spiralWebDb\Cornerstone\Tests\Unit\Test_Case;
+
+/**
+ * Class Tests_GetAllKeysStartingWith
+ *
+ * @package spiralWebDb\centralHub\Tests\Integration\ConfigStore
+ * @group   config-store
+ */
+class Tests_GetAllKeysStartingWith extends Test_Case {
+
+}
+
