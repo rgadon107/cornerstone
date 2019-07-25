845548529d02ce7fd1f8c0a4c5f50720e12f2f79
Robert Gadon <rgadon107@gmail.com>
2019-07-25 17:37:34 -0500

    Add method to start of each integration test to empty the store.

--- mu-plugins/central-hub/tests/phpunit/integration/config-store/getConfigParameter.php
+++ mu-plugins/central-hub/tests/phpunit/integration/config-store/getConfigParameter.php
@@ -24,6 +24,13 @@ use spiralWebDb\Cornerstone\Tests\Integration\Test_Case;
 class Tests_GetConfigParameter extends Test_Case {
 
 	/**
+	 * Empty the store before starting these tests.
+	 */
+	public static function setUpBeforeClass() {
+		self::empty_the_store();
+	}
+
+	/**
 	 * Test getConfigParameter() should return the expected value when a valid store and parameter keys are given.
 	 */
 	public function test_should_return_value_when_valid_store_and_parameter_keys_given() {
@@ -52,9 +59,10 @@ class Tests_GetConfigParameter extends Test_Case {
 	}
 
 	/**
-	 * Test getConfigParameter() should thrown an error when the parameter key does not exist in the stored configuration.
+	 * Test getConfigParameter() should throw an error when the parameter key does not exist in the stored
+	 * configuration.
 	 */
-	public function test_should_thrown_error_when_parameter_key_does_not_exist() {
+	public function test_should_throw_error_when_parameter_key_does_not_exist() {
 		// Store the configuration to set up this test.
 		$config = [
 			'bbb' => 'WordPress rocks!',
@@ -62,10 +70,11 @@ class Tests_GetConfigParameter extends Test_Case {
 		_the_store( __METHOD__, $config );
 
 		$this->expectException( \Exception::class );
-		$this->expectExceptionMessage( sprintf('The configuration parameter [key_does_not_exist] within [%s] does not exist in this configuration', __METHOD__ ) );
+		$this->expectExceptionMessage( sprintf( 'The configuration parameter [key_does_not_exist] within [%s] does not exist in this configuration', __METHOD__ ) );
 		getConfigParameter( __METHOD__, 'key_does_not_exist' );
 
 		// Clean up.
 		self::remove_from_store( __METHOD__ );
 	}
 }
+
