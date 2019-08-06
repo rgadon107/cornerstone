95803cb4f988598b6b05408f58f9e9b3c5ee9a21
Robert Gadon <rgadon107@gmail.com>
2019-08-06 15:45:43 -0500

    Initial commit of integration test file for `renderMetaBox`.

---
+++ mu-plugins/central-hub/tests/phpunit/integration/meta-data/renderMetaBox.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Tests for the function render_meta_box().
+ *
+ * @package     spiralWebDb\centralHub\Tests\Unit\Metadata
+ * @since       1.3.0
+ * @author      Robert Gadon <rgadon107>
+ * @link        https://github.com/rgadon107/cornerstone
+ * @license     GNU-2.0+
+ */
+
+namespace spiralWebDb\centralHub\Tests\Integration\Metadata;
+
+use function spiralWebDB\Metadata\render_meta_box;
+use spiralWebDb\Cornerstone\Tests\Integration\Test_Case;
+
+/**
+ * Class Tests_RenderMetaBox
+ *
+ * @package spiralWebDb\centralHub\Tests\Integration\Metadata
+ * @group   meta-data
+ */
+class Tests_RenderMetaBox extends Test_Case {
+
+}
