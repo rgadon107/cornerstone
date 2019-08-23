ac2fdf5863f08332c9aa496917845821a6198752
Robert Gadon <rgadon107@gmail.com>
2019-08-23 00:19:56 -0500

    [WIP]Refactor the unit test file.
    - Initialize and store the config used in each test.
    - Initialize the constants used in each test. 
    - Mock expected behavior of constants in tests.  
    - Note: First and third tests return same error: 
    
    >Patchwork\Exceptions\NotUserDefined: Please include {"redefinable-internals": ["defined"]} in your patchwork.json.

--- mu-plugins/central-hub/tests/phpunit/unit/meta-data/isOkayToSaveMetaBox.php
+++ mu-plugins/central-hub/tests/phpunit/unit/meta-data/isOkayToSaveMetaBox.php
@@ -12,6 +12,7 @@
 namespace spiralWebDb\centralHub\Tests\Unit\Metadata;
 
 use Brain\Monkey;
+use function \define;
 use function KnowTheCode\ConfigStore\loadConfig;
 use function spiralWebDB\Metadata\is_okay_to_save_meta_box;
 use spiralWebDb\Cornerstone\Tests\Unit\Test_Case;
@@ -24,8 +25,27 @@ use spiralWebDb\Cornerstone\Tests\Unit\Test_Case;
  */
 class Tests_IsOkayToSaveMetaBox extends Test_Case {
 
+	protected function init_and_store_config() {
+		$_POST = [
+			'events' => [
+				'add_meta_box'  => [],
+				'custom_fields' => [],
+				'view'          => '',
+			],
+		];
+		foreach ( $_POST as $store_key => $config_to_store ) {
+			loadConfig( $store_key, $config_to_store );
+		}
+	}
+
+	protected function initialize_constants() {
+		define( 'DOING_AUTOSAVE', true );
+		define( 'DOING_AJAX', true );
+		define( 'DOING_CRON', true );
+	}
+
 	/**
-	 * Prepares the test environment before each test.
+	 * Prepare the test environment before each test.
 	 */
 	protected function setUp() {
 		parent::setUp();
@@ -37,17 +57,25 @@ class Tests_IsOkayToSaveMetaBox extends Test_Case {
 	 * Test is_okay_to_save_meta_box() should check the meta box key exists in the $_POST array.
 	 */
 	public function test_should_check_that_the_meta_box_key_exists_in_the_post_array() {
-		$_POST = [
-			'events' => [
-				'add_meta_box'  => [],
-				'custom_fields' => [],
-				'view'          => '',
-			],
-		];
-		foreach ( $_POST as $store_key => $config_to_store ) {
-			loadConfig( $store_key, $config_to_store );
-		}
-		Monkey\Functions\expect( '\wp_verify_nonce' )
+		self::init_and_store_config();
+		self::initialize_constants();
+		Monkey\Functions\expect( 'defined' )
+			->times( 3 )
+			->with( 'DOING_AUTOSAVE' )
+			->andAlsoExpectIt()
+			->with( 'DOING_AJAX' )
+			->andAlsoExpectIt()
+			->with( 'DOING_CRON' )
+			->andReturn( true );
+		Monkey\Functions\expect( 'constant' )
+			->times( 3 )
+			->with( 'DOING_AUTOSAVE' )
+			->andAlsoExpectIt()
+			->with( 'DOING_AJAX' )
+			->andAlsoExpectIt()
+			->with( 'DOING_CRON' )
+			->andReturn( false );
+		Monkey\Functions\expect( 'wp_verify_nonce' )
 			->once()
 			->with( $_POST[ 'events' . '_nonce_name' ], 'events' . '_nonce_action' )
 			->andReturn( 1 );
@@ -58,17 +86,8 @@ class Tests_IsOkayToSaveMetaBox extends Test_Case {
 	/**
 	 *  Test is_okay_to_save_meta_box() should return false if the meta box key does not exist in the $_POST array.
 	 */
-	function test_function_should_return_false_if_the_meta_box_key_does_not_exist_in_the_post_array() {
-		$_POST = [
-			'events' => [
-				'add_meta_box'  => [],
-				'custom_fields' => [],
-				'view'          => '',
-			],
-		];
-		foreach ( $_POST as $store_key => $config_to_store ) {
-			loadConfig( $store_key, $config_to_store );
-		}
+	public function test_function_should_return_false_if_the_meta_box_key_does_not_exist_in_the_post_array() {
+		self::init_and_store_config();
 
 		$this->assertFalse( is_okay_to_save_meta_box( 'members' ) );
 	}
@@ -77,30 +96,53 @@ class Tests_IsOkayToSaveMetaBox extends Test_Case {
 	 *  Test is_okay_to_save_meta_box() should return false when doing autosave, ajax, or cron.
 	 */
 	public function test_function_should_return_false_when_doing_autosave_ajax_or_cron() {
-		$_POST = [
-			'events' => [
-				'add_meta_box'  => [],
-				'custom_fields' => [],
-				'view'          => '',
-			],
-		];
-		foreach ( $_POST as $store_key => $config_to_store ) {
-			loadConfig( $store_key, $config_to_store );
-		}
-		define( 'DOING_AUTOSAVE', true );
-		define( 'DOING_AJAX', true );
-		define( 'DOING_CRON', true );
-		defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE && ! DOING_AJAX && ! DOING_CRON;
+		self::init_and_store_config();
+		self::initialize_constants();
+		Monkey\Functions\expect( 'defined' )
+			->once()
+			->with( 'DOING_AUTOSAVE' )
+			->andAlsoExpectIt()
+			->with( 'DOING_AJAX' )
+			->andAlsoExpectIt()
+			->with( 'DOING_CRON' )
+			->andReturn( true );
+		Monkey\Functions\expect( 'constant' )
+			->once()
+			->with( 'DOING_AUTOSAVE' )
+			->andReturn( true )
+			->andAlsoExpectIt()
+			->with( 'DOING_AJAX' )
+			->andReturn( false )
+			->andAlsoExpectIt()
+			->with( 'DOING_CRON' )
+			->andReturn( false );
 
 		$this->assertFalse( is_okay_to_save_meta_box( 'events' ) );
 
-		defined( 'DOING_AJAX' ) && DOING_AJAX && ! DOING_AUTOSAVE  && ! DOING_CRON;
+		Monkey\Functions\expect( 'constant' )
+			->times( 3 )
+			->with( 'DOING_AUTOSAVE' )
+			->andReturn( false )
+			->andAlsoExpectIt()
+			->with( 'DOING_AJAX' )
+			->andReturn( true )
+			->andAlsoExpectIt()
+			->with( 'DOING_CRON' )
+			->andReturn( false );
 
 		$this->assertFalse( is_okay_to_save_meta_box( 'events' ) );
 
-		defined( 'DOING_CRON' ) && DOING_CRON && ! DOING_AUTOSAVE && ! DOING_AJAX;
+		Monkey\Functions\expect( 'constant' )
+			->times( 3 )
+			->with( 'DOING_AUTOSAVE' )
+			->andReturn( false )
+			->andAlsoExpectIt()
+			->with( 'DOING_AJAX' )
+			->andReturn( false )
+			->andAlsoExpectIt()
+			->with( 'DOING_CRON' )
+			->andReturn( true );
 
 		$this->assertFalse( is_okay_to_save_meta_box( 'events' ) );
 	}
 }
-
