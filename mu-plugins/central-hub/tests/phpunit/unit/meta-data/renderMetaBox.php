c3ed8a163ae0459a640c7511b7e81e9a146d15f8
Robert Gadon <rgadon107@gmail.com>
2019-08-29 18:23:11 -0500

    Add new and edit existing class properties.
    - Initialize values of $config['custom_fields'].
    - Add $meta_box_args property.
    - Add $nonce_html property.
    - Add $expected_fixture_view_html property.

--- mu-plugins/central-hub/tests/phpunit/unit/meta-data/renderMetaBox.php
+++ mu-plugins/central-hub/tests/phpunit/unit/meta-data/renderMetaBox.php
@@ -29,9 +29,68 @@ class Tests_RenderMetaBox extends Test_Case {
 	 * @var array
 	 */
 	protected $config = [
-		'custom_fields' => [ 'event-date', 'event-time', 'venue-name' ],
+		'custom_fields' => [
+			'event-date' => '',
+			'event-time' => '',
+			'venue-name' => '',
+		],
 		'view'          => CENTRAL_HUB_ROOT_DIR . '/tests/phpunit/fixtures/meta-box-events-view.php',
 	];
+
+	/**
+	 * Array of meta box arguments.
+	 *
+	 * @var array
+	 */
+	protected $meta_box_args = [ 'id' => 'events' ];
+
+	/**
+	 * Nonce html.
+	 *
+	 * @var string
+	 */
+	protected $nonce_html = <<<NONCE
+<input type="hidden" id="events_nonce_name" name="events_nonce_name" value="" />
+NONCE;
+
+	/**
+	 * Expected meta box view html.
+	 *
+	 * @var string
+	 */
+	protected $expected_fixture_view_html    = <<<VIEW
+<div class="event-date">
+	<label for="event-date"><strong>Performance Date</strong></label>
+	<p>
+		<input id="event-date" type="date" name="events[event-date]" value="2019-08-07">
+	</p>
+	<span class="description">Event date description.</span>
+</div>
+<div class="event-time">
+	<p>
+		<label for="event-time"><strong>Performance Time</strong></label>
+	</p>
+	<p>
+		<input id="event-time" type="time" name="events[event-time]" value="09:00:00">
+	</p>
+	<p>
+		<span class="description">Event time description.</span>
+	</p>
+</div>
+<hr>
+<div class="performance-venue">
+	<p>
+		<label for="performance-venue"><strong>Performance Venue</strong></label>
+	</p>
+	<label for="venue-name">Name</label>
+	<p>
+		<input id="venue-name" class="large-text" type="text" name="events[venue-name]" value="Some venue" placeholder="e.g. First Presbyterian Church of St. Louis">
+	</p>
+	<p>
+		<span class="description">Performance venue description.</span>
+	</p>
+</div>
+VIEW;
 
 	/**
 	 * Prepares the test environment before each test.
