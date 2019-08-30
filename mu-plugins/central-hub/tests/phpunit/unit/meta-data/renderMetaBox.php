b24bf576e74e3208d91402841a7a6e876c97e2ef
Robert Gadon <rgadon107@gmail.com>
2019-08-29 21:10:17 -0500

    Fixed custom field value in view fixture that was throwing error.

--- mu-plugins/central-hub/tests/phpunit/unit/meta-data/renderMetaBox.php
+++ mu-plugins/central-hub/tests/phpunit/unit/meta-data/renderMetaBox.php
@@ -82,7 +82,7 @@ NONCE;
 		<label for="event-time"><strong>Performance Time</strong></label>
 	</p>
 	<p>
-		<input id="event-time" type="time" name="events[event-time]" value="09:00:00">
+		<input id="event-time" type="time" name="events[event-time]" value="09:36:00">
 	</p>
 	<p>
 		<span class="description">Event time description.</span>
@@ -95,7 +95,7 @@ NONCE;
 	</p>
 	<label for="venue-name">Name</label>
 	<p>
-		<input id="venue-name" class="large-text" type="text" name="events[venue-name]" value="Some venue" placeholder="e.g. First Presbyterian Church of St. Louis">
+		<input id="venue-name" class="large-text" type="text" name="events[venue-name]" value="Some really cool venue" placeholder="e.g. First Presbyterian Church of St. Louis">
 	</p>
 	<p>
 		<span class="description">Performance venue description.</span>
