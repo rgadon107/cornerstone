d2ff1143d795eea4c151ab85d4a631a594090978
Robert Gadon <rgadon107@gmail.com>
2019-09-19 16:34:56 -0500

    Rewrote first 3 unit tests for save_meta_boxes().
    - Scenario (1): test should not save when there are no meta box keys in the Config Store
    - Scenario (2): test should not save when the meta box key does not begin with meta_box.
    - Scenario (3): test should not save when the $meta_box_key does not exist in $_POST.

--- mu-plugins/central-hub/tests/phpunit/unit/meta-data/saveMetaBoxes.php
+++ mu-plugins/central-hub/tests/phpunit/unit/meta-data/saveMetaBoxes.php
@@ -32,7 +32,80 @@ class Tests_SaveMetaBoxes extends Test_Case {
 
 		require_once CENTRAL_HUB_ROOT_DIR . '/src/meta-data/meta-box.php';
 	}
-	
+
+	/**
+	 * Test save_meta_boxes() should not save when no meta box keys are in the Config Store.
+	 */
+	public function test_should_not_save_when_no_meta_box_keys_are_in_store() {
+		Monkey\Functions\expect( 'spiralWebDB\Metadata\get_meta_box_keys' )
+			->once()
+			->withNoArgs()
+			->andReturn( [] );
+		Monkey\Functions\expect( 'spiralWebDB\Metadata\get_meta_box_id' )->never();
+		Monkey\Functions\expect( 'spiralWebDB\Metadata\is_okay_to_save_meta_box' )->never();
+		Monkey\Functions\expect( 'KnowTheCode\ConfigStore\getConfigParameter' )->never();
+		Monkey\Functions\expect( 'spiralWebDB\Metadata\save_custom_fields' )->never();
+		save_meta_boxes( 178 );
+	}
+
+
+	/**
+	 * Test save_meta_boxes() should not save when config meta box key does not begin with 'meta_box.'.
+	 */
+	public function test_should_not_save_when_config_meta_box_key_does_not_begin_with_metabox() {
+		$configs = [
+			'taxonomy.roles'         => [
+				'Soprano' => 'Soprano (vocalist)',
+			],
+			'shortcode.qa'           => [
+				'Question 1' => 'How many angels can dance on the head of a pin?',
+			],
+			'custom_post_type.books' => [
+				'Title' => 'To Kill a Mockingbird',
+			],
+			'metabox.notametabox'    => [
+				'add_meta_box' => [
+					'id'     => 'notametabox',
+					'title'  => 'Does not start with the right meta_box. structure',
+					'screen' => [ 'notametabox' ],
+				],
+			],
+		];
+		foreach ( $configs as $store_key => $config_to_store ) {
+			loadConfig( $store_key, $config_to_store );
+		}
+		Monkey\Functions\expect( 'spiralWebDB\Metadata\get_meta_box_keys' )
+			->once()
+			->withNoArgs()
+			->andReturn( [] );
+		Monkey\Functions\expect( 'spiralWebDB\Metadata\get_meta_box_id' )->never();
+		Monkey\Functions\expect( 'spiralWebDB\Metadata\is_okay_to_save_meta_box' )->never();
+		Monkey\Functions\expect( 'KnowTheCode\ConfigStore\getConfigParameter' )->never();
+		Monkey\Functions\expect( 'spiralWebDB\Metadata\save_custom_fields' )->never();
+		save_meta_boxes( 229 );
+	}
+
+	/**
+	 * Test save_meta_boxes() should not save when the $meta_box_key does not exist in $_POST.
+	 */
+	public function test_should_not_save_when_the_meta_box_key_does_not_exist_in_POST() {
+		Monkey\Functions\expect( 'spiralWebDB\Metadata\get_meta_box_keys' )
+			->once()
+			->withNoArgs()
+			->andReturn( [ 'meta_box.members' ] );
+		Monkey\Functions\expect( 'spiralWebDB\Metadata\get_meta_box_id' )
+			->once()
+			->with( 'meta_box.members' )
+			->andReturn( 'members' );
+		Monkey\Functions\expect( 'spiralWebDB\Metadata\is_okay_to_save_meta_box' )
+			->once()
+			->with( 'members' )
+			->andReturn( false );
+		Monkey\Functions\expect( 'KnowTheCode\ConfigStore\getConfigParameter' )->never();
+		Monkey\Functions\expect( 'spiralWebDB\Metadata\save_custom_fields' )->never();
+		save_meta_boxes( 317 );
+	}
+
 	/**
 	 * Test save_meta_boxes() should save custom fields and returns a valid nonce.
 	 */
@@ -69,18 +142,6 @@ class Tests_SaveMetaBoxes extends Test_Case {
 			->justReturn();
 
 		$this->assertNull( save_meta_boxes( 19 ) );
-	}
-
-	/*
-	 * Test save_meta_boxes() returns null when no store key starts with 'meta_box.'.
-	 */
-	public function test_function_should_return_null_when_no_store_key_starts_with_meta_box() {
-		Monkey\Functions\expect( 'spiralWebDB\Metadata\get_meta_box_keys' )
-			->once()
-			->withNoArgs()
-			->andReturn( [] );
-
-		$this->assertNull( save_meta_boxes( 56 ) );
 	}
 }
 
