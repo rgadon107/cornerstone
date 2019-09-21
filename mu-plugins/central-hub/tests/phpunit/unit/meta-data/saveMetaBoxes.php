9d015ca080d81082dc2ba1c38888617c60864831
Robert Gadon <rgadon107@gmail.com>
2019-09-21 13:15:14 -0500

    Update unit test file.
    1) Rename test method to address 2 use cases when $meta_box_key is not valided in `is_okay_to_save_meta_box`.
    
    2) Delete redundant test method when config is empty or does not begin with `meta_box.`. 
    
    3) Change mock of `getConfigParameter` from when() to expect() in `test_should_save_when_one_or_more_valid_post_meta_values_are_added()'.
    
    4) Add $config var to pass as argument to mock of `save_custom_fields`. 
    
    5) Edit return value of `save_custom_fields` to return null.

--- mu-plugins/central-hub/tests/phpunit/unit/meta-data/saveMetaBoxes.php
+++ mu-plugins/central-hub/tests/phpunit/unit/meta-data/saveMetaBoxes.php
@@ -47,26 +47,11 @@ class Tests_SaveMetaBoxes extends Test_Case {
 		save_meta_boxes( 178 );
 	}
 
-
 	/**
-	 * Test save_meta_boxes() should not save when config meta box key does not begin with 'meta_box.'.
+	 * Test save_meta_boxes() should not save when the $meta_box_key is not validated in $_POST or
+	 *    wp_verify_nonce().
 	 */
-	public function test_should_not_save_when_config_meta_box_key_does_not_begin_with_metabox() {
-		Monkey\Functions\expect( 'spiralWebDB\Metadata\get_meta_box_keys' )
-			->once()
-			->withNoArgs()
-			->andReturn( [] );
-		Monkey\Functions\expect( 'spiralWebDB\Metadata\get_meta_box_id' )->never();
-		Monkey\Functions\expect( 'spiralWebDB\Metadata\is_okay_to_save_meta_box' )->never();
-		Monkey\Functions\expect( 'KnowTheCode\ConfigStore\getConfigParameter' )->never();
-		Monkey\Functions\expect( 'spiralWebDB\Metadata\save_custom_fields' )->never();
-		save_meta_boxes( 229 );
-	}
-
-	/**
-	 * Test save_meta_boxes() should not save when the $meta_box_key does not exist in $_POST.
-	 */
-	public function test_should_not_save_when_the_meta_box_key_does_not_exist_in_POST() {
+	public function test_should_not_save_when_the_meta_box_key_is_not_validated() {
 		Monkey\Functions\expect( 'spiralWebDB\Metadata\get_meta_box_keys' )
 			->once()
 			->withNoArgs()
@@ -85,48 +70,6 @@ class Tests_SaveMetaBoxes extends Test_Case {
 	}
 
 	/**
-	 *  Test save_meta_boxes() should not save when the expected meta box nonce key is not validated.
-	 */
-	public function test_should_not_save_when_the_expected_meta_box_nonce_key_is_not_validated() {
-		Monkey\Functions\expect( 'spiralWebDB\Metadata\get_meta_box_keys' )
-			->once()
-			->withNoArgs()
-			->andReturn( [ 'meta_box.reviews' ] );
-		Monkey\Functions\expect( 'spiralWebDB\Metadata\get_meta_box_id' )
-			->once()
-			->with( 'meta_box.reviews' )
-			->andReturn( 'reviews' );
-		Monkey\Functions\expect( 'spiralWebDB\Metadata\is_okay_to_save_meta_box' )
-			->once()
-			->with( 'reviews' )
-			->andReturn( false );
-		Monkey\Functions\expect( 'KnowTheCode\ConfigStore\getConfigParameter' )->never();
-		Monkey\Functions\expect( 'spiralWebDB\Metadata\save_custom_fields' )->never();
-		save_meta_boxes( 342 );
-	}
-
-	/**
-	 * Test save_meta_boxes() should not save when the expected nonce fails.
-	 */
-	public function test_should_not_save_when_the_expected_nonce_fails() {
-		Monkey\Functions\expect( 'spiralWebDB\Metadata\get_meta_box_keys' )
-			->once()
-			->withNoArgs()
-			->andReturn( [ 'meta_box.members' ] );
-		Monkey\Functions\expect( 'spiralWebDB\Metadata\get_meta_box_id' )
-			->once()
-			->with( 'meta_box.members' )
-			->andReturn( 'members' );
-		Monkey\Functions\expect( 'spiralWebDB\Metadata\is_okay_to_save_meta_box' )
-			->once()
-			->with( 'members' )
-			->andReturn( false );
-		Monkey\Functions\expect( 'KnowTheCode\ConfigStore\getConfigParameter' )->never();
-		Monkey\Functions\expect( 'spiralWebDB\Metadata\save_custom_fields' )->never();
-		save_meta_boxes( 349 );
-	}
-
-	/**
 	 * Test save_meta_boxes() should save when one or more valid post meta values are added.
 	 */
 	public function test_should_save_when_one_or_more_valid_post_meta_values_are_added() {
@@ -138,17 +81,29 @@ class Tests_SaveMetaBoxes extends Test_Case {
 			->once()
 			->with( 'meta_box.members' )
 			->andReturn( 'members' );
-		Monkey\Functions\when( 'spiralWebDB\Metadata\is_okay_to_save_meta_box' )
-			->justReturn( 'wp_verify_nonce' );
-		Monkey\Functions\when( 'KnowTheCode\ConfigStore\getConfigParameter' )
-			->justReturn( [
+		Monkey\Functions\expect( 'spiralWebDB\Metadata\is_okay_to_save_meta_box' )
+			->once()
+			->with( 'members' )
+			->andReturn( true );
+		Monkey\Functions\expect( 'KnowTheCode\ConfigStore\getConfigParameter' )
+			->once()
+			->with( 'meta_box.members', 'custom_fields' )
+			->andReturn( [
 				'role'            => 'Soprano',
 				'residence_city'  => 'Chicago',
 				'residence_state' => 'IL',
 				'tour_number'     => '3',
 			] );
-		Monkey\Functions\when( 'spiralWebDB\Metadata\save_custom_fields' )
-			->justReturn();
+		$config = [
+			'role'            => 'Soprano',
+			'residence_city'  => 'Chicago',
+			'residence_state' => 'IL',
+			'tour_number'     => '3',
+		];
+		Monkey\Functions\expect( 'spiralWebDB\Metadata\save_custom_fields' )
+			->once()
+			->with( $config, 'members', 354 )
+			->andReturn( null );
 		save_meta_boxes( 354 );
 	}
 }
