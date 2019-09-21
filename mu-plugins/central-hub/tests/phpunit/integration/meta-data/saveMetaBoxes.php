0d865de671c34e4fbb308dd65443fd21da22972e
Robert Gadon <rgadon107@gmail.com>
2019-09-21 13:24:12 -0500

    Modify second integration test.
    - Update the test description.
    - Delete loading config into store.
    - Remove `get_post_meta()`. Not needed.

--- mu-plugins/central-hub/tests/phpunit/integration/meta-data/saveMetaBoxes.php
+++ mu-plugins/central-hub/tests/phpunit/integration/meta-data/saveMetaBoxes.php
@@ -71,20 +71,16 @@ class Tests_SaveMetaBoxes extends Test_Case {
 	}
 
 	/**
-	 * Test save_meta_boxes() should not save when no meta box keys are in the Config Store.
+	 * Test save_meta_boxes() should not save when no meta box keys are in the Config Store or
+	 *    config does not begin with `meta_box.`.
 	 */
 	public function test_should_not_save_when_no_meta_box_keys_are_in_store() {
-		$configs = [];
-		foreach( $configs as $store_key => $config_to_store ) {
-			loadConfig( $store_key, $config_to_store );
-		}
 		$expected = [];
 		save_meta_boxes( $this->post->ID );
 
-		$this->assertSame( $expected, get_post_meta( $this->post->ID ) );
 		$this->assertSame( $expected, get_meta_box_keys() );
 	}
-
+	
 	/**
 	 * Test save_meta_boxes() should check whether okay to save meta box and custom fields.
 	 */
