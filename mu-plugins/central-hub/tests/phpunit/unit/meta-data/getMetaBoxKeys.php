1f386ac4a6a16c4fa8c42694c07e096493a3f0f2
Robert Gadon <rgadon107@gmail.com>
2019-09-23 15:32:10 -0500

    Commit unit tests for `get_meta_box_keys`.
    - `test_returns_empty_array_when_store_key_is_empty_or_does_not_begin_with_meta_box()`
    
    - `test_returns_all_array_keys_that_start_with_meta_box`.

---
+++ mu-plugins/central-hub/tests/phpunit/unit/meta-data/getMetaBoxKeys.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Tests for the function get_meta_box_keys().
+ *
+ * @package     spiralWebDb\centralHub\Tests\Unit\Metadata
+ * @since       1.3.0
+ * @author      Robert Gadon <rgadon107>
+ * @link        https://github.com/rgadon107/cornerstone
+ * @license     GNU-2.0+
+ */
+
+namespace spiralWebDb\centralHub\Tests\Unit\Metadata;
+
+use Brain\Monkey;
+use function spiralWebDB\Metadata\get_meta_box_keys;
+use spiralWebDb\Cornerstone\Tests\Unit\Test_Case;
+
+/**
+ * Class Tests_GetMetaBoxKeys
+ *
+ * @package spiralWebDb\centralHub\Tests\Unit\Metadata
+ * @group   meta-data
+ */
+class Tests_GetMetaBoxKeys extends Test_Case {
+
+	/**
+	 * Prepare the test environment before each test.
+	 */
+	protected function setUp() {
+		parent::setUp();
+
+		require_once CENTRAL_HUB_ROOT_DIR . '/src/meta-data/helpers.php';
+	}
+
+	/**
+	 * Test get_meta_box_keys() returns empty array when store key is empty or does not start with 'meta_box.'
+	 */
+	public function test_returns_empty_array_when_store_key_is_empty_or_does_not_begin_with_meta_box() {
+		$expected = [];
+		get_meta_box_keys();
+
+		$this->assertSame( $expected, get_meta_box_keys() );
+	}
+
+	/**
+	 * Test get_meta_box_keys() returns all array keys that start with `meta_box.`
+	 */
+	public function test_returns_all_array_keys_that_start_with_meta_box() {
+		Monkey\Functions\expect( 'KnowTheCode\ConfigStore\getAllKeys' )
+			->once()
+			->withNoArgs()
+			->andReturn( [
+				'meta_box.events',
+				'meta_box.members',
+				'shortcode.qa',
+				'custom_post_type.books',
+			] );
+
+		Monkey\Functions\expect( 'KnowTheCode\ConfigStore\str_starts_with' )
+			->once()
+			->with( 'meta_box.events', 'meta_box.' )
+			->ordered()
+			->andReturn( true )
+			->andAlsoExpectIt()
+			->once()
+			->with( 'meta_box.members', 'meta_box.' )
+			->ordered()
+			->andReturn( true )
+			->andAlsoExpectIt()
+			->once()
+			->with( 'shortcode.qa', 'meta_box.' )
+			->ordered()
+			->andReturn( false )
+			->andAlsoExpectIt()
+			->once()
+			->with( 'custom_post_type.books', 'meta_box.' )
+			->ordered()
+			->andReturn( false );
+
+		$this->assertSame( [ 'meta_box.events', 'meta_box.members' ], get_meta_box_keys() );
+	}
+}
