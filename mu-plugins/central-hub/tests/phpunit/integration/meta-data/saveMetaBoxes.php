7e02b1cc8cc57fbc3d4fd7dcf7bd6dbb3e2722f0
Robert Gadon <rgadon107@gmail.com>
2019-10-02 15:08:08 -0500

    Update integration test file.
    1) Update the tearDown() method. 
    
    2) Reorder assertions in `test_function_should_register_to_action_hook_when_event_fires()`.
    
    3) Modify first integration test: test_should_not_save_when_no_meta_box_keys_are_in_store().
    
    4) Add second integration test: test_should_not_save_when_store_key_does_not_start_with_meta_box().
    
    5) Add third integration test: test_should_delete_post_meta_keys_passed_to_empty_POST_array_element_when_value_equals_config_delete_state().
    
    6) Modify fourth integration test: test_should_delete_actual_post_meta_values_set_in_POST_when_they_equal_the_config_delete_state().
    
    7) Add fifth integration test: est_should_save_the_sanitized_current_post_meta_from_the_custom_fields_into_the_database().

--- mu-plugins/central-hub/tests/phpunit/integration/meta-data/saveMetaBoxes.php
+++ mu-plugins/central-hub/tests/phpunit/integration/meta-data/saveMetaBoxes.php
@@ -56,37 +56,47 @@ class Tests_SaveMetaBoxes extends Test_Case {
 	 */
 	public function tearDown() {
 		parent::tearDown();
-
+		// Clean up.
 		self::empty_the_store();
+		$_POST = [];
+		delete_post_meta( $this->post, 'role' );
+		delete_post_meta( $this->post, 'residence_city' );
+		delete_post_meta( $this->post, 'residence_state' );
+		delete_post_meta( $this->post, 'tour_number' );
 	}
 
 	/*
 	* Test save_meta_boxes() should register to add_action( save_post' ) when event fires.
 	*/
 	public function test_function_should_register_to_action_hook_when_event_fires() {
-
+		$this->assertTrue( has_action( 'save_post' ) );
 		// `has_action()` returns the priority level of the callback registered to the event ($tag).
 		$this->assertEquals( 10, has_action( 'save_post', 'spiralWebDB\Metadata\save_meta_boxes' ) );
-		$this->assertTrue( has_action( 'save_post' ) );
 	}
 
 	/**
-	 * Test save_meta_boxes() should not save when no meta box keys are in the Config Store or
-	 *    config does not begin with `meta_box.`.
+	 * Test save_meta_boxes() should not save when no meta box keys are in the Config Store.
 	 */
 	public function test_should_not_save_when_no_meta_box_keys_are_in_store() {
-		$expected = [];
-		save_meta_boxes( $this->post->ID );
-
-		$this->assertSame( $expected, get_meta_box_keys() );
+		$configs = [];
+		foreach ( $configs as $store_key => $config_to_store ) {
+			loadConfig( $store_key, $config_to_store );
+		}
+		$_POST = [
+			'post_ID'            => $this->post,
+			'post_status'        => 'publish',
+			'members'            => [],
+			'members_nonce_name' => wp_create_nonce( 'members_nonce_action' ),
+		];
+		$this->assertNull( save_meta_boxes( $this->post ) );
 	}
 
 	/**
-	 * Test save_meta_boxes() should not save when the $meta_box_key is not validated in $_POST.
+	 * Test save_meta_boxes() should not save when the store_key does not start with `meta_box.`.
 	 */
-	public function test_should_not_save_when_the_meta_box_key_is_not_validated_in_POST() {
+	public function test_should_not_save_when_store_key_does_not_start_with_meta_box() {
 		$configs = [
-			'meta_box.members' => [
+			'notametabox.members' => [
 				'custom_fields' => [
 					'role'            => 'Soprano',
 					'residence_city'  => 'Chicago',
@@ -98,90 +108,213 @@ class Tests_SaveMetaBoxes extends Test_Case {
 		foreach ( $configs as $store_key => $config_to_store ) {
 			loadConfig( $store_key, $config_to_store );
 		}
-		$_POST        = [
-			'post_ID'            => $this->post->ID,
-			'post_status'        => 'published',
-			'members_nonce_name' => '0390a99dc5',
+		$_POST = [
+			'post_ID'            => $this->post,
+			'post_status'        => 'publish',
+			'members'            => [],
+			'members_nonce_name' => wp_create_nonce( 'members_nonce_action' ),
 		];
-		$expected     = [ 0 => 'meta_box.members' ];
-		$meta_box_key = 'members';
-		save_meta_boxes( $this->post->ID );
 
-		$this->assertSame( $expected, get_meta_box_keys() );
-		$this->assertSame( $meta_box_key, get_meta_box_id( 'meta_box.members' ) );
-		$this->assertFalse( is_okay_to_save_meta_box( $meta_box_key ) );
+		$this->assertNull( save_meta_boxes( $this->post ) );
 	}
 
 	/**
-	 * Test save_meta_boxes() should not save when the $meta_box_key is not validated by wp_verify_nonce().
+	 * Test save_meta_boxes() should delete post meta keys passed to empty $_POST array element when value equals
+	 * config delete state.
 	 */
-	public function test_should_not_save_when_the_meta_box_key_is_not_validated_by_wp_verify_nonce() {
+	public function test_should_delete_post_meta_keys_passed_to_empty_POST_array_element_when_value_equals_config_delete_state() {
+		// Custom fields config.
 		$configs = [
 			'meta_box.members' => [
 				'custom_fields' => [
-					'role'            => 'Soprano',
-					'residence_city'  => 'Chicago',
-					'residence_state' => 'IL',
-					'tour_number'     => '3',
+					'role'            => [
+						'is_single'    => true,
+						'default'      => '',
+						'delete_state' => '',
+						'sanitize'     => 'sanitize_text_field',
+					],
+					'residence_city'  => [
+						'is_single'    => true,
+						'default'      => '',
+						'delete_state' => '',
+						'sanitize'     => 'sanitize_text_field',
+					],
+					'residence_state' => [
+						'is_single'    => true,
+						'default'      => '',
+						'delete_state' => '',
+						'sanitize'     => 'sanitize_text_field',
+					],
+					'tour_number'     => [
+						'is_single'    => true,
+						'default'      => '',
+						'delete_state' => '',
+						'sanitize'     => 'sanitize_text_field',
+					],
 				],
 			],
 		];
 		foreach ( $configs as $store_key => $config_to_store ) {
 			loadConfig( $store_key, $config_to_store );
 		}
-		$_POST        = [
-			'post_ID'     => $this->post->ID,
-			'post_status' => 'published',
-			'members'     => [
+		// Add post meta to the database.
+		add_post_meta( $this->post, 'role', 'Bass/Baritone' );
+		add_post_meta( $this->post, 'residence_city', 'Houston' );
+		add_post_meta( $this->post, 'residence_state', 'TX' );
+		add_post_meta( $this->post, 'tour_number', '1' );
+		$_POST = [
+			'post_ID'            => $this->post,
+			'post_status'        => 'publish',
+			'members'            => [],
+			'members_nonce_name' => wp_create_nonce( 'members_nonce_action' ),
+		];
+		// Before calling the function, check that post meta was added to the database.
+		$this->assertSame( 'Bass/Baritone', get_post_meta( $this->post, 'role', true ) );
+		$this->assertSame( 'Houston', get_post_meta( $this->post, 'residence_city', true ) );
+		$this->assertSame( 'TX', get_post_meta( $this->post, 'residence_state', true ) );
+		$this->assertSame( '1', get_post_meta( $this->post, 'tour_number', true ) );
+
+		save_meta_boxes( $this->post );
+
+		// Check that post meta keys are deleted when their value equals the config delete state.
+		$this->assertSame( [], $_POST['members'] );
+		$this->assertSame( '', get_post_meta( $this->post, 'role', true ) );
+		$this->assertSame( '', get_post_meta( $this->post, 'residence_city', true ) );
+		$this->assertSame( '', get_post_meta( $this->post, 'residence_state', true ) );
+		$this->assertSame( '', get_post_meta( $this->post, 'tour_number', true ) );
+	}
+
+	/**
+	 * Test save_meta_boxes() should delete actual post meta values set in $_POST when they equal the config delete
+	 * state.
+	 */
+	public function test_should_delete_actual_post_meta_values_set_in_POST_when_they_equal_the_config_delete_state() {
+		// Custom fields config.
+		$configs = [
+			'meta_box.members' => [
+				'custom_fields' => [
+					'role'            => [
+						'is_single'    => true,
+						'delete_state' => '',
+						'sanitize'     => 'sanitize_text_field',
+					],
+					'residence_city'  => [
+						'is_single'    => true,
+						'delete_state' => '',
+						'sanitize'     => 'sanitize_text_field',
+					],
+					'residence_state' => [
+						'is_single'    => true,
+						'delete_state' => '',
+						'sanitize'     => 'sanitize_text_field',
+					],
+					'tour_number'     => [
+						'is_single'    => true,
+						'delete_state' => '',
+						'sanitize'     => 'sanitize_text_field',
+					],
+				],
+			],
+		];
+		foreach ( $configs as $store_key => $config_to_store ) {
+			loadConfig( $store_key, $config_to_store );
+		}
+		// Add post meta to the database.
+		add_post_meta( $this->post, 'role', 'Tenor Trombone' );
+		add_post_meta( $this->post, 'residence_city', 'Philadelphia' );
+		add_post_meta( $this->post, 'residence_state', 'PA' );
+		add_post_meta( $this->post, 'tour_number', '2' );
+		// $_POST contains the actual value for each custom field.
+		$_POST = [
+			'post_ID'            => $this->post,
+			'post_status'        => 'publish',
+			'members'            => [
+				'role'            => '',
+				'residence_city'  => '',
+				'residence_state' => '',
+				'tour_number'     => '',
+			],
+			'members_nonce_name' => wp_create_nonce( 'members_nonce_action' ),
+		];
+		// Before calling the function, check that post meta was added to the database.
+		$this->assertSame( 'Tenor Trombone', get_post_meta( $this->post, 'role', true ) );
+		$this->assertSame( 'Philadelphia', get_post_meta( $this->post, 'residence_city', true ) );
+		$this->assertSame( 'PA', get_post_meta( $this->post, 'residence_state', true ) );
+		$this->assertSame( '2', get_post_meta( $this->post, 'tour_number', true ) );
+
+		save_meta_boxes( $this->post );
+
+		// Check that post meta keys are deleted when their value equals the config delete state.
+		$this->assertSame( '', $_POST['members']['role'] );
+		$this->assertSame( '', $_POST['members']['residence_city'] );
+		$this->assertSame( '', $_POST['members']['residence_state'] );
+		$this->assertSame( '', $_POST['members']['tour_number'] );
+		$this->assertSame( '', get_post_meta( $this->post, 'role', true ) );
+		$this->assertSame( '', get_post_meta( $this->post, 'residence_city', true ) );
+		$this->assertSame( '', get_post_meta( $this->post, 'residence_state', true ) );
+		$this->assertSame( '', get_post_meta( $this->post, 'tour_number', true ) );
+	}
+
+	/**
+	 * Test save_meta_boxes() should save the sanitized current post meta from the custom field into the database.
+	 */
+	public function test_should_save_the_sanitized_current_post_meta_from_the_custom_fields_into_the_database() {
+		$configs = [
+			'meta_box.members' => [
+				'custom_fields' => [
+					'role'            => [
+						'is_single'    => true,
+						'default'      => '',
+						'delete_state' => '',
+						'sanitize'     => 'sanitize_text_field',
+					],
+					'residence_city'  => [
+						'is_single'    => true,
+						'default'      => '',
+						'delete_state' => '',
+						'sanitize'     => 'sanitize_text_field',
+					],
+					'residence_state' => [
+						'is_single'    => true,
+						'default'      => '',
+						'delete_state' => '',
+						'sanitize'     => 'sanitize_text_field',
+					],
+					'tour_number'     => [
+						'is_single'    => true,
+						'default'      => '',
+						'delete_state' => '',
+						'sanitize'     => 'sanitize_text_field',
+					],
+				],
+			],
+		];
+		foreach ( $configs as $store_key => $config_to_store ) {
+			loadConfig( $store_key, $config_to_store );
+		}
+		$_POST = [
+			'post_ID'            => $this->post,
+			'post_status'        => 'publish',
+			'members'            => [
 				'role'            => 'Soprano',
 				'residence_city'  => 'Chicago',
 				'residence_state' => 'IL',
 				'tour_number'     => '3',
 			],
+			'members_nonce_name' => wp_create_nonce( 'members_nonce_action' ),
 		];
-		$expected     = [ 0 => 'meta_box.members' ];
-		$meta_box_key = 'members';
-		save_meta_boxes( $this->post->ID );
+		// Before calling the function, check that post meta is not in the database.
+		$this->assertSame( '', get_post_meta( $this->post, 'role', true ) );
+		$this->assertSame( '', get_post_meta( $this->post, 'residence_city', true ) );
+		$this->assertSame( '', get_post_meta( $this->post, 'residence_state', true ) );
+		$this->assertSame( '', get_post_meta( $this->post, 'tour_number', true ) );
 
-		$this->assertSame( $expected, get_meta_box_keys() );
-		$this->assertSame( $meta_box_key, get_meta_box_id( 'meta_box.members' ) );
-		$this->assertFalse( is_okay_to_save_meta_box( $meta_box_key ) );
-	}
+		save_meta_boxes( $this->post );
 
-
-	/**
-	 * Test save_meta_boxes() should save when one or more valid post meta values are added.
-	 */
-	public function test_should_save_when_one_or_more_valid_post_meta_values_are_added() {
-		$configs = [
-			'meta_box.members' => [
-				'custom_fields' => [
-					'role'            => 'Soprano',
-					'residence_city'  => 'Chicago',
-					'residence_state' => 'IL',
-					'tour_number'     => '3',
-				],
-			],
-		];
-		foreach ( $configs as $store_key => $config_to_store ) {
-			loadConfig( $store_key, $config_to_store );
-		}
-		$_POST                = [
-			'post_ID'              => $this->post->ID,
-			'post_status'          => 'published',
-			'members'              => [
-				'role'            => 'Soprano',
-				'residence_city'  => 'Chicago',
-				'residence_state' => 'IL',
-				'tour_number'     => '3',
-			],
-			'members_nonce_name'   => '0390a99dc5',
-			'members_nonce_action' => 1,
-		];
-		$expected             = [ 0 => 'meta_box.members' ];
-		$meta_box_key         = 'members';
-		$custom_fields_config = $configs['meta_box.members']['custom_fields'];
-
-		save_meta_boxes( $this->post->ID );
+		// After function call, check that database was updated with post meta.
+		$this->assertSame( 'Soprano', get_post_meta( $this->post, 'role', true ) );
+		$this->assertSame( 'Chicago', get_post_meta( $this->post, 'residence_city', true ) );
+		$this->assertSame( 'IL', get_post_meta( $this->post, 'residence_state', true ) );
+		$this->assertSame( '3', get_post_meta( $this->post, 'tour_number', true ) );
 	}
 }
