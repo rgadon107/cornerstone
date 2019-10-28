7984285fa0306441f1dda02d424b838d16013dbf
Robert Gadon <rgadon107@gmail.com>
2019-10-28 13:54:42 -0500

    Write 4th unit test:
    - test_should_return_custom_field_values_passed_through_filter()

--- mu-plugins/central-hub/tests/phpunit/unit/meta-data/getCustomFieldsValues.php
+++ mu-plugins/central-hub/tests/phpunit/unit/meta-data/getCustomFieldsValues.php
@@ -136,69 +136,52 @@ class Tests_GetCustomFieldsValues extends Test_Case {
 
 		$this->assertSame( $expected_custom_fields, get_custom_fields_values( $this->post->ID, 'events', $config ) );
 	}
-//
-//	/**
-//	 * Test get_custom_fields_values() should return filtered custom field values.
-//	 */
-//	public function test_should_return_filtered_custom_field_values() {
-//		// Add post meta to the database so we have something to call.
-//		add_post_meta( $this->post, 'event-date', '10-12-2019' );
-//		add_post_meta( $this->post, 'event-time', '19:30:00' );
-//		add_post_meta( $this->post, 'venue-name', 'Carnegie Hall' );
-//
-//		// Check database that post meta was added.
-//		$this->assertSame( '10-12-2019', get_post_meta( $this->post, 'event-date', true ) );
-//		$this->assertSame( '19:30:00', get_post_meta( $this->post, 'event-time', true ) );
-//		$this->assertSame( 'Carnegie Hall', get_post_meta( $this->post, 'venue-name', true ) );
-//
-//		// Register custom callback to filter $tag with priority of 20 and 3 available arguments.
-//		add_filter( 'filter_meta_box_custom_fields', [ $this, 'filter_custom_field_values' ], 20, 3 );
-//		$this->assertTrue( has_filter( 'filter_meta_box_custom_fields' ) );
-//		$this->assertEquals( 20, has_filter( 'filter_meta_box_custom_fields', [ $this, 'filter_custom_field_values' ] ) );
-//
-//		$updated_custom_fields = [
-//			'event-date' => '10-19-2019',
-//			'event-time' => '15:00:00',
-//			'venue-name' => 'The Fabulous Fox Theater',
-//		];
-//		$this->assertSame( $updated_custom_fields, get_custom_fields_values( $this->post, 'events', $this->config ) );
-//
-//		// Clean up.
-//		remove_all_filters( 'filter_meta_box_custom_fields', 20 );
-//		delete_post_meta( $this->post, 'event-date' );
-//		delete_post_meta( $this->post, 'event-time' );
-//		delete_post_meta( $this->post, 'venue-name' );
-//	}
-//
-//	/*
-//	 * Filter the custom field values with a custom callback.
-//	 *
-//	 * @param array  $custom_fields Array of custom fields values
-//	 * @param string $meta_box_id   Meta box's key (ID) - used to identify this meta box
-//	 * @param int    $post_id       Post's ID
-//	 * @return array Updated custom fields values.
-//	 */
-//	public function filter_custom_field_values( $custom_fields, $meta_box_id, $post_id ) {
-//		$args          = func_get_args();
-//		$expected_args = [
-//			[
-//				'event-date' => '10-12-2019',
-//				'event-time' => '19:30:00',
-//				'venue-name' => 'Carnegie Hall',
-//			],
-//			'events',
-//			$this->post,
-//		];
-//
-//		// Test that the callback received the 3 arguments and the values are as expected.
-//		$this->assertCount( 3, $args );
-//		$this->assertSame( $expected_args, $args );
-//
-//		// Modify the custom field values.
-//		$custom_fields['event-date'] = '10-19-2019';
-//		$custom_fields['event-time'] = '15:00:00';
-//		$custom_fields['venue-name'] = 'The Fabulous Fox Theater';
-//
-//		return $custom_fields;
-//	}
+
+	/**
+	 * Test get_custom_fields_values() should return custom field values passed through filter.
+	 */
+	public function test_should_return_custom_field_values_passed_through_filter() {
+		$config = [
+			'custom_fields' => [
+				'event-date' => [
+					'is_single' => true,
+					'default'   => '',
+				],
+				'event-time' => [
+					'is_single' => true,
+					'default'   => '',
+				],
+				'venue-name' => [
+					'is_single' => true,
+					'default'   => '',
+				],
+			],
+		];
+
+		Monkey\Functions\expect( 'get_post_meta' )
+			->once()
+			->with( $this->post->ID, 'event-date', $config['custom_fields']['event-date']['is_single'] )
+			->andReturn( '10-26-2019' );
+		Monkey\Functions\expect( 'get_post_meta' )
+			->once()
+			->with( $this->post->ID, 'event-time', $config['custom_fields']['event-time']['is_single'] )
+			->andReturn( '19:30:00' );
+		Monkey\Functions\expect( 'get_post_meta' )
+			->once()
+			->with( $this->post->ID, 'venue-name', $config['custom_fields']['venue-name']['is_single'] )
+			->andReturn( 'Carnegie Hall' );
+
+		$expected_custom_fields = [
+			'event-date' => '10-26-2019',
+			'event-time' => '19:30:00',
+			'venue-name' => 'Carnegie Hall',
+		];
+
+		Monkey\Functions\expect( 'apply_filters' )
+			->once()
+			->with( 'filter_meta_box_custom_fields', $expected_custom_fields, 'events', $this->post->ID )
+			->andReturn( $expected_custom_fields );
+
+		$this->assertSame( $expected_custom_fields, get_custom_fields_values( $this->post->ID, 'events', $config ) );
+	}
 }
