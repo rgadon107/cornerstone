68e25ab1a225befdd69a9bfa6d5dce389ad8b713
Tonya Mork <hellofromtonya@knowthecode.io>
2019-11-01 10:48:56 -0500

    Changed the integration test to validate custom fields config initialized.
    Part of the purpose of this function is to merge the original custom fields'
    configuration parameters with the default config parameters.
    
    Why?
    
    To ensure that each parameter exists in the custom fields' config.
    
    In doing so, runtime configs can specify only the parameters that are
    different from the default structure. Why? Less code to manage, test,
    and maintain.

--- mu-plugins/central-hub/tests/phpunit/fixtures/meta-box-members-runtime-config.php
+++ mu-plugins/central-hub/tests/phpunit/fixtures/meta-box-members-runtime-config.php
@@ -10,24 +10,19 @@ return [
 			'callback_args' => null,
 		],
 		'custom_fields' => [
-			'residence_city'  => [
-				'is_single'    => true,
-				'default'      => '',
-				'delete_state' => '',
-				'sanitize'     => 'sanitize_text_field',
-			],
-			'residence_state' => [
-				'is_single'    => true,
-				'default'      => '',
-				'delete_state' => '',
-				'sanitize'     => 'sanitize_text_field',
-			],
+			'residence_city'  => [],
+			'residence_state' => [],
 			'role'            => [
 				'is_single'    => true,
 				'default'      => '',
 				'delete_state' => '',
 				'sanitize'     => 'sanitize_text_field',
 			],
+			'is_active' => [
+				'default'      => 0,
+				'delete_state' => 0,
+				'sanitize'     => 'int_val',
+			]
 		],
 		// Absolute path to config view file.
 		'view'          => '',
