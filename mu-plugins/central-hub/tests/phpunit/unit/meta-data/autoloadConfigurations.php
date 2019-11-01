ec74fa82c11cda16dc26ece8fc0adbcf240161af
Tonya Mork <hellofromtonya@knowthecode.io>
2019-11-01 11:14:18 -0500

    Split the assertions into 2 separate tests as each is different.

--- mu-plugins/central-hub/tests/phpunit/unit/meta-data/autoloadConfigurations.php
+++ mu-plugins/central-hub/tests/phpunit/unit/meta-data/autoloadConfigurations.php
@@ -33,27 +33,41 @@ class Tests_AutoloadConfigurations extends Test_Case {
 	}
 
 	/**
-	 * Test autoload configurations() should load a metabox config, and initialize it's custom fields when given a
-	 * custom config.
+	 * Test autoload configurations() should load a metabox config from the filesystem into Config Store.
 	 */
-	public function test_should_load_a_meta_box_config_and_initialize_the_custom_fields_when_given_a_custom_config() {
+	public function test_should_load_config_from_filesystem_into_config_store() {
 		$config_file = CENTRAL_HUB_ROOT_DIR . '/tests/phpunit/fixtures/meta-box-members-runtime-config.php';
 		$defaults    = (array) require CENTRAL_HUB_ROOT_DIR . '/src/meta-data/defaults/meta-box-config.php';
 		$defaults    = current( $defaults );
 
+		// Assert that loadConfigFromFilesystem() runs once and receives the expected parameters.
 		Monkey\Functions\expect( 'KnowTheCode\ConfigStore\loadConfigFromFilesystem' )
 			->once()
 			->with( $config_file, $defaults )
 			->andReturn( 'meta_box.members' );
+		Monkey\Functions\when( 'spiralWebDB\Metadata\init_custom_fields_configuration' )->justReturn();
+
+		autoload_configurations( [ $config_file ] );
+
+		// This is a placeholder to avoid PHPUnit error. The "expect" above is the assertion in this test.
+		$this->assertTrue( true );
+	}
+
+	/**
+	 * Test autoload configurations() should load the initialized custom fields configuration, i.e. missing custom
+	 * field's configuration parameters are populated by the default config.
+	 */
+	public function test_should_load_initialized_the_custom_fields_config() {
+		Monkey\Functions\when( 'KnowTheCode\ConfigStore\loadConfigFromFilesystem' )->justReturn( 'meta_box.members' );
+		// Assert that init_custom_fields_configuration() runs once and receives the expected parameter.
 		Monkey\Functions\expect( 'spiralWebDB\Metadata\init_custom_fields_configuration' )
 			->once()
 			->with( 'meta_box.members' )
 			->andReturn();
 
-		autoload_configurations( [ $config_file ] );
+		autoload_configurations( [ CENTRAL_HUB_ROOT_DIR . '/tests/phpunit/fixtures/meta-box-members-runtime-config.php' ] );
 
-		// This is a placeholder to avoid PHPUnit error. The assertions are in the "expect" functions above.
+		// This is a placeholder to avoid PHPUnit error. The "expect" above is the assertion in this test.
 		$this->assertTrue( true );
 	}
 }
-
