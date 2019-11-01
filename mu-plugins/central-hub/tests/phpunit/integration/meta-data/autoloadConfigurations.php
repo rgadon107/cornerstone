4af34b0503e2073116cc6c27eef188ee096c1f95
Tonya Mork <hellofromtonya@knowthecode.io>
2019-11-01 12:37:10 -0500

    Added tests to check it handles multiple config files.

--- mu-plugins/central-hub/tests/phpunit/integration/meta-data/autoloadConfigurations.php
+++ mu-plugins/central-hub/tests/phpunit/integration/meta-data/autoloadConfigurations.php
@@ -48,7 +48,91 @@ class Tests_AutoloadConfigurations extends Test_Case {
 
 		// Get the resultant configuration from the Config Store.
 		$actual_config   = getConfig( 'meta_box.members' );
-		$expected_config = [
+		$expected_config = $this->get_expected_members_config();
+
+		// Check that each custom fields' configuration has all of the required parameters.
+		foreach ( $expected_config['custom_fields'] as $meta_key => $custom_field_config ) {
+			$this->assertSame( $custom_field_config, $actual_config['custom_fields'][ $meta_key ] );
+		}
+
+		// Just to make sure, check the entire config array.
+		$this->assertSame( $expected_config, $actual_config );
+
+		// Clean up.
+		self::remove_from_store( 'meta_box.members' );
+	}
+
+	/**
+	 * Test autoload configurations() should load multiple config files from the filesystem into Config Store.
+	 */
+	public function test_should_load_multiple_config_files_from_filesystem_into_config_store() {
+		autoload_configurations( [
+			CENTRAL_HUB_ROOT_DIR . '/tests/phpunit/fixtures/meta-box-events-runtime-config.php',
+			CENTRAL_HUB_ROOT_DIR . '/tests/phpunit/fixtures/meta-box-members-runtime-config.php',
+		] );
+
+		$actual_config = getConfig( 'meta_box.members' );
+		$this->assertArrayHasKey( 'add_meta_box', $actual_config );
+		$this->assertArrayHasKey( 'custom_fields', $actual_config );
+		$this->assertArrayHasKey( 'view', $actual_config );
+		$actual_config = getConfig( 'meta_box.events' );
+		$this->assertArrayHasKey( 'add_meta_box', $actual_config );
+		$this->assertArrayHasKey( 'custom_fields', $actual_config );
+		$this->assertArrayHasKey( 'view', $actual_config );
+
+		// Clean up.
+		self::empty_the_store_by_keys( [ 'meta_box.members', 'meta_box.events' ] );
+	}
+
+	/**
+	 * Test autoload configurations() should load multiple initialized custom fields configurations, i.e. missing custom
+	 * field's configuration parameters are populated by the default config.
+	 */
+	public function test_should_load_multiple_initialized_the_custom_fields_configs() {
+		autoload_configurations( [
+			CENTRAL_HUB_ROOT_DIR . '/tests/phpunit/fixtures/meta-box-events-runtime-config.php',
+			CENTRAL_HUB_ROOT_DIR . '/tests/phpunit/fixtures/meta-box-members-runtime-config.php',
+		] );
+
+		/** Test Members Expected vs. Actual Config. **/
+
+		// Get the resultant configuration from the Config Store.
+		$actual_config   = getConfig( 'meta_box.members' );
+		$expected_config = $this->get_expected_members_config();
+
+		// Check that each custom fields' configuration has all of the required parameters.
+		foreach ( $expected_config['custom_fields'] as $meta_key => $custom_field_config ) {
+			$this->assertSame( $custom_field_config, $actual_config['custom_fields'][ $meta_key ] );
+		}
+
+		// Just to make sure, check the entire config array.
+		$this->assertSame( $expected_config, $actual_config );
+
+		/** Test Events Expected vs. Actual Config. **/
+
+		// Get the resultant configuration from the Config Store.
+		$actual_config   = getConfig( 'meta_box.events' );
+		$expected_config = $this->get_expected_events_config();
+
+		// Check that each custom fields' configuration has all of the required parameters.
+		foreach ( $expected_config['custom_fields'] as $meta_key => $custom_field_config ) {
+			$this->assertSame( $custom_field_config, $actual_config['custom_fields'][ $meta_key ] );
+		}
+
+		// Just to make sure, check the entire config array.
+		$this->assertSame( $expected_config, $actual_config );
+
+		// Clean up.
+		self::empty_the_store_by_keys( [ 'meta_box.members', 'meta_box.events' ] );
+	}
+
+	/**
+	 * Returns the expected members configuration array.
+	 *
+	 * @return array
+	 */
+	private function get_expected_members_config() {
+		return [
 			'add_meta_box'  => [
 				'title'         => 'Tour Member Profile Information',
 				'screen'        => [ 'members' ],
@@ -84,16 +168,43 @@ class Tests_AutoloadConfigurations extends Test_Case {
 			],
 			'view'          => '',
 		];
+	}
 
-		// Check that each custom fields' configuration has all of the required parameters.
-		foreach ( $expected_config['custom_fields'] as $meta_key => $custom_field_config ) {
-			$this->assertSame( $custom_field_config, $actual_config['custom_fields'][ $meta_key ] );
-		}
-
-		// Just to make sure, check the entire config array.
-		$this->assertSame( $expected_config, $actual_config );
-
-		// Clean up.
-		self::remove_from_store( 'meta_box.members' );
+	/**
+	 * Returns the expected events configuration array.
+	 *
+	 * @return array
+	 */
+	private function get_expected_events_config() {
+		return [
+			'add_meta_box'  => [
+				'title'         => 'Event Information',
+				'screen'        => [ 'events' ],
+				'context'       => 'advanced',
+				'priority'      => 'default',
+				'callback_args' => null,
+			],
+			'custom_fields' => [
+				'event-date' => [
+					'is_single'    => true,
+					'default'      => '',
+					'delete_state' => '',
+					'sanitize'     => 'strip_tags',
+				],
+				'event-time' => [
+					'is_single'    => true,
+					'default'      => '',
+					'delete_state' => '',
+					'sanitize'     => 'strip_tags',
+				],
+				'venue-name' => [
+					'is_single'    => true,
+					'default'      => '',
+					'delete_state' => '',
+					'sanitize'     => 'sanitize_text_field',
+				],
+			],
+			'view'          => '',
+		];
 	}
 }
