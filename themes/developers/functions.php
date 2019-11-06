a129c333f731c5690f3aed83ef85bb967569c755
Tonya Mork <hellofromtonya@knowthecode.io>
2019-11-06 15:29:21 -0600

    Added helper functions to the theme's functions.php file.
    These helper functions will aid in customizations throughout the theme.

--- themes/developers/functions.php
+++ themes/developers/functions.php
@@ -10,6 +10,68 @@
  */
 namespace spiralWebDB\Developers;
 
+/**
+ * Get the absolute path to the root directory of the child theme.
+ *
+ * @since 1.0.0
+ *
+ * @return string returns the directory path.
+ */
+function get_theme_dir() {
+	return __DIR__;
+}
+
+/**
+ * Get the URL to the root of the child theme.
+ *
+ * @since 1.0.0
+ *
+ * @return string returns the URL to the root of the child theme.
+ */
+function get_theme_url() {
+	static $url = '';
+
+	if ( empty( $url ) ) {
+		$url = get_stylesheet_directory_uri();
+	}
+
+	return $url;
+}
+
+/**
+ * Get the theme's version.
+ *
+ * @since 1.0.0
+ *
+ * @return string returns the theme's version.
+ */
+function get_theme_version() {
+	static $version = null;
+
+	if ( null !== $version ) {
+		return $version;
+	}
+
+	if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+		$version = filemtime( __DIR__ . '/style.min.css' );
+	} else {
+		$version = ( wp_get_theme() )->get( 'Version' );
+	}
+
+	return $version;
+}
+
+/**
+ * Checks if in a debug/dev environment.
+ *
+ * @since 1.0.0
+ *
+ * @return bool
+ */
+function is_in_debug() {
+	return defined( 'WP_DEBUG' ) && WP_DEBUG;
+}
+
 include_once 'lib/init.php';
 
 include_once 'lib/functions/autoload.php';
