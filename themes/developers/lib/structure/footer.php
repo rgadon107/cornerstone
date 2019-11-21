ecfb7169446590c860392ae5c8bce1b06393b31e
Robert Gadon <rgadon107@gmail.com>
2019-11-21 13:10:48 -0600

    Remove hard-coded client contact view from footer.

--- themes/developers/lib/structure/footer.php
+++ themes/developers/lib/structure/footer.php
@@ -32,24 +32,8 @@ add_action( 'genesis_footer', __NAMESPACE__ . '\add_site_footer_widget_area' );
  *  @return void
  */
 function add_site_footer_widget_area()  {
-	genesis_widget_area( 'donate_call_to_action', array(
+	genesis_widget_area( 'footer_widget_area', array(
 		'before' => '<div class="site-footer--widget-area"><div class="wrap">',
 		'after'  => '</div></div>',
 	) );
-}
-
-add_action( 'genesis_footer', __NAMESPACE__ . '\do_footer_cornerstone_content' );
-/**
- * Customize the site footer to render client info.
- *
- * @since 1.0.0
- *
- * @return void
- */
-function do_footer_cornerstone_content() {
-
-	$site_url   = esc_url( site_url() );
-	$site_title = esc_html( get_bloginfo( 'name' ) );
-
-	require dirname( __DIR__ ) . '/views/site-footer.php';
 }
--- themes/developers/lib/views/site-footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- *  The site footer view.
- */
-namespace spiralWebDB\Developers;
-?>
-<h3 class="site-footer--title"><a class="site-footer--title-link" href="<?php echo $site_url; ?>"><?php echo $site_title ?></a></h3>
-<p class="site-footer--address"><strong>P.O. Box 249, Florissant, MO 63032</strong></p>
-<p class="site-footer--telephone"><strong>Telephone: 1-314-838-4383</strong></p>
