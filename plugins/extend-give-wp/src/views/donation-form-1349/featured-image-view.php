8dc43c774c31a25b8f05f38d1881abf17003e06e
Robert Gadon <rgadon107@gmail.com>
2019-12-15 20:30:22 -0600

    Update view file.
    - Replace reference to $postID with $attachment_id.
    - Remove references to specific attachment or post IDs. 
    - Update arguments to 'wp_get_attachment_image()'.

--- plugins/extend-give-wp/src/views/donation-form-1349/featured-image-view.php
+++ plugins/extend-give-wp/src/views/donation-form-1349/featured-image-view.php
@@ -5,7 +5,7 @@
 namespace spiralWebDB\ExtendGiveWP;
 
 ?>
-<figure id="attachment_<?php echo esc_attr( $postID ) ?>" class="donation-form-featured-image attachment_<?php echo esc_attr( $postID ) ?>" aria-describedby="caption-attachment-<?php echo esc_attr( $postID ) ?>">
-	<?php echo wp_get_attachment_image( 1411, $size = 'large', $icon = false, $attr = array( 'class' => 'featured-image attachment_1411 ID-1411' ) ); ?>
-    <figcaption id="caption-attachment-<?php echo esc_attr( $postID ) ?>" class="post-<?php echo esc_attr( $postID ) ?>-caption"><em><?php echo esc_attr( $post->post_excerpt ); ?></em></figcaption>
+<figure id="attachment_<?php echo esc_attr( $attachment_id ) ?>" class="donation-form-featured-image attachment_<?php echo esc_attr( $attachment_id ) ?>" aria-describedby="caption-attachment-<?php echo esc_attr( $attachment_id ) ?>">
+	<?php echo wp_get_attachment_image( $attachment_id, $size, $icon, $attr = array( 'class' => 'featured-image' ) ); ?>
+    <figcaption id="caption-attachment-<?php echo esc_attr( $attachment_id ) ?>" class="post-<?php echo esc_attr( $attachment_id ) ?>-caption"><em><?php echo esc_attr( $post->post_excerpt ); ?></em></figcaption>
 </figure>
