f9ab4aa2faf7d3ae72e29ec40ccedf2e8584122e
Robert Gadon <rgadon107@gmail.com>
2019-12-15 21:13:03 -0600

    Declare default value of $icon argument to clear PHP error.

--- plugins/extend-give-wp/src/views/donation-form-1349/featured-image-view.php
+++ plugins/extend-give-wp/src/views/donation-form-1349/featured-image-view.php
@@ -6,6 +6,6 @@ namespace spiralWebDB\ExtendGiveWP;
 
 ?>
 <figure id="attachment_<?php echo esc_attr( $attachment_id ) ?>" class="donation-form-featured-image attachment_<?php echo esc_attr( $attachment_id ) ?>" aria-describedby="caption-attachment-<?php echo esc_attr( $attachment_id ) ?>">
-	<?php echo wp_get_attachment_image( $attachment_id, $size, $icon, $attr = array( 'class' => 'featured-image' ) ); ?>
+	<?php echo wp_get_attachment_image( $attachment_id, $size, $icon = false, $attr = array( 'class' => 'featured-image' ) ); ?>
     <figcaption id="caption-attachment-<?php echo esc_attr( $attachment_id ) ?>" class="post-<?php echo esc_attr( $attachment_id ) ?>-caption"><em><?php echo esc_attr( $post->post_excerpt ); ?></em></figcaption>
 </figure>
