b57cba99ef124efa66a4e9357f092dca7925a55a
Robert Gadon <rgadon107@gmail.com>
2019-12-21 23:25:29 -0600

    [WIP]Initialize plugin option settings registered to 'admin_init' hook.

--- plugins/extend-give-wp/src/admin/admin-page.php
+++ plugins/extend-give-wp/src/admin/admin-page.php
@@ -17,6 +17,31 @@ namespace spiralWebDB\ExtendGiveWP\Admin;
 
 use function spiralWebDB\ExtendGiveWP\_get_plugin_dir;
 
+add_action( 'admin_init', __NAMESPACE__ . '\initialize_option_settings' );
+/*
+ * Initialize plugin option settings.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function initialize_option_settings() {
+	$args = [
+		'type'              => 'integer',
+		'group'             => 'extend_give_wp_options',
+		'description'       => 'The image ID for the donation form featured image.',
+		'sanitize_callback' => null, // A callback function that sanitizes the option's value.
+		'show_in_rest'      => false,
+	];
+
+	register_setting( 'extend_give_wp_options', 'featured_image_id', $args );
+
+	add_settings_section();
+
+	add_settings_field();
+}
+
+
 add_action( 'admin_menu', __NAMESPACE__ . '\add_option_settings_page' );
 /*
  * Add an option settings page to the plugin admin.
