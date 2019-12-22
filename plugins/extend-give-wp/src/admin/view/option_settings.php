71d7505ff820114803a7c245881edcfcd1410e31
Robert Gadon <rgadon107@gmail.com>
2019-12-21 20:50:37 -0600

    [WIP] Initial markup for option settings view file.

---
+++ plugins/extend-give-wp/src/admin/view/option_settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *  The option settings view file for the Extend GiveWP plugin.
+ */
+?>
+<div class="wrap">
+    <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+    <form action="options.php" method="post">
+    </form>
+</div>
