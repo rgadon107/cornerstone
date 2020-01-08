516de88796bb151ddee41b75a5a8d98dab3f10a1
Robert Gadon <rgadon107@gmail.com>
2020-01-08 13:04:51 -0600

    Update view file.

--- plugins/extend-give-wp/src/admin/view/option_settings.php
+++ plugins/extend-give-wp/src/admin/view/option_settings.php
@@ -10,15 +10,14 @@
             <tbody>
             <tr>
                 <th scope="row">
-                    <label for="featured_image">Featured Image ID</label>
+                    <label for="featured_image_id">Featured Image ID</label>
                 </th>
                 <td>
-                    <input id="featured_image" class="regular-text" name="feature-image" type="number"
-                           min="1" aria-describedby="featured-image-attachment-id" value="" >
-                    <p id="featured-image-input-label" class="description">Enter the image ID for the donation form featured image.</p>
-                    <p id="featured-image-input-label" class="description">Get the ID by opening the Media Library, and selecting the featured image.</p>
+                    <input id="featured_image_id" class="normal-text" name="featured-image-id" type="number"
+                           min="1" aria-describedby="featured-image-attachment-id" placeholder="1" value="" >
+                    <p id="featured-image-input-label" class="description">Enter the image ID for the donation form featured image in the field above.</p>
+                    <p id="featured-image-input-label" class="description">Get the ID by opening the Media Library, and select the featured image.</p>
                     <p id="featured-image-input-label" class="description">View the permalink on the ‘Attachment Details’ page. The ID is the value of the ‘?item=‘ parameter in the permalink.</p>
-
                 </td>
             </tr>
             </tbody>
