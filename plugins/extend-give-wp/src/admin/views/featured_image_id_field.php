21d9c98c39bdc73629cc746d06dafcb9ff603a19
Robert Gadon <rgadon107@gmail.com>
2020-01-10 00:03:18 -0600

    Edit view file.
    - Get the option value entered into the form. 
    - Edit 'min' and 'placeholder' attributes.

--- plugins/extend-give-wp/src/admin/views/featured_image_id_field.php
+++ plugins/extend-give-wp/src/admin/views/featured_image_id_field.php
@@ -5,7 +5,7 @@
 ?>
 <label>
 	<input id="featured_image_id" class="normal-text" name="featured-image-id" type="number"
-	       min="1" aria-describedby="featured-image-attachment-id" placeholder="1" value="" >
+	       min="0" aria-describedby="featured-image-attachment-id" placeholder="0" value="<?php get_option( 'extend-give-wp_featured_image_id'); ?>" >
 	<p id="featured-image-input-label" class="description">Enter the image ID for the donation form featured image in the field above.</p>
 	<p id="featured-image-input-label" class="description">Get the ID by opening the Media Library, and select the featured image.</p>
 	<p id="featured-image-input-label" class="description">View the permalink on the ‘Attachment Details’ page. The ID is the value of the ‘?item=‘ parameter in the permalink.</p>
