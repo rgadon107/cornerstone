071c0b350d6ab4e42a116341ca1c97b6445168b0
Robert Gadon <rgadon107@gmail.com>
2020-01-13 16:51:04 -0600

    Explicitly declare $echo param in 'menu_page_url'.

--- plugins/extend-give-wp/src/admin/views/option-page-template.php
+++ plugins/extend-give-wp/src/admin/views/option-page-template.php
@@ -5,7 +5,7 @@
 ?>
 <div class="wrap">
     <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-    <form action="<?php esc_url( menu_page_url( 'extend-give-wp-options' ) ); ?>" method="post">
+    <form action="<?php esc_url( menu_page_url( 'extend-give-wp-options', true ) ); ?>" method="post">
 		<?php settings_fields( 'extend-give-wp-options' ); ?>
 		<?php do_settings_sections( 'extend-give-wp-options' ); ?>
 		<?php submit_button( 'Save Settings', 'primary' ); ?>
