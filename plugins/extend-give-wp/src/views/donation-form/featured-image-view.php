95f2431453f6485e8382ff88f30c7e900613a8ee
Robert Gadon <rgadon107@gmail.com>
2020-01-15 12:22:35 -0600

    Update donation-form view file.
    - Edit class attributes to add 'donate-page-featured-image.
    - Rely on this class to style donation form featured image rather than a specific attachment_id.

--- plugins/extend-give-wp/src/views/donation-form/featured-image-view.php
+++ plugins/extend-give-wp/src/views/donation-form/featured-image-view.php
@@ -5,7 +5,7 @@
 namespace spiralWebDB\ExtendGiveWP;
 
 ?>
-<figure id="donation-form-featured-image" class="attachment-<?php echo esc_attr( $attachment_id ) ?>" aria-describedby="donation-form-featured-image">
-	<?php echo wp_get_attachment_image( $attachment_id, $size, $icon = false, $attr = array( 'class' => 'donation-form-featured-image' ) ); ?>
+<figure id="donation-form-featured-image" class="donate-page-featured-image attachment-<?php echo esc_attr( $attachment_id ) ?>" aria-describedby="donation-form-featured-image">
+	<?php echo wp_get_attachment_image( $attachment_id, $size, $icon = false, $attr = array( 'class' => 'featured-image' ) ); ?>
     <figcaption id="donation-form-image-caption" class="featured-image-caption"><em><?php echo esc_attr( $post->post_excerpt ); ?></em></figcaption>
 </figure>
