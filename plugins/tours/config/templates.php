e9115a0cb0bfbd1d5b962945d10e8dc361097001
Robert Gadon <rgadon107@gmail.com>
2020-03-11 17:29:45 -0500

    Add file header for template config.

---
+++ plugins/tours/config/templates.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *  Description
+ *
+ * @package    spiralWebDb\CornerstoneTours\Template
+ * @since      1.0.0
+ * @author     Robert A. Gadon
+ * @link       http://spiralwebdb.com
+ * @license    GNU General Public License 2.0+
+ */
+
+namespace spiralWebDb\CornerstoneTours\Template;
+
+use function spiralWebDb\CornerstoneTours\_get_plugin_directory;
+
+// Add template configuration.
