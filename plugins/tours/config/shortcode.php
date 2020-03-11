fda6cd2c462573fce3a235fa8ba66fa2ec515cee
Robert Gadon <rgadon107@gmail.com>
2020-03-11 17:29:32 -0500

    Add file header for shortcode config.

---
+++ plugins/tours/config/shortcode.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Tours shortcode runtime configuration parameters.
+ *
+ * @package    spiralWebDb\CornerstoneTours\Shortcode
+ * @since      1.0.0
+ * @author     Robert A. Gadon
+ * @link       http://spiralwebdb.com
+ * @license    GPL-2.0+
+ */
+
+namespace spiralWebDb\CornerstoneTours\Shortcode;
+
+use function spiralWebDb\Tours\_get_plugin_directory;
+
+// Add shortcode configuration
