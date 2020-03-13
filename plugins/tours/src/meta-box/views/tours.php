f4df6ca3c86d30bff223ce8f4f6ca41c21cfa730
Robert Gadon <rgadon107@gmail.com>
2020-03-13 01:03:29 -0500

    Remove single quotes from around the 'name' parameter.

--- plugins/tours/src/meta-box/views/tours.php
+++ plugins/tours/src/meta-box/views/tours.php
@@ -3,7 +3,7 @@
 <p class="tour-year">
     <label for="tour"><strong>Tour Year</strong></label>
     <p>
-        <input id="tour-year" type="text" name="<?php echo $meta_box_id; ?>['tour-year']"
+        <input id="tour-year" type="text" name="<?php echo $meta_box_id; ?>[tour-year]"
                value="<?php echo esc_attr( $custom_fields['tour-year'] ); ?>">
     </p>
     <span class="description">The year of this Cornerstone tour.</span>
@@ -11,7 +11,7 @@
 <p class="tour-dates">
     <label class="description"><strong>Tour Dates</strong></label>
     <p>
-        <input id="tour-dates" type="text" name="<?php echo $meta_box_id; ?>['tour-dates']"
+        <input id="tour-dates" type="text" name="<?php echo $meta_box_id; ?>[tour-dates]"
                value="<?php echo esc_attr( $custom_fields['tour-dates'] ); ?>">
     </p>
     <span class="description">The date range for the year of this tour.</span>
@@ -19,7 +19,7 @@
 <p class="tour-region">
     <label class="description"><strong>Geographic Region</strong></label>
     <p>
-        <input id="tour-region" type="text" name="<?php echo $meta_box_id; ?>['tour-region']"
+        <input id="tour-region" type="text" name="<?php echo $meta_box_id; ?>[tour-region]"
                value="<?php echo esc_attr( $custom_fields['tour-region'] ); ?>">
     </p>
     <span class="description">The region(s) visited during this tour (e.g. Midwest, Southeast, etc.)</span>
@@ -27,7 +27,7 @@
 <p class="tour-comments">
     <label class="description"><strong>Comments About Tour</strong></label>
     <p>
-        <input id="tour-comments" type="text" name="<?php echo $meta_box_id; ?>['tour-comments']"
+        <input id="tour-comments" type="text" name="<?php echo $meta_box_id; ?>[tour-comments]"
                value="<?php echo esc_attr( $custom_fields['tour-comments'] ); ?>">
     </p>
     <span class="description">Notable events ( e.g. Performance at Carnegie Hall, NYC )</span>
