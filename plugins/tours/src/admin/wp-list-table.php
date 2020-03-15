285fcb5c61e98507109cca5af707977b0fb7105c
Robert Gadon <rgadon107@gmail.com>
2020-03-14 22:37:17 -0500

    Customize 'Tours' admin page.
    - Update or add columns for title, tour_id, tour_year, and menu_order.
    - Render custom column content for tour_id, tour_year, and menu_order.
    - Set sortable columns for tour_id, tour_year, or menu_order.

---
+++ plugins/tours/src/admin/wp-list-table.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ *  Custom columns for the Tours post type.
+ *
+ * @package    spiralWebDb\CornerstoneTours
+ * @since      1.0.0
+ * @author     Robert A. Gadon
+ * @link       http://spiralwebdb.com
+ * @license    GNU General Public License 2.0+
+ */
+
+namespace spiralWebDb\CornerstoneTours;
+
+add_filter( 'manage_tours_posts_columns', __NAMESPACE__ . '\set_custom_columns' );
+/**
+ * Add custom columns to Events Admin.
+ *
+ * @return array Array of columns.
+ * @since 1.0.0
+ *
+ */
+function set_custom_columns() {
+	return array(
+		'cb'         => '<input type="checkbox"/>',
+		'title'      => 'Tour Name',
+		'tour_id'    => 'Tour ID',
+		'tour_year'  => 'Tour Year',
+		'menu_order' => 'Order Number',
+	);
+}
+
+add_action( 'manage_tours_posts_custom_column', __NAMESPACE__ . '\_render_custom_column_content', 10, 2 );
+/**
+ *  Display the content for each custom column.
+ *
+ * @param string $column_name The name of the column to display.
+ * @param int    $tour_id     The current tour (post) ID.
+ *
+ * @since 1.0.0
+ *
+ */
+function _render_custom_column_content( $column_name, $tour_id ) {
+	switch ( $column_name ) {
+		case 'tour_id':
+			echo (int) $tour_id;
+			break;
+		case 'tour_year':
+			$tour_year = (int) get_post_meta( $tour_id, 'tour_year', true );
+			echo esc_html( $tour_year );
+			break;
+		case 'menu_order':
+			echo (int) get_post_field( 'menu_order', $tour_id );
+			break;
+	}
+}
+
+add_filter( 'manage_edit-tours_sortable_columns', __NAMESPACE__ . '\_set_sortable_columns' );
+/**
+ * Set sortable columns on Events Admin page.
+ *
+ * @return array New sortable columns.
+ * @since 1.0.0
+ *
+ */
+function _set_sortable_columns() {
+	return array(
+		'tour_id'    => 'Tour ID',
+		'tour_year'  => 'Tour Year',
+		'menu_order' => 'Order Number',
+	);
+}
