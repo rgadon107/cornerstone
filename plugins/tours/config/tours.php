65d2a5874bf1f8656ec526cf808a014dfe615ffc
Robert Gadon <rgadon107@gmail.com>
2020-03-14 21:39:11 -0500

    Change the title of the meta box.

--- plugins/tours/config/tours.php
+++ plugins/tours/config/tours.php
@@ -29,7 +29,7 @@ return array(
 		'add_meta_box'  => array(
 			// 'id' is not needed as the meta box id/key is defined above
 			// Title of the meta box
-			'title'         => 'Cornerstone Tours Custom Fields',
+			'title'         => 'Past Tour Custom Fields',
 			// The screen or screens on which to show the box
 			// such as a post type, link, comment, etc.
 			'screen'        => array( 'tours' ),
