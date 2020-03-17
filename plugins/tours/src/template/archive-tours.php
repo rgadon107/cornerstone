44226759644c7bd5ae7e00370329e126373351b9
Robert Gadon <rgadon107@gmail.com>
2020-03-16 22:28:12 -0500

    Add single-tours and archive-tours template files to plugin.

---
+++ plugins/tours/src/template/archive-tours.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *  Template for a 'tours' post_type archive.
+ *
+ * @package    spiralWebDb\CornerstoneTours\Template
+ * @since      1.0.0
+ * @author     Robert A. Gadon
+ * @link       http://spiralwebdb.com
+ * @license    GNU General Public License 2.0+
+ */
+
+namespace spiralWebDb\CornerstoneTours\Template;
+
+require __DIR__ . '/single-tours.php';
+
---
+++ plugins/tours/src/template/single-tours.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *  Template for a single 'tours' post_type.
+ *
+ * @package    spiralWebDb\CornerstoneTours\Template
+ * @since      1.0.0
+ * @author     Robert A. Gadon
+ * @link       http://spiralwebdb.com
+ * @license    GNU General Public License 2.0+
+ */
+
+namespace spiralWebDb\CornerstoneTours\Template;
+
+genesis();
+
