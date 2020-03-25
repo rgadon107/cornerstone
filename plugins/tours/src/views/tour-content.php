53a593cd5463c5e4ca03262aee7ee0af28ed69be
Robert Gadon <rgadon107@gmail.com>
2020-03-25 14:39:44 -0500

    Update microdata & h3 header for view file.

--- plugins/tours/src/views/tour-content.php
+++ plugins/tours/src/views/tour-content.php
@@ -6,15 +6,15 @@
 namespace spiralWebDb\CornerstoneTours;
 
 ?>
-<div class="past-tour past-tour-entry-content tour-<?php echo esc_attr( $tour_id ); ?>" itemprop="" itemscope
-     itemtype="http://schema.org/XXX">
+<div class="past-tour past-tour-entry-content tour-<?php echo esc_attr( $tour_id ); ?>" itemprop="pastConcertTour" itemscope
+     itemtype="http://schema.org/MusicEvent">
     <div class="revealer--visible">
         <span class="revealer--icon <?php echo $show_icon; ?>" aria-hidden="true"
               data-show-icon="<?php echo $show_icon; ?>"
               data-hide-icon="<?php echo $hide_icon; ?>">
         </span>
-        <h3 class="revealer--tour-content-header">See past tour performances.</h3>
-        <span class="screen-reader-text">See past tour performances.</span>
+        <h3 class="revealer--tour-content-header">See performance locations and venues for this tour.</h3>
+        <span class="screen-reader-text">See performance locations and venues for this tour.</span>
     </div>
     <div class="revealer--hidden" itemprop="description" style="display: none;">
 		<?php echo get_the_content(); ?>
