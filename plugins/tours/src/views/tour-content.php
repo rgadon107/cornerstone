56fd31f7487d6e55025b47c87ddbfe1a4a0dacf4
Robert Gadon <rgadon107@gmail.com>
2020-03-25 14:44:20 -0500

    Reorder screen-reader-text. Update microdata.

--- plugins/tours/src/views/tour-content.php
+++ plugins/tours/src/views/tour-content.php
@@ -12,9 +12,9 @@ namespace spiralWebDb\CornerstoneTours;
         <span class="revealer--icon <?php echo $show_icon; ?>" aria-hidden="true"
               data-show-icon="<?php echo $show_icon; ?>"
               data-hide-icon="<?php echo $hide_icon; ?>">
+            <span class="screen-reader-text">See performance locations and venues for this tour.</span>
         </span>
-        <h3 class="revealer--tour-content-header">See performance locations and venues for this tour.</h3>
-        <span class="screen-reader-text">See performance locations and venues for this tour.</span>
+        <h3 class="revealer--tour-content-header" itemprop="text">See performance locations and venues for this tour.</h3>
     </div>
     <div class="revealer--hidden" itemprop="description" style="display: none;">
 		<?php echo get_the_content(); ?>
