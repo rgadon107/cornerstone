1497d98a8e3469c6a442ae86d34dbeafb5df4c73
Robert Gadon <rgadon107@gmail.com>
2020-03-31 22:46:08 -0500

    Add unit and integration test bootstrap.php files to 'tour's plugin.

---
+++ plugins/tours/tests/phpunit/unit/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Bootstraps the Unit Tests.
+ *
+ * @package     spiralWebDb\CornerstoneTours\Tests\Unit
+ * @since       1.0.0
+ * @author      Robert Gadon <rgadon107>
+ * @link        https://github.com/rgadon107/cornerstone
+ * @license     GNU-2.0+
+ */
+
+namespace spiralWebDb\CornerstoneTours\Tests\Unit;
+
+define( 'TOURS_ROOT_DIR', dirname( dirname( dirname( __DIR__ ) ) ) );
