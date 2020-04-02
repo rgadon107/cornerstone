2a156bbbb618fe5328832d37bab2ba5f0488b1dd
Robert Gadon <rgadon107@gmail.com>
2020-04-01 22:33:04 -0500

    [WIP]Update 2nd integration test.
    -Use heredoc syntax to render $expected_html. 
    - Use output buffer to fire function under test & grab the HTML from the buffer. 
    - Note: echo in front of FUT outputs the prefiltered value of $text. W/o echo, FUT returns empty string.

--- plugins/tours/tests/phpunit/integration/admin/changeTitlePlaceholderText.php
+++ plugins/tours/tests/phpunit/integration/admin/changeTitlePlaceholderText.php
@@ -48,7 +48,7 @@ class Tests_ChangeTitlePlaceholderText extends Test_Case {
 	 * Test change_title_placeholder_text() should return the given text when post type is 'post'.
 	 */
 	public function test_should_return_given_text_when_post_type_is_not_tours() {
-		// Create and get the post ID via the factory method.
+		// Create and get the post ID for the 'post' post_type via the factory method.
 		$post_id = self::factory()->post->create();
 		get_post_type( $post_id );
 		$text = 'Add title.';
@@ -60,12 +60,20 @@ class Tests_ChangeTitlePlaceholderText extends Test_Case {
 	 * Test change_title_placeholder_text() should return modified text when post type is 'tours'.
 	 */
 	public function test_should_return_modified_text_when_post_type_is_tours() {
-		// Create and get the post ID and 'tour's post_type via the factory method.
+		// Create and get the post ID and for the 'tours' post_type via the factory method.
 		$post_id = $this->factory()->post->create( [ 'post_type' => 'tours' ] );
-		get_post_type( $post_id ); // returns (string) 'tours'.
-		$text     = 'Add title.';
-		$expected = '<em>' . 'Theme of this Cornerstone tour.' . '</em>';
+		get_post_type( $post_id );
+		$text          = 'Add title.';
+		$expected_html = <<<PLACEHOLDER
+'<em>' . 'Theme of this Cornerstone tour.' . '</em>'
+PLACEHOLDER;
 
-		$this->assertSame( $expected, change_title_placeholder_text( $text ) );
+		// Fire the function under test and grab the HTML out of the buffer.
+		ob_start();
+		echo change_title_placeholder_text( $text );
+		$actual_html = ob_get_clean();
+
+		// Test the HTML.
+		$this->assertContains( $expected_html, $actual_html );
 	}
 }
