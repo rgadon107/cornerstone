5afc350ace67e6e0c2529706487bd91055cbaaf0
Robert Gadon <rgadon107@gmail.com>
2020-04-05 15:30:14 -0500

    Updated integration test file.
    - Updated name of function under test (added underscore to name). 
    - Changed test method description and method name.
    - Reduced test to single assertion (assertSame()). 
    - Removed second test method.

--- plugins/tours/tests/phpunit/integration/setCustomColumns.php
+++ plugins/tours/tests/phpunit/integration/setCustomColumns.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Tests for set_custom_columns().
+ * Tests for _set_custom_columns().
  *
  * @package     spiralWebDb\CornerstoneTours\Tests\Integration
  * @since       1.0.0
@@ -12,7 +12,7 @@
 namespace spiralWebDb\CornerstoneTours\Tests\Integration;
 
 use spiralWebDb\Cornerstone\Tests\Integration\Test_Case;
-use function spiralWebDb\CornerstoneTours\set_custom_columns;
+use function spiralWebDb\CornerstoneTours\_set_custom_columns;
 
 /**
  * Class Tests_SetCustomColumns
@@ -24,34 +24,18 @@ use function spiralWebDb\CornerstoneTours\set_custom_columns;
 class Tests_SetCustomColumns extends Test_Case {
 
 	/*
-     * Test set_custom_columns() should return array of default admin columns for 'post' post type.
+     * Test _set_custom_columns() should return expected array of custom admin columns.
      */
-	public function test_should_return_array_of_default_post_admin_columns() {
-		$post_columns = [
-			'cb'    => '<input type="checkbox"/>',
-			'title' => _x( 'Title', 'column name' ),
-			'date'  => __( 'Date' ),
+	public function test_should_return_expected_array_of_custom_columns() {
+		 $expected = [
+			 'cb'         => '<input type="checkbox"/>',
+			 'title'      => 'Tour Name',
+			 'tour_id'    => 'Tour ID',
+			 'tour_year'  => 'Tour Year',
+			 'menu_order' => 'Order Number',
 		];
 
-		$this->assertTrue( is_array( set_custom_columns( $post_columns ) ) );
-		$this->assertArrayHasKey( 'cb', set_custom_columns( $post_columns ) );
-		$this->assertArrayHasKey( 'title', set_custom_columns( $post_columns ) );
-	}
-
-	/*
-	 * Test set_custom_columns() should return filtered array of additional admin columns for 'tours' post type.
-	 */
-	public function test_should_return_filtered_array_of_additional_tours_admin_columns() {
-		$post_columns = [
-			'cb'    => '<input type="checkbox"/>',
-			'title' => _x( 'Title', 'column name' ),
-			'date'  => __( 'Date' ),
-		];
-
-		$this->assertTrue( is_array( set_custom_columns( $post_columns ) ) );
-		$this->assertArrayHasKey( 'tour_id', set_custom_columns( $post_columns ) );
-		$this->assertArrayHasKey( 'tour_year', set_custom_columns( $post_columns ) );
-		$this->assertArrayHasKey( 'menu_order', set_custom_columns( $post_columns ) );
+		$this->assertSame( $expected, _set_custom_columns() );
 	}
 }
 
