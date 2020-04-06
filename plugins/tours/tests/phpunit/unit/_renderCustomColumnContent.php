6b092c604e342591842c21f2181be39302504f57
Robert Gadon <rgadon107@gmail.com>
2020-04-06 17:14:47 -0500

    Update unit test file following code review.
    - Change class name to reflect underscore prefixing function name. 
    - Invoke method 'setUpBeforeClass' from parent class TestCase to prepare test environment before each test.
    - Rename each of the 3 test methods and descriptions to reflect what the test is doing.
    - Remove Mockery and mock of WP_Post object. 
    - Set unique $tour_id for each test. 
    - Set unique $tour_year and $menu_order in 2nd and 3rd tests. 
    - Use PHPUnit's expectOutputString() method to buffer and output expected value of string to be evaluated.

--- plugins/tours/tests/phpunit/unit/_renderCustomColumnContent.php
+++ plugins/tours/tests/phpunit/unit/_renderCustomColumnContent.php
@@ -22,28 +22,74 @@ use function spiralWebDb\CornerstoneTours\_render_custom_column_content;
  * @group   tours
  * @group   admin
  */
-class Tests_RenderCustomColumnContent extends Test_Case {
+class Tests__RenderCustomColumnContent extends Test_Case {
 
 	/**
 	 * Prepares the test environment before each test.
 	 */
-	protected function setUpBeforeClass() {
-		parent::setUp();
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
 
 		require_once TOURS_ROOT_DIR . '/src/admin/wp-list-table.php';
 	}
 
 	/**
-	 *  Test _render_custom_column_content() should echo 'tour_id' when evaluating $column_name.
+	 *  Test _render_custom_column_content() should echo 'tour_id' when $tour_id is given.
 	 */
-	public function test_should_echo_tour_id_when_evaluating_column_name() {
-		Monkey\Functions\expect( '_render_custom_column_content' )
-			->once()
-			->with( 'column_name', 'tour_id' )
-			->andReturn( 99 );
-		$expected = $this->post->ID;
+	public function test_should_echo_tour_id_when_column_name_is_tour_id() {
+		$tour_id = 99;
 
-		$this->assertSame( $expected, _render_custom_column_content( $column_name, $this->post->ID ) );
+		Monkey\Functions\expect( 'get_post_meta' )
+			->once()
+			->with( $tour_id, 'tour_year', true )
+			->never();
+		Monkey\Functions\expect( 'get_post_field' )
+			->once()
+			->with( 'menu_order', $tour_id )
+			->never();
+
+		$this->expectOutputString( $tour_id );
+		_render_custom_column_content( 'tour_id', $tour_id );
+	}
+
+	/*
+	 * Test _render_custom_column_content() should echo the 'tour_year' when $tour_year is given.
+	 */
+	public function test_should_echo_tour_year_when_tour_year_is_given() {
+		$tour_id   = 157;
+		$tour_year = 2018;
+
+		Monkey\Functions\expect( 'get_post_meta' )
+			->once()
+			->with( $tour_id, 'tour_year', true )
+			->andReturn( $tour_year );
+		Monkey\Functions\expect( 'get_post_field' )
+			->once()
+			->with( 'menu_order', $tour_id )
+			->never();
+
+		$this->expectOutputString( $tour_year );
+		_render_custom_column_content( 'tour_year', $tour_id );
+	}
+
+	/*
+	 * Test _render_custom_column_content() should echo the 'menu_order' when $menu_order is given.
+	 */
+	public function test_should_echo_menu_order_when_menu_order_is_given() {
+		$tour_id    = 211;
+		$menu_order = 5;
+
+		Monkey\Functions\expect( 'get_post_meta' )
+			->once()
+			->with( $tour_id, 'tour_year', true )
+			->never();
+		Monkey\Functions\expect( 'get_post_field' )
+			->once()
+			->with( 'menu_order', $tour_id )
+			->andReturn( $menu_order );
+
+		$this->expectOutputString( $menu_order );
+		_render_custom_column_content( 'menu_order', $tour_id );
 	}
 }
 
