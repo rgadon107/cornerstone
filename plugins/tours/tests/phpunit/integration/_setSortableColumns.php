f2adc8d5f50f40afc1ba7047fb95256608b48ebf
Robert Gadon <rgadon107@gmail.com>
2020-04-08 20:52:10 -0500

    Simplify integration test file.
    - Run one test and not two. 
    - Update test description and method name. 
    - Simplify assertions to a single assertion.
    - Add newline at end of file.

--- plugins/tours/tests/phpunit/integration/_setSortableColumns.php
+++ plugins/tours/tests/phpunit/integration/_setSortableColumns.php
@@ -22,48 +22,18 @@ use function spiralWebDb\CornerstoneTours\_set_sortable_columns;
  * @group   admin
  */
 class Tests__SetSortableColumns extends Test_Case {
-	/**
-	 * Test _set_sortable_columns() should return an array when registered to filter.
-	 */
-	public function test_should_return_an_array_when_registered_to_filter() {
-		$sortable_columns = [
-			'title'    => 'title',
-			'parent'   => 'parent',
-			'comments' => 'comment_count',
-			'date'     => [ 'date', true ],
-		];
-
-		$this->assertContains( is_array( $sortable_columns ), _set_sortable_columns( $sortable_columns ) );
-		$this->assertArrayHasKey( 'title', $sortable_columns );
-		$this->assertArrayHasKey( 'parent', $sortable_columns );
-		$this->assertArrayHasKey( 'comments', $sortable_columns );
-		$this->assertArrayHasKey( 'date', $sortable_columns );
-	}
 
 	/**
-	 * Test _set_sortable_columns() should return a filtered array when registered to filter.
+	 * Test _set_sortable_columns() should return filtered array of sortable columns.
 	 */
-	public function test_should_return_filtered_array_when_registered_to_filter() {
-		$sortable_columns = [
-			'title'    => 'title',
-			'parent'   => 'parent',
-			'comments' => 'comment_count',
-			'date'     => [ 'date', true ],
-		];
-
+	public function test_should_return_filtered_array_of_sortable_columns() {
 		$expected = [
 			'tour_id'    => 'Tour ID',
 			'tour_year'  => 'Tour Year',
 			'menu_order' => 'Order Number',
 		];
 
-		$this->assertSame( $expected, _set_sortable_columns( $sortable_columns ) );
-		$this->assertArrayHasKey( 'tour_id', _set_sortable_columns( $sortable_columns ) );
-		$this->assertArrayHasKey( 'tour_year', _set_sortable_columns( $sortable_columns ) );
-		$this->assertArrayHasKey( 'menu_order', _set_sortable_columns( $sortable_columns ) );
-
-
-
+		$this->assertSame( $expected, _set_sortable_columns() );
 	}
-
 }
+
