1204e57b652b02023837871fd0a015bcba2c8ce1
Robert Gadon <rgadon107@gmail.com>
2020-04-08 23:00:07 -0500

    [WIP]Unit test file initial commit with first unit test.

---
+++ plugins/tours/tests/phpunit/unit/template/renderTourTitleText.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Tests for render_post_title_text().
+ *
+ * @package     spiralWebDb\CornerstoneTours\Tests\Unit
+ * @since       1.0.0
+ * @author      Robert Gadon <rgadon107>
+ * @link        https://github.com/rgadon107/cornerstone
+ * @license     GNU-2.0+
+ */
+
+namespace spiralWebDb\CornerstoneTours\Tests\Unit;
+
+use Brain\Monkey;
+use spiralWebDb\Cornerstone\Tests\Unit\Test_Case;
+use function spiralWebDb\CornerstoneTours\Template\render_post_title_text;
+
+/**
+ * Class Tests_RenderPostTitleText
+ *
+ * @package spiralWebDb\CornerstoneTours\Tests\Unit
+ * @group   tours
+ * @group   template
+ */
+class Tests_RenderPostTitleText extends Test_Case {
+
+	/**
+	 * Prepare the test environment before each test.
+	 */
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+
+		require_once TOURS_ROOT_DIR . '/src/template/single-tours.php';
+		require_once get_template_directory() . '/lib/framework.php';
+
+	}
+
+	/*
+	 * Test render_post_title_text() echoes the title when the filter event fires.
+	 */
+	public function test_title_is_echoed_when_filter_event_fires() {
+		$tour_id    = 71;
+		$menu_order = 15;
+		Monkey\Functions\expect( 'get_post_meta' )
+			->once()
+			->with( 'tour_id', 'tour_year', true )
+			->andReturn( '2011' );
+		Monkey\Functions\expect( 'get_the_title' )
+			->once()
+			->with( 'tour_id' )
+			->andReturn( 'I Make All Things New' );
+		Monkey\Functions\expect( 'genesis' )
+			->once()
+			->with()
+			->never();
+
+		$this->expectOutputString( $tour_id );
+		render_post_title_text( $tour_id );
+	}
+}
