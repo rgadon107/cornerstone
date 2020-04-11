1a26d36a74a9b8bdff5867adb04fa6898be02727
Robert Gadon <rgadon107@gmail.com>
2020-04-11 15:40:26 -0500

    [WIP]Attempt to load configuration into memory fails.
    - PHPUnit returns Mockery Exception.
    - Message:  Method _get_plugin_directory(<No Arguments>) from Mockery_0 should be called
     exactly 1 times but called 0 times.

--- plugins/tours/tests/phpunit/unit/loadConfigurations.php
+++ plugins/tours/tests/phpunit/unit/loadConfigurations.php
@@ -13,7 +13,7 @@ namespace spiralWebDb\CornerstoneTours\Tests\Unit;
 
 use Brain\Monkey;
 use spiralWebDb\Cornerstone\Tests\Unit\Test_Case;
-use function spiralWebDb\Metadata\load_configurations;
+use function spiralWebDb\CornerstoneTours\load_configurations;
 
 /**
  * Class Tests_LoadConfigurations
@@ -29,6 +29,24 @@ class Tests_LoadConfigurations extends Test_Case {
 	protected function setUp() {
 		parent::setUp();
 
+		Monkey\Functions\expect( '_get_plugin_directory' )
+			->once()
+			->with()
+			->andReturn( TOURS_ROOT_DIR );
+
 		require_once TOURS_ROOT_DIR . '/src/config-loader.php';
 	}
+
+	/*
+	 * Test load_configurations() should load the meta-box configuration from the plugin 'config' directory.
+	 */
+	public function test_should_load_meta_box_config_from_plugin_config_directory() {
+		Monkey\Functions\expect( 'spiralWebDB\Metadata\autoload_configurations' )
+			->once()
+			->with( [ TOURS_ROOT_DIR . '/config/tours.php' ] )
+			->andReturn();
+
+//		$this->assertArrayHasKey( 'meta_box.tours', load_configurations() );
+	}
 }
+
