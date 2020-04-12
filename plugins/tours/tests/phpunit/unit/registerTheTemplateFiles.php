1d1b0f9c1b0bda4e8e4ed517c999414eb2333ba6
Robert Gadon <rgadon107@gmail.com>
2020-04-12 16:23:30 -0500

    Update unit test file and method.
    - Add 'use function' declaration for `_get_plugin_directory`. 
    - Add comments prior to Brain\Monkey mock.
    - Add `assertArraySubset()` methods.

--- plugins/tours/tests/phpunit/unit/registerTheTemplateFiles.php
+++ plugins/tours/tests/phpunit/unit/registerTheTemplateFiles.php
@@ -13,6 +13,7 @@ namespace spiralWebDb\CornerstoneTours\Tests\Unit;
 
 use Brain\Monkey;
 use spiralWebDb\Cornerstone\Tests\Unit\Test_Case;
+use function spiralWebDb\CornerstoneTours\_get_plugin_directory;
 use function spiralWebDb\CornerstoneTours\register_the_template_files;
 
 /**
@@ -38,13 +39,16 @@ class Tests_RegisterTheTemplateFiles extends Test_Case {
 	public function test_should_return_template_files_given_valid_path_to_plugin_config() {
 		$templates = [];
 		// _get_plugin_directory() is called 3 times when register_the_template_files() is invoked.
+		// _get_plugin_directory() is called an additional time when the assertArraySubset() method is run.
 		Monkey\Functions\expect( 'spiralWebDb\CornerstoneTours\_get_plugin_directory' )
-			->times( 6 )
+			->times( 14 )
 			->with()
 			->andReturn( TOURS_ROOT_DIR );
 
 		$this->assertArrayHasKey( 'single', register_the_template_files( (array) $templates ) );
+		$this->assertArraySubset( [ 'single' => [ 'tours' => _get_plugin_directory() . '/src/template/single-tours.php' ] ], register_the_template_files( (array) $templates ) );
 		$this->assertArrayHasKey( 'post_type_archive', register_the_template_files( (array) $templates ) );
+		$this->assertArraySubset( [ 'post_type_archive' => [ 'tours' => _get_plugin_directory() . '/src/template/archive-tours.php' ] ], register_the_template_files( (array) $templates ) );
 	}
 }
 
