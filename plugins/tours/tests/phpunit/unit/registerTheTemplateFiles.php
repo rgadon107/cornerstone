098479060c118f3c653d6ade1faa0d3d17a8fbf2
Robert Gadon <rgadon107@gmail.com>
2020-04-12 15:59:12 -0500

    Add unit test file.
    - Add 'test_should_return_template_files_given_valid_path_to_plugin_config'.

---
+++ plugins/tours/tests/phpunit/unit/registerTheTemplateFiles.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Tests for register_the_template_files().
+ *
+ * @package     spiralWebDb\CornerstoneTours\Tests\Unit
+ * @since       1.0.0
+ * @author      Robert Gadon <rgadon107>
+ * @link        https://github.com/rgadon107/cornerstone
+ * @license     GNU-2.0+
+ */
+
+namespace spiralWebDb\CornerstoneTours\Tests\Unit;
+
+use Brain\Monkey;
+use spiralWebDb\Cornerstone\Tests\Unit\Test_Case;
+use function spiralWebDb\CornerstoneTours\register_the_template_files;
+
+/**
+ * Class Tests_RegisterTheTemplateFiles
+ *
+ * @package spiralWebDb\CornerstoneTours\Tests\Unit
+ * @group   tours
+ */
+class Tests_RegisterTheTemplateFiles extends Test_Case {
+
+	/**
+	 * Prepares the test environment before each test.
+	 */
+	protected function setUp() {
+		parent::setUp();
+
+		require_once TOURS_ROOT_DIR . '/src/config-loader.php';
+	}
+
+	/*
+	 * Test register_the_template_files() should return template files when given valid path to plugin config.
+	 */
+	public function test_should_return_template_files_given_valid_path_to_plugin_config() {
+		$templates = [];
+		// _get_plugin_directory() is called 3 times when register_the_template_files() is invoked.
+		Monkey\Functions\expect( 'spiralWebDb\CornerstoneTours\_get_plugin_directory' )
+			->times( 6 )
+			->with()
+			->andReturn( TOURS_ROOT_DIR );
+
+		$this->assertArrayHasKey( 'single', register_the_template_files( (array) $templates ) );
+		$this->assertArrayHasKey( 'post_type_archive', register_the_template_files( (array) $templates ) );
+	}
+}
+
