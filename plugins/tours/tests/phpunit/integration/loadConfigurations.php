264d7e53189480c3696f52fe3ee7b3d3345804ed
Robert Gadon <rgadon107@gmail.com>
2020-04-11 23:18:44 -0500

    Updated integration test 'test_should_load_plugin_meta_box_config'.

--- plugins/tours/tests/phpunit/integration/loadConfigurations.php
+++ plugins/tours/tests/phpunit/integration/loadConfigurations.php
@@ -12,7 +12,8 @@
 namespace spiralWebDb\CornerstoneTours\Tests\Integration;
 
 use spiralWebDb\Cornerstone\Tests\Integration\Test_Case;
-use function spiralWebDb\Metadata\load_configurations;
+use function KnowTheCode\ConfigStore\getConfig;
+use function spiralWebDb\CornerstoneTours\load_configurations;
 
 /**
  * Class Tests_LoadConfigurations
@@ -23,4 +24,19 @@ use function spiralWebDb\Metadata\load_configurations;
  */
 class Tests_LoadConfigurations extends Test_Case {
 
+	/*
+	 * Test load_configurations() should load the 'tours' plugin meta-box configuration.
+	 */
+	public function test_should_load_plugin_meta_box_config() {
+		load_configurations();
+
+		$actual_config = getConfig( 'meta_box.tours' );
+		$this->assertArrayHasKey( 'add_meta_box', $actual_config );
+		$this->assertArrayHasKey( 'custom_fields', $actual_config );
+		$this->assertArrayHasKey( 'view', $actual_config );
+
+		// Clean up.
+		self::remove_from_store( 'meta_box.tours' );
+	}
 }
+
