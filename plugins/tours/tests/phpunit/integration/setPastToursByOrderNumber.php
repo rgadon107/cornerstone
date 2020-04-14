c7de73a1e7a9964e337e26c61b09a1785694924c
Robert Gadon <rgadon107@gmail.com>
2020-04-13 20:00:58 -0500

    Add integration test file.
    - Add test 'test_callback_is_registered_to_action_hook_when_event_fires'.

---
+++ plugins/tours/tests/phpunit/integration/setPastToursByOrderNumber.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Tests for set_past_tours_by_order_number().
+ *
+ * @package     spiralWebDb\CornerstoneTours\Tests\Integration
+ * @since       1.0.0
+ * @author      Robert Gadon <rgadon107>
+ * @link        https://github.com/rgadon107/cornerstone
+ * @license     GNU-2.0+
+ */
+
+namespace spiralWebDb\CornerstoneTours\Tests\Integration;
+
+use spiralWebDb\Cornerstone\Tests\Integration\Test_Case;
+use function has_action;
+use function spiralWebDb\CornerstoneTours\set_past_tours_by_order_number;
+
+/**
+ * Class Tests_SetPastToursByOrderNumber
+ *
+ * @package spiralWebDb\CornerstoneTours\Tests\Integration
+ * @group   tours
+ * @group   admin
+ */
+class Tests_SetPastToursByOrderNumber extends Test_Case {
+
+	/*
+	 * Test set_past_tours_by_order_number() is registered to do_action_ref_array( 'pre_get_posts' ) when event fires.
+	 */
+	public function test_callback_is_registered_to_action_hook_when_event_fires() {
+		$this->assertTrue( has_action( 'pre_get_posts' ) );
+		$this->assertEquals( 10, has_action( 'pre_get_posts', 'spiralWebDb\CornerstoneTours\set_past_tours_by_order_number' ) );
+	}
+
+}
+
