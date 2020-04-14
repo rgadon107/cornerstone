790c3a74ceb2e1a9f9a407d5df5e6cf9a11773d1
Robert Gadon <rgadon107@gmail.com>
2020-04-14 00:25:53 -0500

    [WIP]Update unit test file & first unit test.
    - Add properties to test class. 
    - Mock instance of 'WP_Query' with Mockery.
    - Assign $query_vars array of mocked object to $expected. 
    - Invoke function under test (returns null value). 
    - Unsure what assertion to test, since mocked object is not an array, traversable, or string.

--- plugins/tours/tests/phpunit/unit/setPastToursByOrderNumber.php
+++ plugins/tours/tests/phpunit/unit/setPastToursByOrderNumber.php
@@ -25,45 +25,64 @@ use function spiralWebDb\CornerstoneTours\set_past_tours_by_order_number;
 class Tests_SetPastToursByOrderNumber extends Test_Case {
 
 	/**
+	 * Instance of WP_Query for each test.
+	 *
+	 * @var Mockery
+	 */
+	protected $query;
+
+	/**
+	 * Query vars, after parsing
+	 *
+	 * @var array
+	 */
+	public $query_vars = array();
+
+	/**
 	 * Prepares the test environment before each test.
 	 */
 	protected function setUp() {
 		parent::setUp();
 
 		require_once TOURS_ROOT_DIR . '/src/plugin.php';
+
+		$this->query             = m::mock( 'WP_Query' );
+		$this->query->query_vars = [
+			'order'   => 'ASC',
+			'orderby' => 'menu_order'
+		];
 	}
 
 	/*
      * Test set_past_tours_by_order_number() should return unmodified query_vars from WP_Query when post_type_archive is false.
      */
 	public function test_should_return_unmodified_query_vars_when_post_type_archive_is_false() {
-		$expected = $this->query = (object) [
-			'query_vars' => [
-				'order' => "ASC",
-			]
-		];
+		$expected = $this->query->query_vars;
+
 		Monkey\Functions\expect( 'is_post_type_archive' )
 			->once()
 			->with()
 			->andReturn( false );
 
-		$this->assertContains( $expected, set_past_tours_by_order_number( $this->query ) );
+		set_past_tours_by_order_number( $this->query );
+
+		$this->assertSame( $expected, $this->query->query_vars );
 	}
 
 	/*
 	 * Test set_past_tours_by_order_number() should modify query_var default when post_type_archive is true.
 	 */
-	public function test_should_modify_query_var_default_when_post_type_archive_is_true() {
-		Monkey\Functions\expect( 'is_post_type_archive' )
-			->once()
-			->with()
-			->andReturn( true );
-		Monkey\Functions\expect( 'set')
-			->once()
-			->with( 'order', 'DESC')
-			->andReturnValues( 'DESC');
-
-		set_past_tours_by_order_number( $this->query );
-	}
+//	public function test_should_modify_query_var_default_when_post_type_archive_is_true() {
+//		Monkey\Functions\expect( 'is_post_type_archive' )
+//			->once()
+//			->with()
+//			->andReturn( true );
+//		Monkey\Functions\expect( 'set' )
+//			->once()
+//			->with( 'order', 'DESC' )
+//			->andReturnValues( 'DESC' );
+//
+//		set_past_tours_by_order_number( $this->query );
+//	}
 }
 
