131eca2b55f4af8f0c61d1682d36112cd1682a93
Robert Gadon <rgadon107@gmail.com>
2020-04-14 16:26:55 -0500

    Update unit test file.
    - Assign $query_vars to an array of values in setUp() method. 
    - Change parameter of function under test to var mocking WP_Query object ($query). 
    - Second test: Use Mockery and WP_Query::set() to mock the `set` method in the function under test. 
    - Note: PHPUnit returns the following error message: "Error: Call to a member function shouldReceive() on null" L#80.

--- plugins/tours/tests/phpunit/unit/setPastToursByOrderNumber.php
+++ plugins/tours/tests/phpunit/unit/setPastToursByOrderNumber.php
@@ -46,8 +46,8 @@ class Tests_SetPastToursByOrderNumber extends Test_Case {
 
 		require_once TOURS_ROOT_DIR . '/src/plugin.php';
 
-		$this->query             = m::mock( 'WP_Query' );
-		$this->query->query_vars = [
+		$query             = m::mock( 'WP_Query' );
+		$query->query_vars = [
 			'order'   => 'ASC',
 			'orderby' => 'menu_order'
 		];
@@ -57,32 +57,36 @@ class Tests_SetPastToursByOrderNumber extends Test_Case {
      * Test set_past_tours_by_order_number() should return unmodified query_vars from WP_Query when post_type_archive is false.
      */
 	public function test_should_return_unmodified_query_vars_when_post_type_archive_is_false() {
-		$expected = $this->query->query_vars;
+		$expected = $query->query_vars;
 
 		Monkey\Functions\expect( 'is_post_type_archive' )
 			->once()
 			->with()
 			->andReturn( false );
 
-		set_past_tours_by_order_number( $this->query );
+		set_past_tours_by_order_number( $query );
 
-		$this->assertSame( $expected, $this->query->query_vars );
+		$this->assertSame( $expected, $query->query_vars );
 	}
 
 	/*
 	 * Test set_past_tours_by_order_number() should modify query_var default when post_type_archive is true.
 	 */
-//	public function test_should_modify_query_var_default_when_post_type_archive_is_true() {
-//		Monkey\Functions\expect( 'is_post_type_archive' )
-//			->once()
-//			->with()
-//			->andReturn( true );
-//		Monkey\Functions\expect( 'set' )
-//			->once()
-//			->with( 'order', 'DESC' )
-//			->andReturnValues( 'DESC' );
-//
-//		set_past_tours_by_order_number( $this->query );
-//	}
+	public function test_should_modify_query_var_default_when_post_type_archive_is_true() {
+		Monkey\Functions\expect( 'is_post_type_archive' )
+			->once()
+			->with()
+			->andReturn( true );
+		$query->shouldReceive( 'set')
+			->with( 'orderby', 'menu_order')
+			->once()
+			->andReturnNull();
+		$query->shouldReceive( 'set')
+		      ->with( 'order', 'DESC')
+		      ->once()
+			->andReturnNull();
+
+		set_past_tours_by_order_number( $query );
+	}
 }
 
