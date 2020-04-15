4dba65a982bbff7603b0eeddf35ace0a181e33d4
Robert Gadon <rgadon107@gmail.com>
2020-04-15 16:03:46 -0500

    [WIP]Adding third integration test.
    - Returns error: "spiralWebDb\CornerstoneTours\Tests\Integration\Tests_SetPastToursByOrderNumber::test_should_return_modified_query_when_post_type_is_tours
    PHPUnit_Framework_Exception: Argument #2 (No Value) of PHPUnit_Framework_Assert::assertContains() must be a array, traversable or string"
    - Most like

--- plugins/tours/tests/phpunit/integration/setPastToursByOrderNumber.php
+++ plugins/tours/tests/phpunit/integration/setPastToursByOrderNumber.php
@@ -25,6 +25,20 @@ use function spiralWebDb\CornerstoneTours\set_past_tours_by_order_number;
  */
 class Tests_SetPastToursByOrderNumber extends Test_Case {
 
+	/**
+	 * Instance of WP_Query for each test.
+	 *
+	 * @var object WP_Query
+	 */
+	protected $query;
+
+	/**
+	 * Query vars, after parsing
+	 *
+	 * @var array
+	 */
+	public $query_vars = array();
+
 	/*
 	 * Test set_past_tours_by_order_number() is registered to do_action_ref_array( 'pre_get_posts' ) when event fires.
 	 */
@@ -38,7 +52,7 @@ class Tests_SetPastToursByOrderNumber extends Test_Case {
      */
 	public function test_should_return_unmodified_query_when_post_type_is_post() {
 		// Create and get a post object for 'post' post_type with WordPress' factory method.
-		$post  = self::factory()->post->create_and_get();
+		$post = self::factory()->post->create_and_get();
 		// Instantiate a new WP_Query object.
 		$query = new WP_Query();
 
@@ -46,5 +60,23 @@ class Tests_SetPastToursByOrderNumber extends Test_Case {
 		$this->assertSame( $query, set_past_tours_by_order_number( $query ) );
 	}
 
+	/*
+ * Test set_past_tours_by_order_number() should return modified query object from WP_Query when post_type_is 'tours'.
+ */
+	public function test_should_return_modified_query_when_post_type_is_tours() {
+		// Create and get a post object for 'post' post_type with WordPress' factory method.
+		$post = self::factory()->post->create_and_get( [ 'post_type' => 'tours' ] );
+		// Instantiate a new WP_Query object.
+		$query = new WP_Query();
+		$query->query_vars( [ 'orderby' => 'menu_order', 'order' => 'DESC' ] );
+		
+		set_past_tours_by_order_number( $query );
+
+		// When post_type is 'tours', $query object should contain updated query_vars.
+		$this->assertContains( $query->query_vars( [ 'orderby' ] ), $query );
+		$this->assertContains( $query->query_vars( [ 'order' ] ), $query );
+	}
 }
+
+
 
