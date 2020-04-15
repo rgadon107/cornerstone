1b9c61a4532672c18b14be1f1bdde6f7299658dc
Robert Gadon <rgadon107@gmail.com>
2020-04-15 15:42:58 -0500

    Add 2nd integration test and use WP_Query to instantiate new object.

--- plugins/tours/tests/phpunit/integration/setPastToursByOrderNumber.php
+++ plugins/tours/tests/phpunit/integration/setPastToursByOrderNumber.php
@@ -13,6 +13,7 @@ namespace spiralWebDb\CornerstoneTours\Tests\Integration;
 
 use spiralWebDb\Cornerstone\Tests\Integration\Test_Case;
 use function has_action;
+use WP_Query;
 use function spiralWebDb\CornerstoneTours\set_past_tours_by_order_number;
 
 /**
@@ -30,6 +31,19 @@ class Tests_SetPastToursByOrderNumber extends Test_Case {
 	public function test_callback_is_registered_to_action_hook_when_event_fires() {
 		$this->assertTrue( has_action( 'pre_get_posts' ) );
 		$this->assertEquals( 10, has_action( 'pre_get_posts', 'spiralWebDb\CornerstoneTours\set_past_tours_by_order_number' ) );
+	}
+
+	/*
+     * Test set_past_tours_by_order_number() should return unmodified query object from WP_Query when post_type_is 'post'.
+     */
+	public function test_should_return_unmodified_query_when_post_type_is_post() {
+		// Create and get a post object for 'post' post_type with WordPress' factory method.
+		$post  = self::factory()->post->create_and_get();
+		// Instantiate a new WP_Query object.
+		$query = new WP_Query();
+
+		// When post_type is 'post', there should be no change to the $query object.
+		$this->assertSame( $query, set_past_tours_by_order_number( $query ) );
 	}
 
 }
