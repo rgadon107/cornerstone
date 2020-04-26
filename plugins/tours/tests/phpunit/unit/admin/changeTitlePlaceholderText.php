ffeb13682ae2beedaaab75b58454948d1b7f745f
Robert Gadon <rgadon107@gmail.com>
2020-04-25 21:06:43 -0500

    Update unit test file.
    - Add @covers annotation (from PHPUnit) to test class docblock. 
    - Move mock of WP_Post from setUp() to test method. 
    - Add @dataProvider annotation to test method. 
    -Add $post_type & $view parameters to test method. 
    - Rename test method 
    'test_should_render_placeholder_text_depending_on_post_type'.
    - Add output buffer to test method, invoke function under test inside 
    buffer, and clean buffer to return $actual_html.
    - Change from 'assertSame' to 'assertEquals' to just compare the 
    _values_ of $expected to $actual (not data types). 
    - Add dataProvider method 'addTestData'. 
    - Add newline at end of file.

--- plugins/tours/tests/phpunit/unit/admin/changeTitlePlaceholderText.php
+++ plugins/tours/tests/phpunit/unit/admin/changeTitlePlaceholderText.php
@@ -2,24 +2,23 @@
 /**
  * Tests for change_title_placeholder_text().
  *
- * @package     spiralWebDb\CornerstoneTours\Tests\Unit
  * @since       1.0.0
  * @author      Robert Gadon <rgadon107>
+ * @package     spiralWebDb\CornerstoneTours\Tests\Unit
  * @link        https://github.com/rgadon107/cornerstone
  * @license     GNU-2.0+
  */
 
 namespace spiralWebDb\CornerstoneTours\Tests\Unit;
 
-use Mockery as m;
+use Mockery;
 use Brain\Monkey\Functions;
 use spiralWebDb\Cornerstone\Tests\Unit\Test_Case;
 use function spiralWebDb\CornerstoneTours\change_title_placeholder_text;
 
 /**
- * Class Tests_ChangeTitlePlaceholderText
+ * @covers ::spiralWebDb\CornerstoneTours\change_title_placeholder_text
  *
- * @package spiralWebDb\CornerstoneTours\Tests\Unit
  * @group   tours
  * @group   admin
  */
@@ -39,34 +38,36 @@ class Tests_ChangeTitlePlaceholderText extends Test_Case {
 		parent::setUp();
 
 		require_once TOURS_ROOT_DIR . '/src/admin/edit-form-advanced.php';
-
-		$this->post          = m::mock( 'post' );
 	}
 
 	/**
-	 * Test change_title_placeholder_text() should return original text when the post type is not 'tours'.
+	 * @dataProvider addTestData
 	 */
-	public function test_should_return_given_text_when_post_type_is_not_tours() {
-		$text = 'Add title.';
+	public function test_should_render_placeholder_text_depending_on_post_type( $post_type, $view ) {
+		$post = Mockery::mock( 'WP_Post' );
 		Functions\expect( 'get_post_type' )
 			->once()
-			->with( 'post' )
-			->andReturn( 'post' );
+			->with( $post )
+			->andReturn( $post_type );
 
-		$this->assertSame( $text, change_title_placeholder_text( $text, $this->post ) );
+		ob_start();
+		change_title_placeholder_text( $text, $post );
+		$actual_html = ob_get_clean();
+
+		$this->assertEquals( $expected, $actual_html );
 	}
 
-	/**
-	 * Test change_title_placeholder_text() should return modified text when post type is 'tours'.
-	 */
-	public function test_should_return_modified_text_when_post_type_is_tours() {
-		Functions\expect( 'get_post_type' )
-			->once()
-			->with( 'post' )
-			->andReturn( 'tours' );
-		$text     = 'Add title.';
-		$expected = '<em>Theme of this Cornerstone tour.</em>';
-
-		$this->assertSame( $expected, change_title_placeholder_text( $text, $this->post ) );
+	public function addTestData() {
+		return [
+			'test_should_contain_default_placeholder_text_when_post_type_is_post' => [
+				'post_type' => 'post',
+				'expected'  => 'Add title.',
+			],
+			'test_should_contain_custom_placeholder_text_when_post_type_is_tours' => [
+				'post_type' => 'tours',
+				'expected'  => '<em>Theme of this Cornerstone tour.</em>',
+			],
+		];
 	}
 }
+
