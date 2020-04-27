59fea974fb332846d848af542662d2ae6290c60c
Robert Gadon <rgadon107@gmail.com>
2020-04-27 01:14:45 -0500

    Update integration test file.
    - Remove use declaration for function under test. [ Fire the filter 
    itself, which is located in the parent theme. ]
    - Add @covers annotation to test class.
    - Add @dataProvider annotation to test method.
    - Refactor the test method to use a dataProvider. 
    - Rename test method, & pass it one parameter. 
    - Change expected parameter in test assertion. 
    - Add dataProvider method 'addTestData()'.

--- plugins/tours/tests/phpunit/integration/template/renderTourTitleText.php
+++ plugins/tours/tests/phpunit/integration/template/renderTourTitleText.php
@@ -2,9 +2,9 @@
 /**
  * Tests for render_post_title_text().
  *
- * @package     spiralWebDb\CornerstoneTours\Tests\Integration
  * @since       1.0.0
  * @author      Robert Gadon <rgadon107>
+ * @package     spiralWebDb\CornerstoneTours\Tests\Integration
  * @link        https://github.com/rgadon107/cornerstone
  * @license     GNU-2.0+
  */
@@ -12,32 +12,34 @@
 namespace spiralWebDb\CornerstoneTours\Tests\Integration;
 
 use spiralWebDb\Cornerstone\Tests\Integration\Test_Case;
-use function spiralWebDb\CornerstoneTours\Template\render_post_title_text;
 
 /**
- * Class Tests_RenderPostTitleText
+ * @covers ::\spiralWebDb\CornerstoneTours\Template\render_post_title_text
  *
- * @package spiralWebDb\CornerstoneTours\Tests\Integration
  * @group   tours
  * @group   template
  */
 class Tests_RenderPostTitleText extends Test_Case {
-	
+
 	/**
-	 * Test render_post_title_text() echoes the post title when the filter event fires.
+	 * @dataProvider addTestData
 	 */
-	public function test_title_is_echoed_when_filter_event_fires() {
-		// Create and get the post object for the 'tours' post_type using WordPress' factory method.
-		$post     = self::factory()->post->create_and_get(
-			[
-				'post_type'  => 'tours',
-				'post_title' => 'I Make All Things New',
-			]
-		);
-		$expected = $post->post_title;
+	public function echo_post_title_when_filter_event_fires( $post_data ) {
+		$post = $this->factory->post->create_and_get( $post_data );
 		$this->go_to( '?tours=i-make-all-things-new' );
 
-		$this->assertSame( $expected, apply_filters( 'genesis_post_title_text', get_the_title() ) );
+		$this->assertSame( $post_data['post_title'], apply_filters( 'genesis_post_title_text', get_the_title() ) );
+	}
+
+	public function addTestData() {
+		return [
+			'past tour title' => [
+				'post_data' => [
+					'post_type'  => 'tours',
+					'post_title' => 'I Make All Things New',
+				]
+			]
+		];
 	}
 }
 
