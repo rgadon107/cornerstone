81f28f41acbe2e2505d7085203a4aa18a9cf09d7
Robert Gadon <rgadon107@gmail.com>
2020-04-27 23:50:47 -0500

    Update integration test file.
    - Update file header docblock. 
    - Remove 'use' declaration for 'has_filter'. Not needed. 
    - Add @covers annotaton to test class docblock. 
    - Remove test assertion for filter (alone). 
    - Update test method docblock with @dataProvider annotation, and type 
    casts for test method params.
    - Add test method params to test method. 
    - Change test assertions in test method. 
    - Add dataProvider method 'addTestData'.

--- plugins/tours/tests/phpunit/integration/registerCustomConfigs.php
+++ plugins/tours/tests/phpunit/integration/registerCustomConfigs.php
@@ -2,9 +2,9 @@
 /**
  * Tests for register_custom_configs().
  *
- * @package     spiralWebDb\CornerstoneTours\Tests\Integration
  * @since       1.0.0
  * @author      Robert Gadon <rgadon107>
+ * @package     spiralWebDb\CornerstoneTours\Tests\Integration
  * @link        https://github.com/rgadon107/cornerstone
  * @license     GNU-2.0+
  */
@@ -12,89 +12,73 @@
 namespace spiralWebDb\CornerstoneTours\Tests\Integration;
 
 use spiralWebDb\Cornerstone\Tests\Integration\Test_Case;
-use function has_filter;
 use function spiralWebDb\CornerstoneTours\register_custom_configs;
 
 /**
- * Class Tests_RegisterCustomConfigs
+ * @covers ::\spiralWebDb\CornerstoneTours\register_custom_configs
  *
- * @package spiralWebDb\CornerstoneTours\Tests\Integration
  * @group   tours
- * @group   admin
  */
 class Tests_RegisterCustomConfigs extends Test_Case {
 
-	/*
+	/**
 	 * Test register_custom_configs() is registered to filter 'add_custom_post_type_runtime_config' when event fires.
 	 */
 	public function test_callback_is_registered_to_filter_hook_when_event_fires() {
-		$this->assertTrue( has_filter( 'add_custom_post_type_runtime_config' ) );
 		$this->assertEquals( 7, has_filter( 'add_custom_post_type_runtime_config', 'spiralWebDb\CornerstoneTours\register_custom_configs' ) );
 	}
 
-	/*
-     * Test register_custom_configs() should add a runtime post_type config to existing configs.
+	/**
+	 * @dataProvider addTestData
+	 * @param array $configurations
+	 * @param array $tours
 	 */
-	public function test_should_add_runtime_post_type_config_to_existing_configs() {
-		$configurations      = [];
-		$expected_subarray_1 = [
-			'tours' => [
-				'post_type' => 'tours'
-			]
-		];
-		$expected_subarray_2 = [
-			'tours' => [
-				'labels' => [
-					'singular_label'    => 'Past Tour',
-					'plural_label'      => 'Past Tours',
-					'in_sentance_label' => 'Tours', // The label used within a sentance.
-					'text_domain'       => 'cornerstone-tours',
+	public function test_should_add_runtime_post_type_config_to_existing_configs( array $configurations, array $tours) {
+
+		$this->assertArrayHasKey( 'tours', register_custom_configs( (array) $configurations ) );
+		$this->assertContains( $tours['post_type'], (array) $tours );
+		$this->assertContains( $tours['features'], (array) $tours );
+		$this->assertContains( $tours['args'], (array) $tours );
+	}
+
+	public function addTestData() {
+		return [
+			'post type config' => [
+				'configurations' => [],
+				'tours'          => [
+					'post_type' => 'tours',
+					'features'  => [
+						'base_post_type' => 'post',
+						'exclude'        => [
+							'excerpt',
+							'comments',
+							'trackbacks',
+	//			            'custom-fields',
+							'thumbnail', // also known as the 'featured image'.
+							'author',
+							'post-formats',
+							'genesis-seo',
+							'genesis-scripts',
+							'genesis-layouts',
+							'genesis-rel-author',
+						],
+						'additional'     => [
+							'page-attributes',
+						],
+					],
+					'args'      => [
+						'description'  => '', // For informational purposes only.
+						'label'        => '',
+						'labels'       => '', // automatically generate the labels.
+						'public'       => true,
+						'show_in_rest' => true,
+						'menu_icon'    => 'dashicons-admin-site',
+						'supports'     => '', // automatically generate the support features.
+						'has_archive'  => true,
+					],
 				]
 			]
 		];
-		$expected_subarray_3 = [
-			'tours' => [
-				'features' => [
-					'base_post_type' => 'post',
-					'exclude'        => [
-						'excerpt',
-						'comments',
-						'trackbacks',
-//			            'custom-fields',
-						'thumbnail', // also known as the 'featured image'.
-						'author',
-						'post-formats',
-						'genesis-seo',
-						'genesis-scripts',
-						'genesis-layouts',
-						'genesis-rel-author',
-					],
-					'additional'     => [
-						'page-attributes',
-					],
-				],
-			]
-		];
-		$expected_subarray_4 = [
-			'tours' => [
-				'args'      => [
-					'description'  => '', // For informational purposes only.
-					'label'        => '',
-					'labels'       => '', // automatically generate the labels.
-					'public'       => true,
-					'show_in_rest' => true,
-					'menu_icon'    => 'dashicons-admin-site',
-					'supports'     => '', // automatically generate the support features.
-					'has_archive'  => true,
-				],
-			]
-		];
-
-		$this->assertArrayHasKey( 'tours', register_custom_configs( (array) $configurations ) );
-		$this->assertArraySubset( $expected_subarray_1, register_custom_configs( (array) $configurations ) );
-		$this->assertArraySubset( $expected_subarray_2, register_custom_configs( (array) $configurations ) );
-		$this->assertArraySubset( $expected_subarray_3, register_custom_configs( (array) $configurations ) );
-		$this->assertArraySubset( $expected_subarray_4, register_custom_configs( (array) $configurations ) );
 	}
 }
 
