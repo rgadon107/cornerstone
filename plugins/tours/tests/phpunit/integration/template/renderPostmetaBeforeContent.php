202dab68b586cae1d1fc80652e99b1b75b646a19
Robert Gadon <rgadon107@gmail.com>
2020-05-01 12:47:46 -0500

    [WIP]Build out the integration test file.
    - Add use function declarations at head of file.
    - Add @dataProvider annotation to test method.
    - Add dataProvider params to test method.
    - Prefix test method with `test_` string to ensure that test assertions 
    run.
    - Test assertion fails. The actual value returned is falsey (empty 
    string).
    - Add dataProvider method 'addTestData'.

--- plugins/tours/tests/phpunit/integration/template/renderPostmetaBeforeContent.php
+++ plugins/tours/tests/phpunit/integration/template/renderPostmetaBeforeContent.php
@@ -12,7 +12,8 @@
 namespace spiralWebDb\CornerstoneTours\Template;
 
 use spiralWebDb\Cornerstone\Tests\Integration\Test_Case;
-use function spiralWebDb\CornerstoneTours\Template\render_post_title_text;
+use function spiralWebDb\CornerstoneTours\render_the_tour_regions;
+use function spiralWebDb\CornerstoneTours\render_tour_comments;
 
 /**
  * @covers ::\spiralWebDb\CornerstoneTours\Template\render_postmeta_before_content
@@ -22,6 +23,50 @@ use function spiralWebDb\CornerstoneTours\Template\render_post_title_text;
  */
 class Tests_RenderPostmetaBeforeContent extends Test_Case {
 
+	/**
+	 * @dataProvider addTestData
+	 */
+	public function test_should_render_postmeta_before_entry_content( $post_data, $post_meta, $expected_html ) {
+		$post    = $this->factory->post->create_and_get( $post_data );
+		$tour_id = $post->ID;
 
+		// Add post_meta so that it can be called during test assertion.
+		add_post_meta( $tour_id, 'tour_regions', $post_meta['tour_regions'] );
+		add_post_meta( $tour_id, 'tour_comments', $post_meta['tour_comments'] );
+
+		// Invoke postmeta functions from plugin.
+		render_the_tour_regions( $tour_id );
+		get_post_meta( $tour_id, 'tour_comments', true );
+		render_tour_comments( $tour_id );
+
+		ob_start();
+		do_action( 'genesis_before_entry_content' );
+		$this->assertEquals( $expected_html, ob_get_clean() );
+
+		// Database cleanup.
+		delete_post_meta( $tour_id, 'tour_regions' );
+		delete_post_meta( $tour_id, 'tour_comments' );
+	}
+
+	public function addTestData() {
+		return [
+			'data_set' => [
+				'post_data'     => [
+					'post_type' => 'tours',
+				],
+				'post_meta'     => [
+					'tour_regions'  => 'Mountain West/West Coast/Southwest',
+					'tour_comments' => 'Performed at the Walt Disney Concert Hall in Los Angeles, CA',
+				],
+				'expected_html' => <<<POSTMETA_VIEW
+<h3 class="tour-post-meta">
+    <p><em class="tour-region">Region: Mountain West/West Coast/Southwest</em></p>
+    <p class="tour-comments"><em>Note: Performed at Walt Disney Concert Hall in Los Angeles, CA</em></p>
+</h3>
+POSTMETA_VIEW
+				,
+			]
+		];
+	}
 }
- 
+
