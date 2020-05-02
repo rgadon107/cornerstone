52350db3aa9c463e5633de2fce9076769887b241
Robert Gadon <rgadon107@gmail.com>
2020-05-01 22:43:58 -0500

    Change conditional in view file to return early.

--- plugins/tours/src/views/tour-postmeta.php
+++ plugins/tours/src/views/tour-postmeta.php
@@ -7,9 +7,9 @@ namespace spiralWebDb\CornerstoneTours;
 ?>
 <h3 class="tour-post-meta">
     <p><em class="tour-region">Region: <?php render_the_tour_regions( $tour_id ); ?></em></p>
-	<?php if ( get_post_meta( $tour_id, 'tour_comments', true ) ):
-		?>
+	<?php if ( empty( get_post_meta( $tour_id, 'tour_comments', true ) ) ):
+		return;
+	else: ?>
         <p class="tour-comments"><em>Note: <?php render_tour_comments( $tour_id ); ?></em></p>
-	<?php else: return;
-	endif; ?>
+	<?php endif ?>
 </h3>
