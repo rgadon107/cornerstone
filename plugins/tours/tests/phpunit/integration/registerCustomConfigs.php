3e580589b2414993b1793d2f49a8ad4a3cada1a5
hellofromtonya <hellofromtonya@knowthecode.io>
2020-05-03 10:48:12 -0500

    Improves register_custom_configs integration tests.

--- plugins/tours/tests/phpunit/integration/registerCustomConfigs.php
+++ plugins/tours/tests/phpunit/integration/registerCustomConfigs.php
@@ -12,7 +12,7 @@
 namespace spiralWebDb\CornerstoneTours\Tests\Integration;
 
 use spiralWebDb\Cornerstone\Tests\Integration\Test_Case;
-use function spiralWebDb\CornerstoneTours\register_custom_configs;
+use function spiralWebDb\CornerstoneTours\_get_plugin_directory;
 
 /**
  * @covers ::\spiralWebDb\CornerstoneTours\register_custom_configs
@@ -21,63 +21,49 @@ use function spiralWebDb\CornerstoneTours\register_custom_configs;
  */
 class Tests_RegisterCustomConfigs extends Test_Case {
 
-	/**
-	 * Test register_custom_configs() is registered to filter 'add_custom_post_type_runtime_config' when event fires.
-	 */
 	public function test_callback_is_registered_to_filter_hook_when_event_fires() {
 		$this->assertEquals( 7, has_filter( 'add_custom_post_type_runtime_config', 'spiralWebDb\CornerstoneTours\register_custom_configs' ) );
 	}
 
 	/**
 	 * @dataProvider addTestData
-	 * @param array $configurations
-	 * @param array $tours
 	 */
-	public function test_should_add_runtime_post_type_config_to_existing_configs( array $configurations, array $tours) {
+	public function test_should_add_runtime_post_type_config_to_existing_configs( array $existing, array $expected ) {
+		$configs = apply_filters( 'add_custom_post_type_runtime_config', $existing );
 
-		$this->assertArrayHasKey( 'tours', register_custom_configs( (array) $configurations ) );
-		$this->assertContains( $tours['post_type'], (array) $tours );
-		$this->assertContains( $tours['features'], (array) $tours );
-		$this->assertContains( $tours['args'], (array) $tours );
+		$this->assertArrayHasKey( 'tours', $configs );
+		$this->assertSame( $expected, $configs['tours'] );
+
+		foreach ( $existing as $key => $config ) {
+			$this->assertArrayHasKey( $key, $configs );
+			$this->assertSame( $config, $configs[ $key ] );
+		}
 	}
 
 	public function addTestData() {
+		$tours = (array) require _get_plugin_directory() . '/config/post-type.php';
+
 		return [
-			'post type config' => [
-				'configurations' => [],
-				'tours'          => [
-					'post_type' => 'tours',
-					'features'  => [
-						'base_post_type' => 'post',
-						'exclude'        => [
-							'excerpt',
-							'comments',
-							'trackbacks',
-	//			            'custom-fields',
-							'thumbnail', // also known as the 'featured image'.
-							'author',
-							'post-formats',
-							'genesis-seo',
-							'genesis-scripts',
-							'genesis-layouts',
-							'genesis-rel-author',
+			'testShouldIncludToursConfig' => [
+				'existing_configs' => [],
+				'tours'            => $tours,
+			],
+
+			'testShouldMergeToursWithExistinConfigs' => [
+				'existing_configs' => [
+					'ipseum' => [
+						'post_type' => 'ipseum',
+						'labels'    => [
+							'singular_label' => 'Ipseum',
 						],
-						'additional'     => [
-							'page-attributes',
+						'features'  => [
+							'base_post_type' => 'post',
 						],
+						'args'      => [],
 					],
-					'args'      => [
-						'description'  => '', // For informational purposes only.
-						'label'        => '',
-						'labels'       => '', // automatically generate the labels.
-						'public'       => true,
-						'show_in_rest' => true,
-						'menu_icon'    => 'dashicons-admin-site',
-						'supports'     => '', // automatically generate the support features.
-						'has_archive'  => true,
-					],
-				]
-			]
+				],
+				'tours'            => $tours,
+			],
 		];
 	}
 }
