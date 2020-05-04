66478a17cb3eab8ced67fdd24e5b5f23fa5e6bc8
Robert Gadon <rgadon107@gmail.com>
2020-05-04 15:07:28 -0500

    Fix unit test array to string conversion error in TravisCI Build #524.
    - Change test method param from $meta to $tour_comments.
    - Change value returned by function mock to $tour_comments.
    - Remove conditional check on `expectOutputString()`. 
    - Pass $tour_comments to `expectOutputString()`.
    - Update data provider to remove 'post_meta' array. Change to 
    'tour_comments'.

--- plugins/tours/tests/phpunit/unit/renderTourComments.php
+++ plugins/tours/tests/phpunit/unit/renderTourComments.php
@@ -34,38 +34,29 @@ class Tests_RenderTourComments extends Test_Case {
 	/**
 	 * @dataProvider addTestData
 	 */
-	public function test_should_echo_meta_key_values_when_postmeta_exists( $tour_id, $meta ) {
+	public function test_should_echo_meta_key_values_when_postmeta_exists( $tour_id, $tour_comments ) {
 		Functions\expect( 'get_post_meta' )
 			->once()
 			->with( $tour_id, 'tour_comments', true )
-			->andReturn( $meta );
+			->andReturn( $tour_comments );
 
-		if ( ! is_null( $meta ) ) {
-			$this->expectOutputString( $meta );
-		}
-
+		$this->expectOutputString( $tour_comments );
 		render_tour_comments( $tour_id );
 	}
 
 	public function addTestData() {
 		return [
 			'empty postmeta key value'   => [
-				'tour_id'   => 143,
-				'post_meta' => [
-					'tour_comments' => '',
-				]
+				'tour_id'       => 143,
+				'tour_comments' => '',
 			],
 			'postmeta key value1 exists' => [
-				'tour_id'   => 359,
-				'post_meta' => [
-					'tour_comments' => 'Note: Performed in Zankel Hall at Carnegie Hall, New York, NY',
-				]
+				'tour_id'       => 359,
+				'tour_comments' => 'Note: Performed in Zankel Hall at Carnegie Hall, New York, NY',
 			],
 			'postmeta key value2 exists' => [
-				'tour_id'   => 502,
-				'post_meta' => [
-					'tour_comments' => 'Note: Performed at Alice Tully Hall, Lincoln Center, New York, NY',
-				]
+				'tour_id'       => 502,
+				'tour_comments' => 'Note: Performed at Alice Tully Hall, Lincoln Center, New York, NY',
 			]
 		];
 	}
