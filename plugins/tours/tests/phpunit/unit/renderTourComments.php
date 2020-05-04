3d392a9c7f9ee0f8d14593e9282f9ff15322d81f
Robert Gadon <rgadon107@gmail.com>
2020-05-04 14:11:49 -0500

    Update unit test file.
    - Change test method parameter $postmeta to $meta for consistency with 
    int. test. 
    - Change value returned by function mock to $meta.
    - Add conditional check for expected $meta. 
    - Restructure each data set to add 'post_meta' array.

--- plugins/tours/tests/phpunit/unit/renderTourComments.php
+++ plugins/tours/tests/phpunit/unit/renderTourComments.php
@@ -34,29 +34,38 @@ class Tests_RenderTourComments extends Test_Case {
 	/**
 	 * @dataProvider addTestData
 	 */
-	public function test_should_echo_meta_key_values_when_postmeta_exists( $tour_id, $postmeta ) {
+	public function test_should_echo_meta_key_values_when_postmeta_exists( $tour_id, $meta ) {
 		Functions\expect( 'get_post_meta' )
 			->once()
 			->with( $tour_id, 'tour_comments', true )
-			->andReturn( $postmeta );
+			->andReturn( $meta );
 
-		$this->expectOutputString( $tour_id );
+		if ( ! is_null( $meta ) ) {
+			$this->expectOutputString( $meta );
+		}
+
 		render_tour_comments( $tour_id );
 	}
 
 	public function addTestData() {
 		return [
 			'empty postmeta key value'   => [
-				'tour_id'       => 143,
-				'tour_comments' => '',
+				'tour_id'   => 143,
+				'post_meta' => [
+					'tour_comments' => '',
+				]
 			],
 			'postmeta key value1 exists' => [
-				'tour_id'       => 359,
-				'tour_comments' => 'Note: Performed in Zankel Hall at Carnegie Hall, New York, NY',
+				'tour_id'   => 359,
+				'post_meta' => [
+					'tour_comments' => 'Note: Performed in Zankel Hall at Carnegie Hall, New York, NY',
+				]
 			],
 			'postmeta key value2 exists' => [
-				'tour_id'       => 502,
-				'tour_comments' => 'Note: Performed at Alice Tully Hall, Lincoln Center, New York, NY',
+				'tour_id'   => 502,
+				'post_meta' => [
+					'tour_comments' => 'Note: Performed at Alice Tully Hall, Lincoln Center, New York, NY',
+				]
 			]
 		];
 	}
