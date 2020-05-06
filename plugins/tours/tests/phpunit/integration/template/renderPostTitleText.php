43ac6408a51a452cd9ae98469fcb2982aea360e9
Robert Gadon <rgadon107@gmail.com>
2020-05-05 21:54:32 -0500

    Update integration test file.
    - Add 'tearDown()' method to delete post meta from database after each 
    run of the test method.
    - Add dataProvider annotation and parameters to test method. 
    - Use create() method within WP factory to get post ID.
    - Pass data input to factory with data provider array. 
    - Add and get post_meta to build view. 
    - Change variable name when returning output buffer.
    - Change parameters in test assertion.
    - Add dataProvider method 'addTestData'. 
    - Add newline to end of file.

--- plugins/tours/tests/phpunit/integration/template/renderPostTitleText.php
+++ plugins/tours/tests/phpunit/integration/template/renderPostTitleText.php
@@ -22,23 +22,53 @@ use spiralWebDb\Cornerstone\Tests\Integration\Test_Case;
 class Tests_RenderPostTitleText extends Test_Case {
 
 	/**
-	 * Test render_post_title_text() should render the post title when the filter event fires.
+	 * Clean up the test environment after each test.
 	 */
-	public function test_should_echo_post_title_when_filter_event_fires() {
-		$post = $this->factory->post->create_and_get(
-			[
-				'post_type'  => 'tours',
-				'post_title' => 'I Make All Things New',
-			]
-		);
-		$expected_title = $post->post_title;
+	public function tearDown() {
+		parent::tearDown();
+
+		// Database cleanup.
+		delete_post_meta( $tours, 'tour_year' );
+	}
+
+	/**
+	 * @dataProvider addTestData
+	 */
+	public function test_should_echo_post_title_when_filter_event_fires( $post_data, $meta, $expected ) {
+		$tours = $this->factory->post->create( $post_data );
+
+		foreach ( $meta as $key => $value ) {
+			add_post_meta( $tours, $key, true );
+		}
+		get_post_meta( $tours, 'tour_year', true );
+
 		$this->go_to( '?tours=i-make-all-things-new' );
 
 		ob_start();
 		apply_filters( 'genesis_post_title_text', get_the_title() );
-		$actual_title = ob_get_clean();
+		$actual = ob_get_clean();
 
-		$this->assertEquals( $expected_title, $actual_title );
+		$this->assertEquals( $expected, $actual );
+	}
+
+	public function addTestData() {
+		return [
+			'add post data' => [
+				'post_data'     => [
+					'post_type'  => 'tours',
+					'post_title' => 'I Make All Things New',
+					'menu_order' => 5
+				],
+				'post_meta'     => [
+					'tour_year' => 2011,
+				],
+				'expected_view' => <<<PAST_TOUR_TITLE_VIEW
+<h2 class="entry-title tour-title" itemprop="headline">Tour 5 | 2011 | I Make All Things New</h2>
+PAST_TOUR_TITLE_VIEW
+				,
+			]
+		];
 	}
 }
+
 
